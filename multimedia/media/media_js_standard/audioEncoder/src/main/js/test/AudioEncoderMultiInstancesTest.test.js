62ea814591acceaf5a42569ffb4d9ecfa0a4fb18
ling990 <ling990@qq.com>
2022-05-05 13:47:32 +0800

    modify testcases Signed-off-by: ling990 <liling96@huawei.com>
    Signed-off-by: ling990 <ling990@qq.com>

--- multimedia/media/media_js_standard/audioEncoder/src/main/js/test/AudioEncoderMultiInstancesTest.test.js
+++ multimedia/media/media_js_standard/audioEncoder/src/main/js/test/AudioEncoderMultiInstancesTest.test.js
@@ -17,7 +17,7 @@ import media from '@ohos.multimedia.media'
 import Fileio from '@ohos.fileio'
 import {describe, beforeAll, beforeEach, afterEach, afterAll, it, expect} from 'deccjsunit/index'
 
-describe('AudioEncoderFuncPromise', function () {
+describe('AudioEncoderMultiInstances', function () {
     const RESOURCEPATH = '/data/accounts/account_0/appdata/ohos.acts.multimedia.audio.audioencoder/'
     const AUDIOPATH = RESOURCEPATH + 'S16LE.pcm';
     let readStreamSync;
