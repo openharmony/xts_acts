976d4a2c8d767d5b901487042491494d8822a36e
bayanxing <bayanxing@kaihongdigi.com>
2022-05-21 14:27:09 +0800

    add test cases
    Signed-off-by: bayanxing <bayanxing@kaihongdigi.com>

--- powermgr/powermgrbattery/src/main/js/default/test/battery_common_event.test.js
+++ powermgr/powermgrbattery/src/main/js/default/test/battery_common_event.test.js
@@ -107,8 +107,157 @@ describe('appInfoTest', function () {
         done();
     })
 
+    /**
+     * @tc.number BatteryCommonEventTest_010
+     * @tc.name subscribe battery completed common event
+     * @tc.desc Battery acquisition kit
+     */
+    it('BatteryCommonEventTest_010', 0, function (done) {
+        createShutdownSubscriber();
+        done();
+    })
+
+    /**
+     * @tc.number BatteryCommonEventTest_011
+     * @tc.name subscribe battery completed common event
+     * @tc.desc Battery acquisition kit
+     */
+    it('BatteryCommonEventTest_011', 0, function (done) {
+        createScreenOnSubscriber();
+        done();
+    })
+
     console.log("*************Battery commonEvent Test End*************");
 })
+
+function createScreenOnSubscriber() {
+    var commonEventSubscribeInfo = {
+        events: [commonEvent.Support.COMMON_EVENT_SCREEN_ON],
+    };
+    commonEvent.createSubscriber(commonEventSubscribeInfo)
+        .then(subscriber => {
+            console.info('createScreenOnSubscriber success');
+            var mySubscriber = subscriber;
+            console.log(subscriber);
+
+            if (subscriber == "" || subscriber == undefined || subscriber == null) {
+                console.info("createSubscriber failed");
+            }
+            mySubscriber.getCode()
+                .then((data) => {
+                    console.info('Subscriber getCode success : ' + JSON.stringify(data));
+                }).catch((error) => {
+                console.error('Subscriber getCode error because: ' + JSON.stringify(error));
+            })
+            mySubscriber.getData()
+                .then((data) => {
+                    console.info('Subscriber getData success : ' + JSON.stringify(data));
+                }).catch((error) => {
+                console.error('Subscriber getData error because: ' + JSON.stringify(error));
+            })
+            console.info('subscribe Screen_On begin ');
+
+            commonEvent.subscribe(mySubscriber, (error, commonEventData) => {
+                console.error('err code: ' + JSON.stringify(error));
+                console.info('subscribe callback: ' + JSON.stringify(commonEventData));
+                console.info("commonEventData event: " + commonEventData.event);
+                console.info("commonEventData bundleName: " + commonEventData.bundleName);
+                console.info("commonEventData data: " + commonEventData.data);
+                console.info("commonEventData parameter: " + commonEventData.parameters[0]);
+                var capacity = commonEventData.parameters['0'];
+                console.info("capacity is:" + capacity);
+                expect(capacity >= 0 && capacity <= 100).assertTrue();
+            });
+        }).catch((error) => {
+        console.error('Operation failed. Cause: ' + JSON.stringify(error));
+    });
+}
+
+function createScreenOffSubscriber() {
+    var commonEventSubscribeInfo = {
+        events: [commonEvent.Support.COMMON_EVENT_SCREEN_OFF],
+    };
+    commonEvent.createSubscriber(commonEventSubscribeInfo)
+        .then(subscriber => {
+            console.info('createScreenOffSubscriber success');
+            var mySubscriber = subscriber;
+            console.log(subscriber);
+
+            if (subscriber == "" || subscriber == undefined || subscriber == null) {
+                console.info("createSubscriber failed");
+            }
+            mySubscriber.getCode()
+                .then((data) => {
+                    console.info('Subscriber getCode success : ' + JSON.stringify(data));
+                }).catch((error) => {
+                console.error('Subscriber getCode error because: ' + JSON.stringify(error));
+            })
+            mySubscriber.getData()
+                .then((data) => {
+                    console.info('Subscriber getData success : ' + JSON.stringify(data));
+                }).catch((error) => {
+                console.error('Subscriber getData error because: ' + JSON.stringify(error));
+            })
+            console.info('subscribe Screen_Off begin ');
+
+            commonEvent.subscribe(mySubscriber, (error, commonEventData) => {
+                console.error('err code: ' + JSON.stringify(error));
+                console.info('subscribe callback: ' + JSON.stringify(commonEventData));
+                console.info("commonEventData event: " + commonEventData.event);
+                console.info("commonEventData bundleName: " + commonEventData.bundleName);
+                console.info("commonEventData data: " + commonEventData.data);
+                console.info("commonEventData parameter: " + commonEventData.parameters[0]);
+                var capacity = commonEventData.parameters['0'];
+                console.info("capacity is:" + capacity);
+                expect(capacity >= 0 && capacity <= 100).assertTrue();
+            });
+        }).catch((error) => {
+        console.error('Operation failed. Cause: ' + JSON.stringify(error));
+    });
+}
+
+function createShutdownSubscriber() {
+    var commonEventSubscribeInfo = {
+        events: [commonEvent.Support.COMMON_EVENT_SHUTDOWN],
+    };
+    commonEvent.createSubscriber(commonEventSubscribeInfo)
+        .then(subscriber => {
+            console.info('createShutdownSubscriber success');
+            var mySubscriber = subscriber;
+            console.log(subscriber);
+
+            if (subscriber == "" || subscriber == undefined || subscriber == null) {
+                console.info("createSubscriber failed");
+            }
+            mySubscriber.getCode()
+                .then((data) => {
+                    console.info('Subscriber getCode success : ' + JSON.stringify(data));
+                }).catch((error) => {
+                console.error('Subscriber getCode error because: ' + JSON.stringify(error));
+            })
+            mySubscriber.getData()
+                .then((data) => {
+                    console.info('Subscriber getData success : ' + JSON.stringify(data));
+                }).catch((error) => {
+                console.error('Subscriber getData error because: ' + JSON.stringify(error));
+            })
+            console.info('subscribe shutdown begin ');
+
+            commonEvent.subscribe(mySubscriber, (error, commonEventData) => {
+                console.error('err code: ' + JSON.stringify(error));
+                console.info('subscribe callback: ' + JSON.stringify(commonEventData));
+                console.info("commonEventData event: " + commonEventData.event);
+                console.info("commonEventData bundleName: " + commonEventData.bundleName);
+                console.info("commonEventData data: " + commonEventData.data);
+                console.info("commonEventData parameter: " + commonEventData.parameters[0]);
+                var capacity = commonEventData.parameters['0'];
+                console.info("capacity is:" + capacity);
+                expect(capacity >= 0 && capacity <= 100).assertTrue();
+            });
+        }).catch((error) => {
+        console.error('Operation failed. Cause: ' + JSON.stringify(error));
+    });
+}
 
 function createLockedBootCompletedSubscriber() {
     var commonEventSubscribeInfo = {
