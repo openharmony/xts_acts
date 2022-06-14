5bf79daa839614e254d6311d7d5872a91e25a2ed
NOBUGGERS <ruanmeng@huawei.com>
2022-06-14 14:10:21 +0800

    test name update Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>
    Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>

--- multimedia/media/media_js_standard/hlsPlayer/src/main/js/test/VideoPlayerHLSBitrateTest.test.js
+++ multimedia/media/media_js_standard/hlsPlayer/src/main/js/test/VideoPlayerHLSBitrateTest.test.js
@@ -134,7 +134,7 @@ describe('VideoPlayerHLSBitrateTest.test', function () {
         * @tc.type      : Function test
         * @tc.level     : Level0
     */
-     it('SUB_MEDIA_VIDEO_PLAYER_HLS_SET_BITRATE_0100', 0, async function (done) {
+    it('testHlsVideoPlayerSelectBitrate_0100', 0, async function (done) {
         await initVideoPlayer(done);
         await setVideoUrl(HTTP_PATH + '05.hls/hls_variant/index.m3u8');
         await playAndSetLoop(PLAY_TIME * 2);
@@ -156,7 +156,7 @@ describe('VideoPlayerHLSBitrateTest.test', function () {
         * @tc.type      : Function test
         * @tc.level     : Level0
     */
-    it('SUB_MEDIA_VIDEO_PLAYER_HLS_SET_BITRATE_0200', 0, async function (done) {
+    it('testHlsVideoPlayerSelectBitrate_0200', 0, async function (done) {
         await initVideoPlayer(done);
         await setVideoUrl(HTTP_PATH + '05.hls/hls_variant/index.m3u8');
         await playAndSetLoop(PLAY_TIME);
