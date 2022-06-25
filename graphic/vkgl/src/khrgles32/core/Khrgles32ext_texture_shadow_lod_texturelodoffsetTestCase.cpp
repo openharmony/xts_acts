02717623120954145e5ce3d6409b281c73182694
zhangchao <zhangchao@kaihongdigi.com>
2022-06-25 21:18:47 +0800

    create: vkgl dir architecture
    Signed-off-by: zhangchao <zhangchao@kaihongdigi.com>

---
+++ graphic/vkgl/src/khrgles32/core/Khrgles32ext_texture_shadow_lod_texturelodoffsetTestCase.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <climits>
+#include <gtest/gtest.h>
+#include "../Khrgles32BaseFunc.h"
+#include "../ActsKhrgles320002TestSuite.h"
+
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS;
+
+static HWTEST_F(ActsKhrgles320002TestSuite, TestCase_001145, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_001145 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.ext_texture_shadow_lod.te"
+        "xturelodoffset.sampler2darrayshadow_vertex",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320002TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320002TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320002TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320002TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320002TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_001145 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_001145 end";
+}
+
+static HWTEST_F(ActsKhrgles320002TestSuite, TestCase_001146, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_001146 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.ext_texture_shadow_lod.tex"
+        "turelodoffset.sampler2darrayshadow_fragment",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320002TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320002TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320002TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320002TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320002TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_001146 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_001146 end";
+}
