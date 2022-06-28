b99248bc9720638834ba390ecd9b6a63d6681b13
zhengyang <zhengyang@kaihongdigi.com>
2022-06-28 17:05:27 +0800

    test
    Signed-off-by: zhengyang <zhengyang@kaihongdigi.com>

---
+++ graphic/vkgl/src/khrgles32/core/Khrgles32tessellation_shader_tessellation_shader_tessellationTestCase.cpp
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "../Khrgles32BaseFunc.h"
+#include "../ActsKhrgles320001TestSuite.h"
+
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS;
+
+static HWTEST_F(ActsKhrgles320001TestSuite, TestCase_000202, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_000202 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.tessellation_shader.tessellation_shader_"
+        "tessellation.gl_InvocationID_PatchVerticesIn_PrimitiveID",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320001TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320001TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320001TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320001TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320001TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_000202 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_000202 end";
+}
+
+static HWTEST_F(ActsKhrgles320001TestSuite, TestCase_000203, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_000203 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.tessellation_shader.te"
+        "ssellation_shader_tessellation.TCS_TES",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320001TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320001TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320001TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320001TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320001TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_000203 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_000203 end";
+}
+
+static HWTEST_F(ActsKhrgles320001TestSuite, TestCase_000204, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_000204 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.tessellation_shader."
+        "tessellation_shader_tessellation.TES",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320001TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320001TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320001TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320001TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320001TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_000204 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_000204 end";
+}
+
+static HWTEST_F(ActsKhrgles320001TestSuite, TestCase_000205, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_000205 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.tessellation_shader.tessella"
+        "tion_shader_tessellation.input_patch_discard",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320001TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320001TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320001TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320001TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320001TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_000205 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_000205 end";
+}
+
+static HWTEST_F(ActsKhrgles320001TestSuite, TestCase_000206, Function | MediumTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TestCase_000206 start";
+    int argc = 3;
+    const char *argv[3] = {
+        ".",
+        "--deqp-case="
+        "KHR-GLES32.core.tessellation_shader.tessellat"
+        "ion_shader_tessellation.max_in_out_attributes",
+        "--deqp-archive-dir=/data/local/tmp/"
+    };
+    
+    FuncRunResult result = RunTestKHRGLES(argc, argv);
+    ActsKhrgles320001TestSuite::runResult.numPassed += result.numPassed;
+    ActsKhrgles320001TestSuite::runResult.numFailed += result.numFailed;
+    ActsKhrgles320001TestSuite::runResult.numNotSupported += result.numNotSupported;
+    ActsKhrgles320001TestSuite::runResult.numWarnings += result.numWarnings;
+    ActsKhrgles320001TestSuite::runResult.numWaived += result.numWaived;
+    if (result.numNotSupported == 1) {
+        GTEST_LOG_(INFO) << "TestCase_000206 notsupport!";
+    } else if (result.isComplete) {
+        EXPECT_TRUE(result.isComplete);
+        EXPECT_TRUE(result.numPassed == 1);
+    };
+    GTEST_LOG_(INFO) << "TestCase_000206 end";
+}
