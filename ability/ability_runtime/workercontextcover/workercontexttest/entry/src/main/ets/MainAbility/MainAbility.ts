6ddedaccb63ab9beaf3bab46ec71e7138e6bdf0a
chengxingzhen <chengxingzhen@huawei.com>
2022-07-16 14:57:44 +0800

    元能力新增api覆盖
    Signed-off-by: chengxingzhen <chengxingzhen@huawei.com>

---
+++ ability/ability_runtime/workercontextcover/workercontexttest/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Ability from '@ohos.application.Ability';
+
+var printLog1 = 'Worker:MainAbility:'
+var listPush1 = 'Worker_MainAbility_'
+export default class MainAbility extends Ability {
+    onCreate(want, launchParam) {
+        console.info(printLog1 + 'onCreate')
+        globalThis.abilityWant = want
+        globalThis.applicationContext = this.context.getApplicationContext()
+        globalThis.abilityContext = this.context
+    }
+
+    onDestroy() {
+        console.info(printLog1 + 'onDestroy')
+    }
+
+    onWindowStageCreate(windowStage) {
+        // Main window is created, set main page for this ability
+        console.info(printLog1 + 'onWindowStageCreate')
+
+        windowStage.loadContent("pages/index", (err, data) => {
+            if (err.code) {
+                console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+                return;
+            }
+            console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
+        });
+    }
+
+    onWindowStageDestroy() {
+        console.info(printLog1 + 'onWindowStageDestroy')
+    }
+
+    onForeground() {
+        console.info(printLog1 + 'onForeground')
+    }
+
+    onBackground() {
+        console.info(printLog1 + 'onBackground')
+    }
+};
---
+++ ability/ability_runtime/workercontextcover/workercontexttest/entry/src/main/ets/workers/worker_SUB_AA_AMS_Stage_StageContextSupportsWorker_0400.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import worker from '@ohos.worker';
+
+let parentPort = worker.parentPort;
+var printLog = 'SUB_AA_AMS_Stage_StageContextSupportsWorker_0400 '
+parentPort.onmessage = function(message) {
+    console.info(printLog + "onmessage: " + JSON.stringify(message));
+    let context = message.data
+    let ctx = context.isTerminating()
+    console.info(printLog + "ctx: " + ctx);
+    let expectValue = false
+    let status1 = typeof ctx == 'boolean'
+    let status2 = ctx == false
+    console.info(printLog + "status1: " + status1 + ";status2: " + status2);
+    if(status1 && status2) {
+        expectValue = true
+    }
+    parentPort.postMessage(expectValue)
+}
+
+parentPort.onmessageerror = function(messageerror) {
+    console.info(printLog + "onmessageerror: " + JSON.stringify(messageerror));
+}
+
+parentPort.onerror = function(error) {
+    console.info(printLog + "onerror: " + JSON.stringify(error));
+}
