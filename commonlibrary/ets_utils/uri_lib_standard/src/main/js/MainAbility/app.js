34f3952fdbe2744b7a9e609b805c6b9a0ecb898f
jiyong_sd <jiyong@huawei.com>
2022-07-22 10:19:31 +0800

    Adapt to the new framework
    Signed-off-by: jiyong_sd <jiyong@huawei.com>
    Change-Id: I4dfc59d54a85c0325c7b85914eb43239e66474a2

--- commonlibrary/ets_utils/uri_lib_standard/src/main/js/MainAbility/app.js
+++ commonlibrary/ets_utils/uri_lib_standard/src/main/js/MainAbility/app.js
@@ -12,8 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { UriTest } from './uri.test.js'
 
-export default function testsuite() {
-    UriTest()
-}
+export default {
+    onCreate() {
+        console.info('AceApplication onCreate');
+    },
+    onDestroy() {
+        console.info('AceApplication onDestroy');
+    }
+};
