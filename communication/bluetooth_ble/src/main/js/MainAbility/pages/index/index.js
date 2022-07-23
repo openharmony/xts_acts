4edd41c282f4e66aa9c8d447174f33304dc45013
jiyong_sd <jiyong@huawei.com>
2022-07-23 17:05:33 +0800

    Adapt to the new framework
    Signed-off-by: jiyong_sd <jiyong@huawei.com>
    Change-Id: Ib4c5af6df24971ab3fd22b85bdb8a38107c6f240

--- communication/bluetooth_ble/src/main/js/MainAbility/pages/index/index.js
+++ communication/bluetooth_ble/src/main/js/MainAbility/pages/index/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,4 +13,20 @@
  * limitations under the License.
  */
 
-require('./BluetoothOn.test.js')
+
+
+export default {
+    data: {
+        title: ""
+    },
+    onInit() {
+        this.title = this.$t('strings.world');
+    },
+    onShow() {
+        console.info('onShow finish')
+    },
+    onReady() {
+        console.info('onReady finish')
+    },
+}
+
