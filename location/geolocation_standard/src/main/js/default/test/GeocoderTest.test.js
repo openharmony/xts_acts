0f11eb4fa0020643eea36c6ce6e1ec4e793653a4
nwn1007101 <niyufei@huawei.com>
2022-07-26 07:37:38 +0000

    fixed ee047f4 from https://gitee.com/ni-yufei/xts_acts/pulls/4617 update
    Signed-off-by: nwn1007101 <niyufei@huawei.com>

--- location/geolocation_standard/src/main/js/default/test/GeocoderTest.test.js
+++ location/geolocation_standard/src/main/js/default/test/GeocoderTest.test.js
@@ -39,7 +39,6 @@ async function changedLocationMode(){
             await geolocation.requestEnableLocation().then(async(result) => {
                 await sleep(3000);
                 console.info('[lbs_js] test requestEnableLocation promise result: ' + JSON.stringify(result));
-                expect(result).assertTrue();
             }).catch((error) => {
                 console.info("[lbs_js] promise then error." + JSON.stringify(error));
                 expect().assertFail();
@@ -47,7 +46,7 @@ async function changedLocationMode(){
         }
     });
     await geolocation.isLocationEnabled().then(async(result) => {
-        console.info('[lbs_js] getLocationSwitchState result: ' + JSON.stringify(result));
+        console.info('[lbs_js]check LocationSwitchState result: ' + JSON.stringify(result));
     });
 }
 
