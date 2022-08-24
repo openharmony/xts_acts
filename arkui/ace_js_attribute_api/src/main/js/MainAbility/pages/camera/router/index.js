eae3b3f4d1a433809c1f9233fdc9bb1ca030044b
bayanxing <bayanxing@kaihong.com>
2022-08-24 17:52:05 +0800

    push 0816
    Signed-off-by: bayanxing <bayanxing@kaihong.com>

---
+++ arkui/ace_js_attribute_api/src/main/js/MainAbility/pages/camera/router/index.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default {
+    data: {
+        title: ""
+    },
+    onInit() {
+
+    },
+    functionTest1() {
+        var camera = this.$element('camera1');
+        camera.takePhoto({
+            quality: 'high'
+        })
+    },
+    functionTest2() {
+        var camera = this.$element('camera1');
+        camera.takePhoto({
+            quality: 'normal'
+        })
+    },
+    functionTest3() {
+        var camera = this.$element('camera1');
+        camera.takePhoto({
+            quality: 'low'
+        })
+    },
+    cameraError() {
+        console.info('授权失败！')
+    }
+}
+
+
+
