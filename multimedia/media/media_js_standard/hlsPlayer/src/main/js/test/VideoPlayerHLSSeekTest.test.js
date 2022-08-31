e00bc705e54b9183cc9bbb2c233195756e4ef68a
NOBUGGERS <ruanmeng@huawei.com>
2022-08-31 17:47:38 +0800

    用例名规范整改  Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>
    Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>

--- multimedia/media/media_js_standard/hlsPlayer/src/main/js/test/VideoPlayerHLSSeekTest.test.js
+++ multimedia/media/media_js_standard/hlsPlayer/src/main/js/test/VideoPlayerHLSSeekTest.test.js
@@ -47,7 +47,7 @@ describe('VideoPlayerHLSTest', function () {
     })
 
     /* *
-        * @tc.number    : SUB_MEDIA_VIDEO_PLAYER_HLS_SEEK
+        * @tc.number    : SUB_MULTIMEDIA_MEDIA_VIDEO_PLAYER_HLS_SEEK_0100
         * @tc.name      : 001.test hls
         * @tc.desc      : HLS Video playback control test
         * @tc.size      : MediumTest
