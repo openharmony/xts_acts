51659826de83f893665b9dc8adb71ff6d9878f46
qiuxiangdong <qiuxiangdong3@huawei.com>
2022-10-18 16:43:25 +0800

    test: Added the use case of changing an interface
    Signed-off-by: qiuxiangdong <qiuxiangdong3@huawei.com>

--- powermgr/power_manager/src/main/js/test/power_manager_power.test.js
+++ powermgr/power_manager/src/main/js/test/power_manager_power.test.js
@@ -135,44 +135,6 @@ describe('PowerManagerPowerTest', function () {
     })
 
     /**
-     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0050
-     * @tc.name Power_Get_PowerMode_Callback_JSTest0050
-     * @tc.desc Get The mode the device
-     */
-     it('Power_Get_PowerMode_Callback_JSTest0050', 0, async function (done) {
-        power.getPowerMode()
-        .then(powerMode => {
-            console.info('Power_Get_PowerMode_Callback_JSTest0050 power mode is ' + powerMode);
-            expect(powerMode >= 600 && powerMode <= 603).assertTrue();
-            done();
-        })
-        .catch(error => {
-            console.log('Power_Get_PowerMode_Callback_JSTest0050 error: ' + error);
-            expect().assertFail();
-            done();
-        })    
-    })
-
-    /**
-     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0060
-     * @tc.name Power_Get_PowerMode_Promise_JSTest0060
-     * @tc.desc Get The mode the device
-     */
-    it('Power_Get_PowerMode_Promise_JSTest0060', 0, async function (done) {
-        power.getPowerMode((error, powerMode) => {
-            if (typeof error === "undefined") {
-                console.info('Power_Get_PowerMode_Promise_JSTest0060 power mode is ' + powerMode);
-                expect(powerMode >= 600 && powerMode <= 603).assertTrue();
-                done();
-            } else {
-                console.log('Power_Get_PowerMode_Promise_JSTest0060: ' + error);
-                expect().assertFail();
-                done();
-            }    
-        })
-    })
-
-    /**
      * @tc.number SUB_PowerSystem_PowerManager_JSTest_0070
      * @tc.name Power_Device_Power_Mode_MODENORMAL_JSTest0070
      * @tc.desc Get device power mode MODE_NORMAL
@@ -215,5 +177,186 @@ describe('PowerManagerPowerTest', function () {
         console.info('MODE_EXTREME_POWER_SAVE = ' + devicePowerMode);
         expect(devicePowerMode === 603).assertTrue();
       })
+    
+    // New interface
+    /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0110
+     * @tc.name Power_Get_Set_Mode_Promise_JSTest0110
+     * @tc.desc Gets and sets the power mode
+     */
+     it('Power_Get_Set_Mode_Promise_JSTest0110', 0, async function (done) {
+        let currentMode = power.getPowerMode();
+        console.info('Power_Get_Set_Mode_Promise_JSTest0110 currentMode:' + currentMode);
+        expect(currentMode >= power.DevicePowerMode.MODE_NORMAL &&
+            currentMode <= power.DevicePowerMode.MODE_EXTREME_POWER_SAVE).assertTrue();
+        try {
+            let isExec = false;
+            power.setPowerMode(power.DevicePowerMode.MODE_POWER_SAVE)
+                .then((error) => {
+                    isExec = true;
+                    console.info('Power_Get_Set_Mode_Promise_JSTest0110 error:' + (typeof error));
+                    expect(typeof error === "undefined").assertTrue();
+                    let mode = power.getPowerMode();
+                    console.info('Power_Get_Set_Mode_Promise_JSTest0110 mode:' + mode);
+                    expect(mode === power.DevicePowerMode.MODE_POWER_SAVE).assertTrue();
+                }).finally(() => {
+                    expect(isExec).assertTrue();
+                })
+        } catch (e) {
+            console.info('Power_Get_Set_Mode_Promise_JSTest0110 error:' + e);
+            expect().assertFail();
+        }
+        power.setPowerMode(currentMode);
+        done();
+    })
+
+    /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0120
+     * @tc.name Power_Get_Set_Mode_Promise_Invalid_JSTest0120
+     * @tc.desc Set the wrong power mode
+     */
+    it('Power_Get_Set_Mode_Promise_Invalid_JSTest0120', 0, async function (done) {
+        try {
+            // invalid mode
+            power.setPowerMode('').then(() => {
+                console.info('Power_Get_Set_Mode_Promise_Invalid_JSTest0120 string failed');
+                expect().assertFail();
+            })
+        } catch (e) {
+            console.info('Power_Get_Set_Mode_Promise_Invalid_JSTest0120 code:' + e.code + 'msg:' + e.message);
+            // 401: Invalid input parameter
+            expect(e.code === 401).assertTrue();
+        }
+
+        try {
+            // Number of invalid parameters
+            power.setPowerMode('', '', 0).then(() => {
+                console.info('Power_Get_Set_Mode_Promise_Invalid_JSTest0120 number failed');
+                expect().assertFail();
+            })
+        } catch (e) {
+            console.info('Power_Get_Set_Mode_Promise_Invalid_JSTest0120 code:' + e.code + 'msg:' + e.message);
+            // 401: Invalid input parameter
+            expect(e.code === 401).assertTrue();
+        }
+        done();
+    })
+
+    /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0130
+     * @tc.name Power_Get_Set_Mode_Callback_JSTest0130
+     * @tc.desc Gets and sets the power mode
+     */
+    it('Power_Get_Set_Mode_Callback_JSTest0130', 0, async function (done) {
+        let currentMode = power.getPowerMode();
+        console.info('Power_Get_Set_Mode_Callback_JSTest0130 currentMode:' + currentMode);
+        expect(currentMode >= power.DevicePowerMode.MODE_NORMAL &&
+            currentMode <= power.DevicePowerMode.MODE_EXTREME_POWER_SAVE).assertTrue();
+        try {
+            power.setPowerMode(power.DevicePowerMode.MODE_PERFORMANCE, (error) => {
+                console.info('Power_Get_Set_Mode_Callback_JSTest0130 error:' + (typeof error));
+                expect(typeof error === "undefined").assertTrue();
+                let mode = power.getPowerMode();
+                console.info('Power_Get_Set_Mode_Callback_JSTest0130 mode:' + mode);
+                expect(mode === power.DevicePowerMode.MODE_PERFORMANCE).assertTrue();
+            })
+        } catch (e) {
+            console.info('Power_Get_Set_Mode_Callback_JSTest0130 error:' + e);
+            expect().assertFail();
+        }
+        power.setPowerMode(currentMode);
+        done();
+    })
+
+    /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0140
+     * @tc.name Power_Get_Set_Mode_Callback_Invalid_JSTest0140
+     * @tc.desc Set the wrong power mode
+     */
+    it('Power_Get_Set_Mode_Callback_Invalid_JSTest0140', 0, async function (done) {
+        try {
+            // invalid mode
+            power.setPowerMode('', (error) => {
+                expect().assertFail();
+            })
+        } catch (e) {
+            console.info('Power_Get_Set_Mode_Callback_Invalid_JSTest0140 code:' + e.code + 'msg:' + e.message);
+            // 401: Invalid input parameter
+            expect(e.code === 401).assertTrue();
+        }
+
+        try {
+            // invalid callback
+            power.setPowerMode(power.DevicePowerMode.MODE_PERFORMANCE, '')
+        } catch (e) {
+            console.info('Power_Get_Set_Mode_Callback_Invalid_JSTest0140 code:' + e.code + 'msg:' + e.message);
+            // 401: Invalid input parameter
+            expect(e.code === 401).assertTrue();
+        }
+        done();
+    })
+
+     /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0150
+     * @tc.name Power_Reboot_JSTest0150
+     * @tc.desc reboot
+     */
+      it('Power_Reboot_JSTest0150', 0, function () {
+        // Reboot tests are not performed by default
+        if (false) {
+            try {
+                power.reboot('Power_Reboot_JSTest0150');
+            } catch (e) {
+                console.info('Power_Reboot_JSTest0150 error:' + e);
+                expect().assertFail();
+            }
+        }
+    })
+
+    /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0160
+     * @tc.name Power_Wakeup_Suspend_IsActive_JSTest0160
+     * @tc.desc On and Off Screen and get the current screen on and off
+     */
+    it('Power_Wakeup_Suspend_IsActive_JSTest0160', 0, function () {
+        try {
+            power.suspend();
+            power.wakeup('wakeup_suspend_isPower_Wakeup_Suspend_IsActive_JSTest0160_active');
+            let on = power.isActive();
+            console.info('Power_Wakeup_Suspend_IsActive_JSTest0160 on:' + on);
+            expect(on).assertTrue();
+            power.suspend();
+            let off = power.isActive();
+            console.info('Power_Wakeup_Suspend_IsActive_JSTest0160 off:' + off);
+            expect(off).assertFalse();
+        } catch (e) {
+            console.info('Power_Wakeup_Suspend_IsActive_JSTest0160:' + e);
+            expect().assertFail();
+        }
+    })
+
+    /**
+     * @tc.number SUB_PowerSystem_PowerManager_JSTest_0170
+     * @tc.name Power_Wakeup_Invalid_JSTest0170
+     * @tc.desc wakeup Input invalid parameter
+     */
+    it('Power_Wakeup_Invalid_JSTest0170', 0, function () {
+        try {
+            power.wakeup(0)
+        } catch (e) {
+            console.info('Power_Wakeup_Invalid_JSTest0170 code:' + e.code + "msg:" + e.message);
+            // 401: Invalid input parameter
+            expect(e.code === 401).assertTrue();
+        }
+
+        try {
+            // The number of parameters does not match
+            power.wakeup('', 0)
+        } catch (e) {
+            console.info('Power_Wakeup_Invalid_JSTest0170 code:' + e.code + "msg:" + e.message);
+            // 401: Invalid input parameter
+            expect(e.code === 401).assertTrue();
+        }
+    })
 })
 }
