8b3b995bca1b911fc01db2491e9e1a32065954f8
zhijianwen <zhijianwen@huawei.com>
2022-11-04 12:22:48 +0800

    --ActsNotificationManagerDistributedTest--
    Signed-off-by: zhijianwen <zhijianwen@huawei.com>

--- notification/ans_standard/actsNotificationDistributedTest/src/main/js/test/ActsNotificationDistributedTest.js
+++ notification/ans_standard/actsNotificationDistributedTest/src/main/js/test/ActsNotificationDistributedTest.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import notification from '@ohos.notification';
+import notification from '@ohos.notification'
 import { describe, beforeAll, beforeEach, afterEach, afterAll, it, expect } from '@ohos/hypium'
 
 export default function ActsNotificationDistributedTest() {
