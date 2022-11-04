d2718376f78dd92b08c5a09b807bcb058977b637
zhijianwen <zhijianwen@huawei.com>
2022-11-04 14:37:34 +0800

    --ActsNotificationManagerPublishTest--
    Signed-off-by: zhijianwen <zhijianwen@huawei.com>

--- notification/ans_standard/actsNotificationPublishTest/src/main/js/test/List.test.js
+++ notification/ans_standard/actsNotificationPublishTest/src/main/js/test/List.test.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +13,9 @@
  * limitations under the License.
  */
 import ActsNotificationPublishTest from './ActsNotificationPublishTest.js'
+import ActsNotificationManagerPublishTest from './ActsNotificationManagerPublishTest.js'
+
 export default function testsuite() {
      ActsNotificationPublishTest()
+     ActsNotificationManagerPublishTest()
 }
