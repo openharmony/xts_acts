029f4f7bc4c85b309d5aafea9b4b2a462a974b05
liuhaonan2 <liuhaonan8@huawei.com>
2022-11-29 10:45:24 +0800

    <liuhaonan8@huawei.com>
    Signed-off-by: liuhaonan2 <liuhaonan8@huawei.com>

--- msdp/msdpdevicestatus/src/main/js/default/app.js
+++ msdp/msdpdevicestatus/src/main/js/default/app.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,9 +16,9 @@ import device from '@system.device';
 
 export default {
     onCreate() {
-        console.info('AceApplication onCreate');
+        console.info('MsdpApplication onCreate');
     },
     onDestroy() {
-        console.info('AceApplication onDestroy');
+        console.info('MsdpApplication onDestroy');
     }
 };
