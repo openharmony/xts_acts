6589be6f7b4784906135613d93f629192bd70ec2
inter515 <ry.renyi@huawei.com>
2022-11-30 10:44:42 +0800

    用例调整
    Signed-off-by: inter515 <ry.renyi@huawei.com>

---
+++ bundlemanager/bundle_standard/bundlemanager/actsbmshapmoduletest/src/main/js/test/ActsBmsHapModuleTest.test.js
@@ -0,0 +1,211 @@
+/*
+* Copyright (c) 2021 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import bundle from '@ohos.bundle'
+import { describe, beforeAll, beforeEach, afterEach, afterAll, it, expect } from '@ohos/hypium'
+
+const BUNDLE_NAME1 = 'com.example.bmsmainabilityfirstscene';
+const BUNDLE_NAME2 = 'com.example.third2';
+const BUNDLE_NAME3 = 'com.example.third5';
+const BUNDLE_NAME5 = 'com.example.system1';
+const FIRSTMAINABILITY = 'com.example.bmsmainabilityfirstscene.MainAbility';
+const SECONDMAINABILITY = 'com.example.bmsmainabilitysecondscene.MainAbility';
+const MODULE_NAME = 'entry';
+const ICON = '$media:icon';
+const ICON_ID = 16777218;
+const LABEL = '$string:app_name';
+const LABEL_ID = 16777216;
+const DEVICE_TYPES = 'default';
+const FIRST_SCENE_HAP_NAME = 'com.example.bmsmainabilityfirstscene.MyApplication';
+const SECOND_SCENE_HAP_NAME = 'com.example.bmsmainabilitysecondscene.MyApplication';
+const THIRD_TWO_HAP_NAME = 'com.example.third2.MyApplication';
+const THIRD_FIVE_HAP_NAME = 'com.example.third5.BmsThirdBundle5';
+const SYSTEM_ONE_HAP_NAME = 'com.example.system1.BmsSystemBundle1';
+
+export default function ActsBmsHapModuleTest() {
+describe('ActsBmsHapModuleTest', function () {
+
+    /*
+     * @tc.number: bms_getHapModuleInfo_0100
+     * @tc.name: get hapModuleInfo from one app by getBundleInfo
+     * @tc.desc: get the module information of the hap with type of entry
+     */
+    it('bms_getHapModuleInfo_0100', 0, async function (done) {
+        let dataInfo = await bundle.getBundleInfo(BUNDLE_NAME1, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        bundle.getBundleInfo(BUNDLE_NAME1, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES, callback);
+        async function callback(err, result) {
+            expect(JSON.stringify(result)).assertEqual(JSON.stringify(dataInfo));
+            expect(result.hapModuleInfos.length).assertEqual(2);
+            if (result.hapModuleInfos.length > 0) {
+                let hapModuleInfo = result.hapModuleInfos[0];
+                expect(hapModuleInfo.name).assertEqual(FIRST_SCENE_HAP_NAME);
+                expect(hapModuleInfo.moduleName).assertEqual(MODULE_NAME);
+                expect(hapModuleInfo.description).assertEqual('');
+                expect(hapModuleInfo.descriptionId).assertEqual(0);
+                expect(hapModuleInfo.icon).assertEqual(ICON);
+                expect(hapModuleInfo.label).assertEqual(LABEL);
+                expect(hapModuleInfo.labelId).assertEqual(LABEL_ID);
+                expect(hapModuleInfo.iconId).assertEqual(ICON_ID);
+                expect(hapModuleInfo.backgroundImg).assertEqual("");
+                expect(hapModuleInfo.supportedModes).assertEqual(0);
+                expect(hapModuleInfo.reqCapabilities.length).assertEqual(0);
+                expect(hapModuleInfo.deviceTypes[0]).assertEqual(DEVICE_TYPES);
+                expect(hapModuleInfo.mainAbilityName).assertEqual(FIRSTMAINABILITY);
+                expect(hapModuleInfo.abilityInfo.length).assertLarger(0);
+                expect(hapModuleInfo.installationFree).assertEqual(false);
+                done();
+            }
+        }
+    });
+
+    /*
+     * @tc.number: bms_getHapModuleInfo_0200
+     * @tc.name: get hapModuleInfo from two modules by getBundleInfo
+     * @tc.desc: get the module information of the hap with type of feature
+     */
+    it('bms_getHapModuleInfo_0200', 0, async function (done) {
+        let dataInfo = await bundle.getBundleInfo(BUNDLE_NAME1, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        bundle.getBundleInfo(BUNDLE_NAME1, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES, async (err, result) => {
+            expect(JSON.stringify(result)).assertEqual(JSON.stringify(dataInfo));
+            expect(result.hapModuleInfos.length).assertEqual(2);
+            if (result.hapModuleInfos.length == 2) {
+                let hapModuleInfo = result.hapModuleInfos[0];
+                let hapModuleInfo1 = result.hapModuleInfos[1];
+                checkHapMoudleInfos(hapModuleInfo);
+                checkHapMoudleInfos(hapModuleInfo1);           
+                expect(hapModuleInfo.name).assertEqual(FIRST_SCENE_HAP_NAME);
+                expect(hapModuleInfo1.name).assertEqual(SECOND_SCENE_HAP_NAME);
+                expect(hapModuleInfo.moduleName).assertEqual(MODULE_NAME);
+                expect(hapModuleInfo.mainAbilityName).assertEqual(FIRSTMAINABILITY);
+                expect(hapModuleInfo1.moduleName).assertEqual('bmsmainabilitysecondscene');
+                expect(hapModuleInfo1.mainAbilityName).assertEqual(SECONDMAINABILITY);
+                expect(hapModuleInfo.installationFree).assertEqual(false);
+                expect(hapModuleInfo1.installationFree).assertEqual(true);
+                done();
+            }
+        });
+    });
+
+    /*
+     * @tc.number: bms_getHapModuleInfo_0300
+     * @tc.name: get hapModuleInfo one app without mainAbility by getBundleInfo
+     * @tc.desc: get the module information of the hap without mainAbility
+     */
+    it('bms_getHapModuleInfo_0300', 0, async function (done) {
+        let result = await bundle.getBundleInfo(BUNDLE_NAME2, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        bundle.getBundleInfo(BUNDLE_NAME2, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES, (err, data) => {
+            expect(JSON.stringify(result)).assertEqual(JSON.stringify(data));
+            expect(result.hapModuleInfos.length).assertEqual(1);
+            if (result.hapModuleInfos.length > 0) {
+                let hapModuleInfo = result.hapModuleInfos[0];
+                expect(hapModuleInfo.name).assertEqual(THIRD_TWO_HAP_NAME);
+                expect(hapModuleInfo.moduleName).assertEqual(MODULE_NAME);
+                expect(hapModuleInfo.description).assertEqual('');
+                expect(hapModuleInfo.descriptionId).assertEqual(0);
+                expect(hapModuleInfo.icon).assertEqual('');
+                expect(hapModuleInfo.label).assertEqual('');
+                expect(hapModuleInfo.labelId).assertEqual(0);
+                expect(hapModuleInfo.iconId).assertEqual(0);
+                expect(hapModuleInfo.backgroundImg).assertEqual("");
+                expect(hapModuleInfo.supportedModes).assertEqual(0);
+                expect(hapModuleInfo.reqCapabilities.length).assertEqual(0);
+                expect(hapModuleInfo.deviceTypes[0]).assertEqual(DEVICE_TYPES);
+                expect(hapModuleInfo.mainAbilityName).assertEqual("");
+                expect(hapModuleInfo.abilityInfo.length).assertLarger(0);
+                expect(hapModuleInfo.installationFree).assertEqual(false);
+                done();
+            }
+        });
+    });
+
+    /*
+     * @tc.number: bms_getHapModuleInfo_0400
+     * @tc.name: get hapModuleInfo through getBundleInfo, an application that adds mainAbility
+     * @tc.desc: get the module information of the hap with the added field mainAbility
+     */
+    it('bms_getHapModuleInfo_0400', 0, async function (done) {
+        let dataInfo = await bundle.getBundleInfo(BUNDLE_NAME3, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        bundle.getBundleInfo(BUNDLE_NAME3, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES, async (err, result) => {
+            expect(JSON.stringify(result)).assertEqual(JSON.stringify(dataInfo));
+            expect(result.hapModuleInfos.length).assertEqual(1);
+            if (result.hapModuleInfos.length == 1) {
+                let hapModuleInfo = result.hapModuleInfos[0];
+                expect(hapModuleInfo.name).assertEqual(THIRD_FIVE_HAP_NAME);
+                expect(hapModuleInfo.moduleName).assertEqual(MODULE_NAME);
+                expect(hapModuleInfo.description).assertEqual('');
+                expect(hapModuleInfo.descriptionId).assertEqual(0);
+                expect(hapModuleInfo.icon).assertEqual(ICON);
+                expect(hapModuleInfo.label).assertEqual(LABEL);
+                expect(hapModuleInfo.labelId).assertEqual(LABEL_ID);
+                expect(hapModuleInfo.iconId).assertEqual(16777219);
+                expect(hapModuleInfo.backgroundImg).assertEqual("");
+                expect(hapModuleInfo.supportedModes).assertEqual(0);
+                expect(hapModuleInfo.reqCapabilities.length).assertEqual(0);
+                expect(hapModuleInfo.deviceTypes[0]).assertEqual(DEVICE_TYPES);
+                expect(hapModuleInfo.mainAbilityName).assertEqual("com.example.third5.AMainAbility");
+                expect(hapModuleInfo.abilityInfo.length).assertLarger(0);
+                expect(hapModuleInfo.installationFree).assertEqual(false);
+                done();
+            }
+        });
+    });
+
+    /*
+     * @tc.number: bms_getHapModuleInfo_0600
+     * @tc.name: get hapModuleInfo from the system through getBundleInfo 
+     * @tc.desc: get module information of mainAbility system application 
+     */
+    it('bms_getHapModuleInfo_0600', 0, async function (done) {
+        let dataInfo = await bundle.getBundleInfo(BUNDLE_NAME5, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        bundle.getBundleInfo(BUNDLE_NAME5, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES, callback);
+        function callback(err, data) {
+            expect(JSON.stringify(data)).assertEqual(JSON.stringify(dataInfo));
+            expect(data.hapModuleInfos.length).assertEqual(1);
+            if (data.hapModuleInfos.length == 1) {
+                let hapModuleInfo = data.hapModuleInfos[0];
+                expect(hapModuleInfo.name).assertEqual(SYSTEM_ONE_HAP_NAME);
+                expect(hapModuleInfo.moduleName).assertEqual(MODULE_NAME);
+                expect(hapModuleInfo.description).assertEqual('');
+                expect(hapModuleInfo.descriptionId).assertEqual(0);
+                expect(hapModuleInfo.icon).assertEqual(ICON);
+                expect(hapModuleInfo.label).assertEqual(LABEL);
+                expect(hapModuleInfo.labelId).assertEqual(LABEL_ID);
+                expect(hapModuleInfo.iconId).assertEqual(16777219);
+                expect(hapModuleInfo.backgroundImg).assertEqual("");
+                expect(hapModuleInfo.supportedModes).assertEqual(0);
+                expect(hapModuleInfo.reqCapabilities.length).assertEqual(0);
+                expect(hapModuleInfo.deviceTypes[0]).assertEqual(DEVICE_TYPES);
+                expect(hapModuleInfo.mainAbilityName).assertEqual("com.example.system1.MainAbility");
+                expect(hapModuleInfo.abilityInfo.length).assertLarger(0);
+                expect(hapModuleInfo.installationFree).assertEqual(false);
+            }
+            done();
+        }
+    });
+
+    function checkHapMoudleInfos(info) {
+        expect(info.description).assertEqual('');
+        expect(info.descriptionId).assertEqual(0);
+        expect(info.icon).assertEqual(ICON);
+        expect(info.labelId).assertEqual(LABEL_ID);
+        expect(info.iconId).assertEqual(ICON_ID);
+        expect(info.label).assertEqual(LABEL);
+        expect(info.backgroundImg).assertEqual("");
+        expect(info.supportedModes).assertEqual(0);
+        expect(info.reqCapabilities.length).assertEqual(0);
+        expect(info.deviceTypes[0]).assertEqual(DEVICE_TYPES);
+        expect(info.abilityInfo.length).assertLarger(0);
+    }
+})}
---
+++ bundlemanager/bundle_standard/bundlemanager/actsbmsjstest/src/main/js/test/ActsBmsJsTest.test.js
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import bundle from '@ohos.bundle'
+import pkg from '@system.package'
+import { describe, beforeAll, beforeEach, afterEach, afterAll, it, expect } from '@ohos/hypium'
+
+const NUM_TWO = 2;
+const NUM_FOUR = 4;
+const BUNDLE_NAME2 = 'com.example.third2';
+const BUNDLE_NAME3 = 'com.example.third5';
+const BUNDLE_NAME4 = 'com.example.js';
+const BUNDLE_NAME5 = 'com.example.c';
+const ABILITIY_NAME1 = 'com.example.third2.MainAbility';
+const ABILITIY_NAME2 = 'com.example.third5.AMainAbility';
+const ABILITIY_NAME3 = 'com.example.third5.BMainAbility';
+const ABILITIY_NAME4 = 'com.example.js.MainAbility';
+const ABILITIY_NAME5 = '.MainAbility';
+const ABILITIY_NAME6 = "com.example.third2.MainAbilityA";
+const ABILITIY_NAME7 = "com.example.third5.MainAbilityA";
+const ABILITIY_NAME8 = "com.example.third5.MainAbilityB";
+
+export default function ActsBmsJsTest() {
+describe('ActsBmsJsTest', function () {
+
+    /*
+    * @tc.number: bms_getJsAbility_0100
+    * @tc.name: test the multi js ability
+    * @tc.desc: 1.install a js hap which has an ability with full domain name
+    *           2.check the ability name by the interface of getBundleInfo
+    */
+    it('bms_getJsAbility_0100', 0, async function (done) {
+        let result = await bundle.getBundleInfo(BUNDLE_NAME2, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        expect(result.abilityInfos.length).assertEqual(NUM_TWO);
+        if (result.abilityInfos.length == NUM_TWO) {
+            console.info("getBundleIno result" + JSON.stringify(result));
+            let abilityInfo1 = result.abilityInfos[0];
+            let abilityInfo2 = result.abilityInfos[1];
+            expect(abilityInfo1.name).assertEqual(ABILITIY_NAME1);
+            expect(abilityInfo2.name).assertEqual(ABILITIY_NAME6);
+            expect(abilityInfo1.label).assertEqual("$string:app_name");
+            expect(abilityInfo1.description).assertEqual("$string:mainability_description");
+            expect(abilityInfo1.icon).assertEqual("$media:icon");
+            expect(abilityInfo1.isVisible).assertEqual(true);
+            expect(abilityInfo1.permissions.length).assertEqual(0);
+            expect(abilityInfo1.deviceTypes[0]).assertEqual('default');
+            expect(abilityInfo1.process).assertEqual(BUNDLE_NAME2);
+            expect(abilityInfo1.uri).assertEqual("");
+            expect(abilityInfo1.bundleName).assertEqual(BUNDLE_NAME2);
+            expect(abilityInfo1.moduleName).assertEqual("entry");
+            expect(Object.keys(abilityInfo1.applicationInfo).length).assertLarger(0);
+            expect(abilityInfo1.type).assertEqual(1);
+            expect(abilityInfo2.type).assertEqual(2);
+            expect(abilityInfo1.orientation).assertEqual(0);
+            expect(abilityInfo1.launchMode).assertEqual(1);
+            expect(abilityInfo1.backgroundModes).assertEqual(0);
+            expect(abilityInfo2.backgroundModes).assertEqual(510);
+            expect(abilityInfo1.descriptionId).assertLarger(0);
+            expect(abilityInfo1.formEnabled).assertEqual(false);
+            expect(abilityInfo1.iconId).assertLarger(0);
+            expect(abilityInfo1.labelId).assertLarger(0);
+            expect(abilityInfo1.subType).assertEqual(0);
+            expect(abilityInfo1.readPermission).assertEqual("");
+            expect(abilityInfo1.writePermission).assertEqual("");
+            expect(abilityInfo1.targetAbility).assertEqual("");
+            expect(abilityInfo1.metaData.length).assertEqual(0);
+            expect(abilityInfo1.enabled).assertEqual(true);
+        }
+        done();
+    });
+
+    /*
+     * @tc.number: bms_getJsAbility_0200
+     * @tc.name: test the multi js ability
+     * @tc.desc: 1.install a js hap with two ability
+     *           2.check the ability name by the interface of getBundleInfo
+     */
+    it('bms_getJsAbility_0200', 0, async function (done) {
+        let result = await bundle.getBundleInfo(BUNDLE_NAME3, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        expect(result.abilityInfos.length).assertEqual(NUM_FOUR);
+        if (result.abilityInfos.length == NUM_FOUR) {
+            console.info("getBundleIno result" + JSON.stringify(result));
+            let abilityInfo1 = result.abilityInfos[0];
+            let abilityInfo2 = result.abilityInfos[1];
+            let abilityInfo3 = result.abilityInfos[2];
+            let abilityInfo4 = result.abilityInfos[3];
+            checkAbilityInfos(abilityInfo1);
+            expect(abilityInfo2.metaData.length).assertEqual(1);
+            expect(abilityInfo2.metaData[0].name).assertEqual("Data5B");
+            expect(abilityInfo2.metaData[0].value).assertEqual("float");
+            expect(abilityInfo2.metaData[0].extra).assertEqual("$string:mainability_description");
+            expect(abilityInfo2.name).assertEqual(ABILITIY_NAME3);
+            expect(abilityInfo3.name).assertEqual(ABILITIY_NAME7);
+            expect(abilityInfo4.name).assertEqual(ABILITIY_NAME8);
+            expect(abilityInfo3.type).assertEqual(2);
+            expect(abilityInfo4.type).assertEqual(2);
+            expect(abilityInfo3.backgroundModes).assertEqual(511);
+            expect(abilityInfo4.backgroundModes).assertEqual(129);
+        }
+        done();
+    });
+
+    /*
+     * @tc.number: bms_getJsAbility_0300
+     * @tc.name: test the multi js ability
+     * @tc.desc: 1.install a js hap which has an ability with short domain name
+     *           2.check the ability name by the interface of getBundleInfo
+     */
+    it('bms_getJsAbility_0300', 0, async function (done) {
+        let result = await bundle.getBundleInfo(BUNDLE_NAME4, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        expect(result.abilityInfos.length).assertEqual(1);
+        if (result.abilityInfos.length == 1) {
+            console.info("getBundleIno result" + JSON.stringify(result));
+            let abilityInfo1 = result.abilityInfos[0];
+            expect(abilityInfo1.name).assertEqual(ABILITIY_NAME4);
+            expect(abilityInfo1.label).assertEqual("$string:app_name");
+            expect(abilityInfo1.description).assertEqual("$string:mainability_description");
+            expect(abilityInfo1.icon).assertEqual("$media:icon");
+            expect(abilityInfo1.isVisible).assertEqual(true);
+            expect(abilityInfo1.permissions.length).assertEqual(0);
+            expect(abilityInfo1.deviceTypes[0]).assertEqual('default');
+            expect(abilityInfo1.process).assertEqual(BUNDLE_NAME4);
+            expect(abilityInfo1.uri).assertEqual("");
+            expect(abilityInfo1.bundleName).assertEqual(BUNDLE_NAME4);
+            expect(abilityInfo1.moduleName).assertEqual("entry");
+            expect(Object.keys(abilityInfo1.applicationInfo).length).assertLarger(0);
+            expect(abilityInfo1.type).assertEqual(1);
+            expect(abilityInfo1.orientation).assertEqual(0);
+            expect(abilityInfo1.launchMode).assertEqual(1);
+            expect(abilityInfo1.backgroundModes).assertEqual(0);
+            expect(abilityInfo1.descriptionId).assertLarger(0);
+            expect(abilityInfo1.formEnabled).assertEqual(false);
+            expect(abilityInfo1.iconId).assertLarger(0);
+            expect(abilityInfo1.labelId).assertLarger(0);
+            expect(abilityInfo1.subType).assertEqual(0);
+            expect(abilityInfo1.readPermission).assertEqual("");
+            expect(abilityInfo1.writePermission).assertEqual("");
+            expect(abilityInfo1.targetAbility).assertEqual("");
+            expect(abilityInfo1.metaData.length).assertEqual(0);
+            expect(abilityInfo1.enabled).assertEqual(true);
+        }
+        done();
+    });
+
+    /*
+    * @tc.number: bms_getJsAbility_0400
+    * @tc.name: test the multi js ability
+    * @tc.desc: 1.install a c++ hap which has an ability with short domain name
+    *           2.check the ability name by the interface of getBundleInfo
+    */
+    it('bms_getJsAbility_0400', 0, async function (done) {
+        let result = await bundle.getBundleInfo(BUNDLE_NAME5, bundle.BundleFlag.GET_BUNDLE_WITH_ABILITIES);
+        expect(result.abilityInfos.length).assertEqual(1);
+        if (result.abilityInfos.length == 1) {
+            console.info("getBundleIno result" + JSON.stringify(result));
+            let abilityInfo1 = result.abilityInfos[0];
+            expect(abilityInfo1.name).assertEqual(ABILITIY_NAME5);
+            expect(abilityInfo1.label).assertEqual("$string:app_name");
+            expect(abilityInfo1.description).assertEqual("$string:mainability_description");
+            expect(abilityInfo1.icon).assertEqual("$media:icon");
+            expect(abilityInfo1.isVisible).assertEqual(true);
+            expect(abilityInfo1.permissions.length).assertEqual(0);
+            expect(abilityInfo1.deviceTypes[0]).assertEqual('default');
+            expect(abilityInfo1.process).assertEqual(BUNDLE_NAME5);
+            expect(abilityInfo1.uri).assertEqual("");
+            expect(abilityInfo1.bundleName).assertEqual(BUNDLE_NAME5);
+            expect(abilityInfo1.moduleName).assertEqual("entry");
+            expect(Object.keys(abilityInfo1.applicationInfo).length).assertLarger(0);
+            expect(abilityInfo1.type).assertEqual(1);
+            expect(abilityInfo1.orientation).assertEqual(0);
+            expect(abilityInfo1.launchMode).assertEqual(1);
+            expect(abilityInfo1.backgroundModes).assertEqual(0);
+            expect(abilityInfo1.descriptionId).assertLarger(0);
+            expect(abilityInfo1.formEnabled).assertEqual(false);
+            expect(abilityInfo1.iconId).assertLarger(0);
+            expect(abilityInfo1.labelId).assertLarger(0);
+            expect(abilityInfo1.subType).assertEqual(0);
+            expect(abilityInfo1.readPermission).assertEqual("");
+            expect(abilityInfo1.writePermission).assertEqual("");
+            expect(abilityInfo1.targetAbility).assertEqual("");
+            expect(abilityInfo1.metaData.length).assertEqual(0);
+            expect(abilityInfo1.enabled).assertEqual(true);
+        }
+        done();
+    });
+
+    function checkAbilityInfos(info) {
+        expect(info.name).assertEqual(ABILITIY_NAME2);
+        expect(info.label).assertEqual("$string:app_name");
+        expect(info.description).assertEqual("$string:mainability_description");
+        expect(info.icon).assertEqual("$media:icon");
+        expect(info.isVisible).assertEqual(true);
+        expect(info.permissions.length).assertEqual(0);
+        expect(info.deviceTypes[0]).assertEqual('default');
+        expect(info.process).assertEqual(BUNDLE_NAME3);
+        expect(info.uri).assertEqual("");
+        expect(info.bundleName).assertEqual(BUNDLE_NAME3);
+        expect(info.moduleName).assertEqual("entry");
+        expect(Object.keys(info.applicationInfo).length).assertLarger(0);
+        expect(info.type).assertEqual(1);
+        expect(info.orientation).assertEqual(0);
+        expect(info.launchMode).assertEqual(1);
+        expect(info.backgroundModes).assertEqual(0);
+        expect(info.descriptionId).assertLarger(0);
+        expect(info.formEnabled).assertEqual(false);
+        expect(info.iconId).assertLarger(0);
+        expect(info.labelId).assertLarger(0);
+        expect(info.subType).assertEqual(0);
+        expect(info.readPermission).assertEqual("");
+        expect(info.writePermission).assertEqual("");
+        expect(info.targetAbility).assertEqual("");
+        expect(info.metaData.length).assertEqual(1);
+        expect(info.metaData[0].name).assertEqual("Data5A");
+        expect(info.metaData[0].value).assertEqual("float");
+        expect(info.metaData[0].extra).assertEqual("$string:mainability_description");
+        expect(info.enabled).assertEqual(true);
+    }
+})}
