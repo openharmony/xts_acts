2acb2c47acb1473d86c07d0bb0b713dbac9a5645
chengxingzhen <chengxingzhen@huawei.com>
2022-12-13 20:42:24 +0800

    xts签名文件整改
    Signed-off-by: chengxingzhen <chengxingzhen@huawei.com>

--- ability/ability_runtime/context/actsstagecontexttest/module1/src/main/ets/Application/MyAbilityStage.ts
+++ ability/ability_runtime/context/actsstagecontexttest/module1/src/main/ets/Application/MyAbilityStage.ts
@@ -14,7 +14,7 @@
  */
 
 import hilog from '@ohos.hilog';
-import AbilityStage from "@ohos.application.AbilityStage"
+import AbilityStage from "@ohos.app.ability.AbilityStage"
 
 export default class MyAbilityStage extends AbilityStage {
     onCreate() {
