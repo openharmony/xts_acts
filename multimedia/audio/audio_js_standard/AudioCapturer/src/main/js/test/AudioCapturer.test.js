8639b32b2399de6320f06f85a6c1ddaa0fa7d044
qinliwen <qinliwen3@huawei.com>
2022-12-13 15:51:08 +0800

    <avsession签名文件更改>
    Signed-off-by: qinliwen <qinliwen3@huawei.com>

--- multimedia/audio/audio_js_standard/AudioCapturer/src/main/js/test/AudioCapturer.test.js
+++ multimedia/audio/audio_js_standard/AudioCapturer/src/main/js/test/AudioCapturer.test.js
@@ -17,7 +17,7 @@ import audio from '@ohos.multimedia.audio';
 import fileio from '@ohos.fileio';
 import featureAbility from '@ohos.ability.featureAbility';
 import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from '@ohos/hypium';
-import { UiComponent, UiDriver, Component, Driver, UiWindow, ON, BY, MatchPattern, DisplayRotation, ResizeDirection, WindowMode, PointerMatrix } from '@ohos.uitest'
+import { UiDriver, BY } from '@ohos.uitest'
 export default function audioCapturer() {
 
     describe('audioCapturer', function () {
