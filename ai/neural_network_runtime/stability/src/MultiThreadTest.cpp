f9322c2ae8b343183916fcb002c858866bb15d7c
tangshihua <tangshihua@huawei.com>
2023-01-06 11:35:22 +0800

    稳定性用例修改
    Signed-off-by: tangshihua <tangshihua@huawei.com>

--- ai/neural_network_runtime/stability/src/MultiThreadTest.cpp
+++ ai/neural_network_runtime/stability/src/MultiThreadTest.cpp
@@ -41,20 +41,10 @@ protected:
     OHNNGraphArgs graphArgs = addModel.graphArgs;
 };
 
-void CompileModel(OH_NNCompilation *compilation, const OHNNCompileParam &compileParam)
-{
-    ASSERT_EQ(OH_NN_SUCCESS, CompileGraphMock(compilation, compileParam));
-}
-
-void ExecuteModel(OH_NNExecutor *executor, const OHNNGraphArgs &graphArgs)
-{
-    ASSERT_EQ(OH_NN_SUCCESS, ExecuteGraphMock(executor, graphArgs, nullptr));
-}
-
 
 /**
  * @tc.number : SUB_AI_NNR_Reliability_North_Stress_0100
- * @tc.name   : 模型编译多线程并发长稳测试
+ * @tc.name   : 模型编译并发长稳测试
  * @tc.desc   : [C- SOFTWARE -0200]
  */
 HWTEST_F(MultiThreadTest, SUB_AI_NNR_Reliability_North_Stress_0100, Reliability | MediumTest | Level2)
@@ -64,21 +54,11 @@ HWTEST_F(MultiThreadTest, SUB_AI_NNR_Reliability_North_Stress_0100, Reliability 
         ASSERT_NE(nullptr, model1);
         ASSERT_EQ(OH_NN_SUCCESS, BuildSingleOpGraph(model1, graphArgs));
 
-        OH_NNModel *model2 = OH_NNModel_Construct();
-        ASSERT_NE(nullptr, model2);
-        ASSERT_EQ(OH_NN_SUCCESS, BuildSingleOpGraph(model2, graphArgs));
-
         OH_NNCompilation *compilation1 = OH_NNCompilation_Construct(model1);
         ASSERT_NE(nullptr, compilation1);
-        OH_NNCompilation *compilation2 = OH_NNCompilation_Construct(model2);
-        ASSERT_NE(nullptr, compilation2);
+        ASSERT_EQ(OH_NN_SUCCESS, CompileGraphMock(compilation1, compileParam));
 
-        std::thread th1(CompileModel, compilation1, compileParam);
-        std::thread th2(CompileModel, compilation2, compileParam);
-        th1.join();
-        th2.join();
         Free(model1, compilation1);
-        Free(model2, compilation2);
         if (i % PRINT_FREQ == 0) {
             printf("[NnrtTest] SUB_AI_NNR_Reliability_North_Stress_0100 times: %d/%d\n", i, STRESS_COUNT);
         }
@@ -87,7 +67,7 @@ HWTEST_F(MultiThreadTest, SUB_AI_NNR_Reliability_North_Stress_0100, Reliability 
 
 /**
  * @tc.number : SUB_AI_NNR_Reliability_North_Stress_0200
- * @tc.name   : 模型推理多线程并发长稳测试
+ * @tc.name   : 模型推理并发长稳测试
  * @tc.desc   : [C- SOFTWARE -0200]
  */
 HWTEST_F(MultiThreadTest, SUB_AI_NNR_Reliability_North_Stress_0200, Reliability | MediumTest | Level2)
@@ -96,33 +76,16 @@ HWTEST_F(MultiThreadTest, SUB_AI_NNR_Reliability_North_Stress_0200, Reliability 
     ASSERT_NE(nullptr, model1);
     ASSERT_EQ(OH_NN_SUCCESS, BuildSingleOpGraph(model1, graphArgs));
 
-    OH_NNModel *model2 = OH_NNModel_Construct();
-    ASSERT_NE(nullptr, model2);
-    ASSERT_EQ(OH_NN_SUCCESS, BuildSingleOpGraph(model2, graphArgs));
-
     OH_NNCompilation *compilation1 = OH_NNCompilation_Construct(model1);
     ASSERT_NE(nullptr, compilation1);
     ASSERT_EQ(OH_NN_SUCCESS, CompileGraphMock(compilation1, compileParam));
 
-    OH_NNCompilation *compilation2 = OH_NNCompilation_Construct(model2);
-    ASSERT_NE(nullptr, compilation2);
-    ASSERT_EQ(OH_NN_SUCCESS, CompileGraphMock(compilation2, compileParam));
-
     for (int i = 0; i < STRESS_COUNT; i++) {
         OH_NNExecutor *executor1 = OH_NNExecutor_Construct(compilation1);
         ASSERT_NE(nullptr, executor1);
-
-        OH_NNExecutor *executor2 = OH_NNExecutor_Construct(compilation2);
-        ASSERT_NE(nullptr, executor2);
-
-        std::thread th1(ExecuteModel, executor1, graphArgs);
-        std::thread th2(ExecuteModel, executor2, graphArgs);
-        th1.join();
-        th2.join();
+        ASSERT_EQ(OH_NN_SUCCESS, ExecuteGraphMock(executor, graphArgs, nullptr));
         OH_NNExecutor_Destroy(&executor1);
         ASSERT_EQ(nullptr, executor1);
-        OH_NNExecutor_Destroy(&executor2);
-        ASSERT_EQ(nullptr, executor2);
         if (i % PRINT_FREQ == 0) {
             printf("[NnrtTest] SUB_AI_NNR_Reliability_North_Stress_0200 times: %d/%d\n", i, STRESS_COUNT);
         }
