1f4e0e7bc6e55710028e5951245805659f71dcdb
y00314596 <yangqing3@huawei.com>
2023-02-22 16:00:19 +0800

    add by yangqing3@huawei.com
    Signed-off-by: y00314596 <yangqing3@huawei.com>

--- global/i18n_standard/intljs/src/main/js/test/I18n.test.js
+++ global/i18n_standard/intljs/src/main/js/test/I18n.test.js
@@ -761,7 +761,7 @@ describe('I18nTest', function () {
         console.log('i18n_test_7810 ' + ran);
         let value = phonenumber.getLocationName('1351057467' + ran, 'zh-CN');
         console.log('i18n_test_7810 ' + value);
-        expect(value).assertEqual('中国');
+        expect(value == '中国' || value == '广东省深圳市').assertTrue();
     })
 
     /**
@@ -776,10 +776,10 @@ describe('I18nTest', function () {
         console.log('i18n_test_7820 ' + ran);
         let value = phonenumber.getLocationName('+861351057467' + ran, 'en-US');
         console.log('i18n_test_7820 ' + value);
-        expect(value).assertEqual('China');
+        expect(value == 'China' || value == 'Shenzhen, Guangdong').assertTrue();
         let value2 = phonenumber.getLocationName('+86 1351057467' + ran, 'en-US');
         console.log('i18n_test_7820 ' + value2);
-        expect(value2).assertEqual('China');
+        expect(value2 == 'China' || value2 == 'Shenzhen, Guangdong').assertTrue();
     })
 
     /**
@@ -794,10 +794,10 @@ describe('I18nTest', function () {
         console.log('i18n_test_7830 ' + ran);
         let value1 = phonenumber.getLocationName('+861586661737' + ran, 'en-US');
         console.log('i18n_test_7830 ' + value1);
-        expect(value1).assertEqual('China');
+        expect(value1 == 'China' || value1 == 'Jinan, Shandong').assertTrue();
         let value2 = phonenumber.getLocationName('+86 1586661737' + ran, 'en-US');
         console.log('i18n_test_7830 ' + value2);
-        expect(value2).assertEqual('China');
+        expect(value2 == 'China' || value2 == 'Jinan, Shandong').assertTrue();
     })
 
     /**
@@ -812,7 +812,7 @@ describe('I18nTest', function () {
         console.log('i18n_test_7840 ' + ran);
         let value = phonenumber.getLocationName('027-5937411' + ran, 'zh-CN');
         console.log('i18n_test_7840 ' + value);
-        expect(value).assertEqual('中国');
+        expect(value == '中国' || value == '湖北省武汉市').assertTrue();
     })
 
     /**
@@ -827,7 +827,7 @@ describe('I18nTest', function () {
         console.log('i18n_test_7850 ' + ran);
         let value = phonenumber.getLocationName('0728-623797' + ran, 'zh-CN');
         console.log('i18n_test_7850 ' + value);
-        expect(value).assertEqual('中国');
+        expect(value == '中国' || value == '湖北省仙桃市').assertTrue();
     })
 
     /**
