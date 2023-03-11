7a8078daf2192e6d66647418787597e7adcbb0b1
lixiaofeng_154b <lixiaofeng54@huawei.com>
2023-03-11 17:19:02 +0800

    适配用例
    Signed-off-by: lixiaofeng_154b <lixiaofeng54@huawei.com>

--- usb/usb_standard/src/main/js/test/UsbDevicePipeJsunit.test.js
+++ usb/usb_standard/src/main/js/test/UsbDevicePipeJsunit.test.js
@@ -33,6 +33,10 @@ describe('UsbDevicePipeJsFunctionsTest', function () {
     console.info('usb unit begin test getversion :' + Version)
     // version > 17  host currentMode = 2 device currentMode = 1
     var usbPortList = usb.getPorts()
+    if (usbPortList == undefined) {
+      portCurrentMode = 1;
+      return
+    }
     gDeviceList = usb.getDevices();
     if (usbPortList.length > 0) {
       if (gDeviceList.length > 0) {
