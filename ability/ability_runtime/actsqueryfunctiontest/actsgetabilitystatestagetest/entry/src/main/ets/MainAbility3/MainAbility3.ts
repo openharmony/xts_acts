8abd7ce945256417baf73f41aaed3f2f23396214
chengxingzhen <chengxingzhen@huawei.com>
2023-03-23 12:04:38 +0800

    xts-getCurrentTopAbility问题修改
    Signed-off-by: chengxingzhen <chengxingzhen@huawei.com>

--- ability/ability_runtime/actsqueryfunctiontest/actsgetabilitystatestagetest/entry/src/main/ets/MainAbility3/MainAbility3.ts
+++ ability/ability_runtime/actsqueryfunctiontest/actsgetabilitystatestagetest/entry/src/main/ets/MainAbility3/MainAbility3.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 /*
  * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,22 +14,34 @@
  */
 
 import Ability from '@ohos.app.ability.UIAbility'
+import commonEvent from '@ohos.commonEvent'
+
+function publishCallBackOne() {
+  console.log("====MainAbility3 Publish CallBack GetCurrentTopAbility");
+}
 export default class MainAbility3 extends Ability {
 
     onCreate(want, launchParam) {
         console.log("MainAbility3 onCreate")
     }
 
-    onDestroy(want) {
+    onDestroy() {
         // Ability is destroying, release resources for this ability
         console.log("MainAbility3 onDestroy")
     }
 
     onWindowStageCreate(windowStage) {
         // Main window is created, set main page for this ability
-        console.log("MainAbility3 onWindowStageCreate")
-        windowStage.setUIContent(this.context, "pages/index", null)
+        console.log("MainAbility3 onWindowStageCreate");
+        windowStage.setUIContent(this.context, "pages/index", null);
         globalThis.ability3Context = this.context;
+        windowStage.on('windowStageEvent', (data) => {
+          console.log(`ActsGetAbilityStatestAgeTest onWindwoStageMainAbilty3 is : ${JSON.stringify(data)}`);
+          if (data == 2 ) {
+            console.log(`ActsGetAbilityStatestAgeTest getWindowStageActiveMainAbility3 is sucess`);
+            commonEvent.publish("GetCurrentTopAbility", publishCallBackOne);
+          }
+        })
     }
 
     onWindowStageDestroy() {
