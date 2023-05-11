33e7a30cceacfd0bcca5ae70d0cf645f1d0fb7dc
y00314596 <yangqing3@huawei.com>
2023-05-11 10:43:56 +0800

    add by yangqing3@huawei.com
    Signed-off-by: y00314596 <yangqing3@huawei.com>

--- global/i18n_standard/intljs/src/main/js/test/List.test.js
+++ global/i18n_standard/intljs/src/main/js/test/List.test.js
@@ -12,13 +12,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import BreakIteratorInI18nTest from './BreakIteratorInI18n.test.js'
+import CalendarInI18nTest from './CalendarInI18n.test.js'
 import I18nTest from './I18n.test.js'
+import I18nUtilInI18nTest from './I18nUtilInI18n.test.js'
+import IndexUtilInI18nTest from './IndexUtilInI18n.test.js'
 import intlTest from './Intl.test.js'
+import PhoneNumberFormatInI18nTest from './PhoneNumberFormatInI18n.test.js'
 import LangTest from './Lang.test.js'
 import SystemInI18nTest from './SystemInI18n.test.js'
+import UtilInI18nTest from './UtilInI18n.test.js'
 export default function testsuite() {
+BreakIteratorInI18nTest()
+CalendarInI18nTest()
 I18nTest()
+I18nUtilInI18nTest()
+IndexUtilInI18nTest()
 intlTest()
+PhoneNumberFormatInI18nTest()
 LangTest()
 SystemInI18nTest()
+UtilInI18nTest()
 }
