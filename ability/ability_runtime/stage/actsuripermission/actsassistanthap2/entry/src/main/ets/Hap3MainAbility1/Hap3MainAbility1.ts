15cb1c64c5605e15b1743cff552f6fd789549aca
yuwenze <yuwenze1@huawei.com>
2023-05-30 07:15:31 +0000

    fix uri xts
    Signed-off-by: yuwenze <yuwenze1@huawei.com>
    Change-Id: I916403a2518b87d2a5649aaa10d8e08117952a2d

--- ability/ability_runtime/stage/actsuripermission/actsassistanthap2/entry/src/main/ets/Hap3MainAbility1/Hap3MainAbility1.ts
+++ ability/ability_runtime/stage/actsuripermission/actsassistanthap2/entry/src/main/ets/Hap3MainAbility1/Hap3MainAbility1.ts
@@ -17,89 +17,94 @@ import Ability from '@ohos.app.ability.UIAbility'
 import commonEvent from '@ohos.commonEvent';
 import fs from '@ohos.file.fs'
 
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
 export default class Hap3MainAbility1 extends Ability {
-    onCreate(want, launchParam) {
-        console.log('[Demo] Hap3MainAbility1 onCreate')
-        globalThis.abilityWant = want;
+  onCreate(want, launchParam) {
+    console.log('[Demo] Hap3MainAbility1 onCreate')
+    globalThis.abilityWant = want;
+  }
+
+  onDestroy() {
+    console.log('[Demo] Hap3MainAbility1 onDestroy')
+  }
+
+  onWindowStageCreate(windowStage) {
+    console.log('[Demo] Hap3MainAbility1 onWindowStageCreate')
+    windowStage.setUIContent(this.context, 'pages/Hap3MainAbility1_pages', null)
+  }
+
+  onWindowStageDestroy() {
+    console.log('[Demo] Hap3MainAbility1 onWindowStageDestroy')
+  }
+
+  onForeground() {
+    console.log('[Demo] Hap3MainAbility1 onForeground')
+    let eventName : string = 'TerminateNotify_Hap3MainAbility1';
+    let subscribeInfo = {
+      events: [eventName]
+    }
+    let subscriber_1;
+    function UnSubscribeInfoCallback_1(err, data) {
+      console.info("Hap2MainAbility1 ===UnSubscribeInfoCallback_1===")
     }
 
-    onDestroy() {
-        console.log('[Demo] Hap3MainAbility1 onDestroy')
-    }
-
-    onWindowStageCreate(windowStage) {
-        console.log('[Demo] Hap3MainAbility1 onWindowStageCreate')
-        windowStage.setUIContent(this.context, 'pages/Hap3MainAbility1_pages', null)
-    }
-
-    onWindowStageDestroy() {
-        console.log('[Demo] Hap3MainAbility1 onWindowStageDestroy')
-    }
-
-    onForeground() {
-        console.log('[Demo] Hap3MainAbility1 onForeground')
-        let eventName : string = 'TerminateNotify_Hap3MainAbility1';
-        let subscribeInfo = {
-            events: [eventName]
+    async function SubscribeCallBackContext(err, data) {
+      console.log('Hap3MainAbility1 Subscribe CallBack data:' + JSON.stringify(data));
+      if (data.event === eventName) {
+        await sleep(1000);
+        let uri : string = globalThis.abilityWant.uri;
+        if (uri == null) {
+          console.log('[Demo] Hap3MainAbility1 uri is invalid');
+          return;
         }
-        let subscriber_1;
-        function UnSubscribeInfoCallback_1(err, data) {
-          console.info("Hap2MainAbility1 ===UnSubscribeInfoCallback_1===")
-        }
-
-        function SubscribeCallBackContext(err, data) {
-          console.log('Hap3MainAbility1 Subscribe CallBack data:' + JSON.stringify(data));
-          if (data.event === eventName) {
-            let uri : string = globalThis.abilityWant.uri;
-            if (uri == null) {
-              console.log('[Demo] Hap3MainAbility1 uri is invalid');
-              return;
-            }
-            console.info('[Demo] Hap3MainAbility1 uri is ' + JSON.stringify(uri));
-            let eventName13_1 : string = 'Temp_UriPermissionTest_1300_Read_Successfully';
-            let eventName13_2 : string = 'Temp_UriPermissionTest_1300_Read_Failed';
-            fs.open(uri, fs.OpenMode.READ_ONLY).then((file) => {
-              console.info('file fd: ' + file.fd);
-              commonEvent.publish(eventName13_1, (err) => {
-                console.log('Hap3MainAbility1' + eventName13_1);
-              });
-            }).catch((err) => {
-              console.info('open file failed with error message: ' + err.message + ', error code: ' + err.code);
-              commonEvent.publish(eventName13_2, (err) => {
-                console.log('Hap3MainAbility1' + eventName13_2);
-              });
-            });
-
-            let eventName13_3 : string = 'Temp_UriPermissionTest_1300_Write_Successfully';
-            let eventName13_4 : string = 'Temp_UriPermissionTest_1300_Write_Failed';
-            fs.open(uri, fs.OpenMode.READ_WRITE).then((file) => {
-              console.info('file fd: ' + file.fd);
-              commonEvent.publish(eventName13_3, (err) => {
-                console.log('Hap3MainAbility1' + eventName13_3);
-              });
-            }).catch((err) => {
-              console.info('open file failed with error message: ' + err.message + ', error code: ' + err.code);
-              commonEvent.publish(eventName13_4, (err) => {
-                console.log('Hap3MainAbility1' + eventName13_4);
-              });
-            });
-          }
-          commonEvent.unsubscribe(subscriber_1, UnSubscribeInfoCallback_1)
-        }
-
-        commonEvent.createSubscriber(subscribeInfo).then(async (data) => {
-          subscriber_1 = data;
-          console.debug('====>Create Subscriber====>');
-          await commonEvent.subscribe(subscriber_1, SubscribeCallBackContext);
+        console.info('[Demo] Hap3MainAbility1 uri is ' + JSON.stringify(uri));
+        let eventName13_1 : string = 'Temp_UriPermissionTest_1300_Read_Successfully';
+        let eventName13_2 : string = 'Temp_UriPermissionTest_1300_Read_Failed';
+        fs.open(uri, fs.OpenMode.READ_ONLY).then((file) => {
+          console.info('file fd: ' + file.fd);
+          commonEvent.publish(eventName13_1, (err) => {
+            console.log('Hap3MainAbility1' + eventName13_1);
+          });
+        }).catch((err) => {
+          console.info('open file failed with error message: ' + err.message + ', error code: ' + err.code);
+          commonEvent.publish(eventName13_2, (err) => {
+            console.log('Hap3MainAbility1' + eventName13_2);
+          });
         });
 
-        let eventName1 : string = 'RevicedFrom_Hap3MainAbility1';
-        commonEvent.publish(eventName1, (err) => {
-            console.log('Hap3MainAbility1' + eventName1);
+        let eventName13_3 : string = 'Temp_UriPermissionTest_1300_Write_Successfully';
+        let eventName13_4 : string = 'Temp_UriPermissionTest_1300_Write_Failed';
+        fs.open(uri, fs.OpenMode.READ_WRITE).then((file) => {
+          console.info('file fd: ' + file.fd);
+          commonEvent.publish(eventName13_3, (err) => {
+            console.log('Hap3MainAbility1' + eventName13_3);
+          });
+        }).catch((err) => {
+          console.info('open file failed with error message: ' + err.message + ', error code: ' + err.code);
+          commonEvent.publish(eventName13_4, (err) => {
+            console.log('Hap3MainAbility1' + eventName13_4);
+          });
         });
+      }
+      commonEvent.unsubscribe(subscriber_1, UnSubscribeInfoCallback_1)
     }
 
-    onBackground() {
-        console.log('[Demo] Hap3MainAbility1 onBackground')
-    }
+    commonEvent.createSubscriber(subscribeInfo).then(async (data) => {
+      subscriber_1 = data;
+      console.debug('====>Create Subscriber====>');
+      await commonEvent.subscribe(subscriber_1, SubscribeCallBackContext);
+    });
+
+    let eventName1 : string = 'RevicedFrom_Hap3MainAbility1';
+    commonEvent.publish(eventName1, (err) => {
+      console.log('Hap3MainAbility1' + eventName1);
+    });
+  }
+
+  onBackground() {
+    console.log('[Demo] Hap3MainAbility1 onBackground')
+  }
 };
