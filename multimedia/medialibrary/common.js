435ea84247bd3e754088a5a43258f6b9c91ebbf6
yangbo <yangbo258@huawei.com>
2023-06-09 16:35:06 +0800

    medialibrary兼容api10
    Signed-off-by: yangbo <yangbo258@huawei.com>

--- multimedia/medialibrary/common.js
+++ multimedia/medialibrary/common.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -94,13 +94,13 @@ const fileIdFetchOps = function (testNum, id) {
     return ops;
 };
 
-const albumFetchOps = function (testNum, path, albumName, type, others) {
-    if (!others) {
-        others = { order: FILEKEY.DATE_ADDED + " DESC" };
-    }
+const albumFetchOps = function (testNum, albumName, others) {
+    // if (!others) {
+    //     others = { order: FILEKEY.DATE_ADDED + " DESC" };
+    // }
     let ops = {
-        selections: RELATIVE_PATH + "= ? AND " + ALBUM_NAME + "= ? AND " + MEDIA_TYPE + "= ?",
-        selectionArgs: [path, albumName, type.toString()],
+        selections: ALBUM_NAME + "= ?",
+        selectionArgs: [albumName],
         ...others,
     };
     console.info(`${testNum}: fetchOps${JSON.stringify(ops)}`);
@@ -170,12 +170,21 @@ const albumThreeTypesFetchOps = function (testNum, paths, albumName, types, othe
             ],
             ...others,
         };
-        console.info(`${testNum}: fetchOps${JSON.stringify(ops)}`);
+        console.info(`${testNum}: fetchOps ${JSON.stringify(ops)}`);
         return ops;
     } catch (error) {
         console.info(`albumThreeTypesFetchOps :: error: ${error}`);
     }
 };
+
+const fileFetchOption = function (testNum, selections, selectionArgs) {
+    let ops = {
+        selections: selections,
+        selectionArgs: selectionArgs,
+    };
+    console.info(`${testNum} fetchOps: ${JSON.stringify(ops)}`);
+    return ops;
+}
 
 const checkPresetsAssets = async function (media, hapName) {
     console.info("checkPresetsAssets start");
@@ -282,6 +291,7 @@ export {
     nameFetchOps,
     idFetchOps,
     albumFetchOps,
+    fileFetchOption,
     albumTwoTypesFetchOps,
     albumThreeTypesFetchOps,
     checkPresetsAssets,
