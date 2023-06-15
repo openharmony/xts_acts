59c50192b3fda4687dca3f13bbea382110079f63
yangbo <yangbo258@huawei.com>
2023-06-15 12:10:53 +0800

    update
    Signed-off-by: yangbo <yangbo258@huawei.com>

--- multimedia/medialibrary/common.js
+++ multimedia/medialibrary/common.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -95,9 +95,6 @@ const fileIdFetchOps = function (testNum, id) {
 };
 
 const albumFetchOps = function (testNum, albumName, others) {
-    // if (!others) {
-    //     others = { order: FILEKEY.DATE_ADDED + " DESC" };
-    // }
     let ops = {
         selections: ALBUM_NAME + "= ?",
         selectionArgs: [albumName],
