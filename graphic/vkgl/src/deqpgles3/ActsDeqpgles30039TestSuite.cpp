aec8a737d16e31a8ded7a33ae41cc150042a436c
wangshi <wangshi@kaihong.com>
2023-06-29 11:05:30 +0800

    "modifile graphic/vkgl/src/deqpgles3/ActsDeqpgles30039TestSuite.cpp 优化vkgl测试代码"
    Signed-off-by: wangshi <wangshi@kaihong.com>

--- graphic/vkgl/src/deqpgles3/ActsDeqpgles30039TestSuite.cpp
+++ graphic/vkgl/src/deqpgles3/ActsDeqpgles30039TestSuite.cpp
@@ -26,35 +26,4 @@ namespace OHOS {
     time_t ActsDeqpgles30039TestSuite::startTime;
     time_t ActsDeqpgles30039TestSuite::endTime;
     FuncRunResult ActsDeqpgles30039TestSuite::runResult;
-
-    // Preset action of the test suite, which is executed before the first test case
-    void ActsDeqpgles30039TestSuite::SetUpTestCase(void)
-    {
-        time(&startTime);
-        RegistPackage();
-        runResult.numPassed = 0;
-        runResult.numFailed = 0;
-        runResult.numNotSupported = 0;
-        runResult.numWarnings = 0;
-        runResult.numWaived = 0;
-    }
-    // Test suite cleanup action, which is executed after the last test case
-    void ActsDeqpgles30039TestSuite::TearDownTestCase(void)
-    {
-        time(&endTime);
-        printf("Test run totals --- Passed[%d]\n", runResult.numPassed);
-        printf("Test run totals --- Failed[%d]\n", runResult.numFailed);
-        printf("Test run totals --- Notsupport[%d]\n", runResult.numNotSupported);
-        printf("Test run totals --- Warnings[%d]\n", runResult.numWarnings);
-        printf("Test run totals --- Waved[%d]\n", runResult.numWaived);
-        cout << "testmain end --- COST TIME ["<< (endTime-startTime) << "]" << std::endl;;
-    }
-    // Preset action of the test case
-    void ActsDeqpgles30039TestSuite::SetUp(void)
-    {
-    }
-    // Cleanup action of the test case
-    void ActsDeqpgles30039TestSuite::TearDown(void)
-    {
-    }
 }
