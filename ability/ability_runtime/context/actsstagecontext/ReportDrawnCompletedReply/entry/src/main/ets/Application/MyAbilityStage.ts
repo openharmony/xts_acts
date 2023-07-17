93ffa4e6528f86bf1fe50d76396e0b92183adb98
xinking129 <xinxin13@huawei.com>
2023-07-17 22:35:36 +0800

    modify code
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/context/actsstagecontext/ReportDrawnCompletedReply/entry/src/main/ets/Application/MyAbilityStage.ts
+++ ability/ability_runtime/context/actsstagecontext/ReportDrawnCompletedReply/entry/src/main/ets/Application/MyAbilityStage.ts
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2022 Huawei Device Co., Ltd.
+* Copyright (c) 2023 Huawei Device Co., Ltd.
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -13,11 +13,11 @@
 * limitations under the License.
 */
 
-import AbilityStage from "@ohos.app.ability.AbilityStage"
+import AbilityStage from "@ohos.app.ability.AbilityStage";
 
 export default class MyAbilityStage extends AbilityStage {
-    onCreate() {
-        console.info("ActsStageContextAssistOne StageAbility onCreate")
-        globalThis.abilityStageContextA = this.context;
-    }
+  onCreate() {
+    console.info("ActsStageContextAssistOne StageAbility onCreate");
+    globalThis.abilityStageContextA = this.context;
+  }
 }
--- ability/ability_runtime/context/actsstagecontext/ReportDrawnCompletedReply/entry/src/main/ets/MainAbility/DrawnAbility.ts
+++ ability/ability_runtime/context/actsstagecontext/ReportDrawnCompletedReply/entry/src/main/ets/MainAbility/DrawnAbility.ts
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2022 Huawei Device Co., Ltd.
+* Copyright (c) 2023 Huawei Device Co., Ltd.
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -13,39 +13,39 @@
 * limitations under the License.
 */
 
-import Ability from '@ohos.app.ability.UIAbility'
-import commonEvent from '@ohos.commonEvent'
+import Ability from '@ohos.app.ability.UIAbility';
+import commonEvent from '@ohos.commonEvent';
 
 export default class DrawnAbility extends Ability {
-    onCreate(want, launchParam) {
-        console.info('ActsStageContextAssistOne DrawnAbility onCreate')
-        this.context.reportDrawnCompleted(() => {
-            console.info('The call to the reportDrawnCompleted interface succeeded');
-        });
-    }
+  onCreate(want, launchParam) {
+    console.info('ActsStageContextAssistOne DrawnAbility onCreate');
+    this.context.reportDrawnCompleted(() => {
+      console.info('The call to the reportDrawnCompleted interface succeeded');
+    });
+  }
 
-    onDestroy() {
-        console.info('ActsStageContextAssistOne DrawnAbility onDestroy')
-    }
+  onDestroy() {
+    console.info('ActsStageContextAssistOne DrawnAbility onDestroy');
+  }
 
-    onWindowStageCreate(windowStage) {
-        console.info('ActsStageContextAssistOne DrawnAbility onWindowStageCreate');
-        windowStage.setUIContent(this.context, 'pages/index', null)
-    }
+  onWindowStageCreate(windowStage) {
+    console.info('ActsStageContextAssistOne DrawnAbility onWindowStageCreate');
+    windowStage.setUIContent(this.context, 'pages/index', null);
+  }
 
-    onWindowStageDestroy() {
-        console.info('ActsStageContextAssistOne DrawnAbility onWindowStageDestroy')
-    }
+  onWindowStageDestroy() {
+    console.info('ActsStageContextAssistOne DrawnAbility onWindowStageDestroy');
+  }
 
-    onForeground() {
-        console.info('ActsStageContextAssistOne DrawnAbility onForeground');
-        globalThis.abilityContextA = this.context;
-        commonEvent.publish("StartDrawnAbility_CommonEvent_ContextOne", () => {
-            console.log("Publish StartDrawnAbility_CommonEvent_ContextOne callback");
-        })
-    }
+  onForeground() {
+    console.info('ActsStageContextAssistOne DrawnAbility onForeground');
+    globalThis.abilityContextA = this.context;
+    commonEvent.publish("StartDrawnAbility_CommonEvent_ContextOne", () => {
+      console.log("Publish StartDrawnAbility_CommonEvent_ContextOne callback");
+    });
+  }
 
-    onBackground() {
-        console.info('ActsStageContextAssistOne DrawnAbility onBackground');
-    }
+  onBackground() {
+    console.info('ActsStageContextAssistOne DrawnAbility onBackground');
+  }
 }
