f7c21f77c4e0e0cc8541e11d939924f1380bd9be
xinking129 <xinxin13@huawei.com>
2023-07-17 11:00:17 +0800

    format code
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/actsnfcselecttypes/ActsNFCSelectTypesTestRelyFifth/entry/src/main/ets/entryability/EntryAbility.ts
+++ ability/ability_runtime/actsnfcselecttypes/ActsNFCSelectTypesTestRelyFifth/entry/src/main/ets/entryability/EntryAbility.ts
@@ -20,65 +20,65 @@ import commonEvent from '@ohos.commonEventManager';
 
 let message;
 let commonEventData = {
-    parameters: {
-        message: message,
-    }
+  parameters: {
+    message: message,
+  }
 };
 let timeout = 50;
 
 export default class EntryAbility extends UIAbility {
-    onCreate(want, launchParam) {
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Abilitytest4 onCreate');
-        globalThis.terminate = () => {
-            setTimeout(() => {
-                this.context.terminateSelf().then(() => {
-                    console.info('====>EntryAbility terminateSelf end');
-                }).catch((err) => {
-                    console.info('====>EntryAbility terminateSelf err:' + JSON.stringify(err));
-                });
-            }, timeout);
-        };
-        console.info('Ability4 onCreate' + JSON.stringify(want));
-        let actionStr = want.action;
-        if (actionStr === 'ohos.nfc.tag.action.TAG_FOUND') {
-            hilog.info(0x0000, 'testTag', '%{public}s', 'a4Ability ok');
-            commonEventData.parameters.message = 'select';
-            commonEvent.publish('ACTS_CROSS_CALL_EVENT', commonEventData, (err) => {
-                console.info('====>' + actionStr + ' apublish err:' + JSON.stringify(err));
-                globalThis.terminate();
+  onCreate(want, launchParam) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Abilitytest4 onCreate');
+    globalThis.terminate = () => {
+        setTimeout(() => {
+            this.context.terminateSelf().then(() => {
+                console.info('====>EntryAbility terminateSelf end');
+            }).catch((err) => {
+                console.info('====>EntryAbility terminateSelf err:' + JSON.stringify(err));
             });
-        }
-    }
-
-    onDestroy() {
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onDestroy');
-    }
-
-    onWindowStageCreate(windowStage: window.WindowStage) {
-        // Main window is created, set main page for this ability
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onWindowStageCreate');
-
-        windowStage.loadContent('pages/Index', (err, data) => {
-            if (err.code) {
-                hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
-                return;
-            }
-            hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+        }, timeout);
+    };
+    console.info('Ability4 onCreate' + JSON.stringify(want));
+    let actionStr = want.action;
+    if (actionStr === 'ohos.nfc.tag.action.TAG_FOUND') {
+        hilog.info(0x0000, 'testTag', '%{public}s', 'a4Ability ok');
+        commonEventData.parameters.message = 'select';
+        commonEvent.publish('ACTS_CROSS_CALL_EVENT', commonEventData, (err) => {
+            console.info('====>' + actionStr + ' apublish err:' + JSON.stringify(err));
+            globalThis.terminate();
         });
     }
+  }
 
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onWindowStageDestroy');
-    }
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onDestroy');
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onForeground');
-    }
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onWindowStageCreate');
 
-    onBackground() {
-        // Ability has back to background
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onBackground');
-    }
+    windowStage.loadContent('pages/Index', (err, data) => {
+        if (err.code) {
+            hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+            return;
+        }
+        hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onWindowStageDestroy');
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onForeground');
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability4 onBackground');
+  }
 }
