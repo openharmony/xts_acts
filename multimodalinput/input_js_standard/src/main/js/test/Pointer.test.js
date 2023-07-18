4d83fc3504502ac68c14056a883016daf1ede199
xixian_2023 <xixian@huawei.com>
2023-07-18 18:56:54 +0800

    mmi_touchPad_set_enum_acts Signed-off-by:xixian_2023<xixian@huawei.com>

--- multimodalinput/input_js_standard/src/main/js/test/Pointer.test.js
+++ multimodalinput/input_js_standard/src/main/js/test/Pointer.test.js
@@ -433,6 +433,18 @@ export default function Pointer_test() {
       expect(pointer.PointerStyle.CURSOR_CIRCLE == 41).assertTrue();
     })
 
+    it('Touchpad_RightClickType_test', 0, function () {
+      console.info('Touchpad_TOUCHPAD_RIGHT_BUTTON_test = ' + pointer.RightClickType.TOUCHPAD_RIGHT_BUTTON);
+      expect(pointer.RightClickType.TOUCHPAD_RIGHT_BUTTON == 1).assertTrue();
+
+      console.info('Touchpad_TOUCHPAD_LEFT_BUTTON_test = ' + pointer.RightClickType.TOUCHPAD_LEFT_BUTTON);
+      expect(pointer.RightClickType.TOUCHPAD_LEFT_BUTTON == 2).assertTrue();
+
+      console.info('Touchpad_TOUCHPAD_TWO_FINGER_TAP_test = ' + pointer.RightClickType.TOUCHPAD_TWO_FINGER_TAP);
+      expect(pointer.RightClickType.TOUCHPAD_TWO_FINGER_TAP == 3).assertTrue();
+    })
+
+    
     /**
      * @tc.number MultimodalInputPointer_Test_001
      * @tc.name MultimodalInputDevice_PointerVisibleTest_Exception_Test_001
