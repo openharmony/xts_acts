6a2a901b20862ab41bcc0ef4b99de7d3649561db
15929983503 <duanhaizhen@huawei.com>
2023-07-21 11:38:20 +0800

    测试套内存泄漏整改
    Signed-off-by: 15929983503 <duanhaizhen@huawei.com>

--- hiviewdfx/hitracemetertest/src/main/js/test/hitracemeter_test.js
+++ hiviewdfx/hitracemetertest/src/main/js/test/hitracemeter_test.js
@@ -21,9 +21,6 @@ describe("HitraceMeterTest", function () {
         console.info("-------DFX_DFR_HitraceMeter_0100 start--------");
         for (var i = 0; i < 3; i++) {
             console.info("DFX_DFR_HitraceMeter_0100 i = " + i)
-            console.info("DFX_DFR_HitraceMeter_0100 " + i.toString() + " startTrace start");
-            hiTraceMeter.startTrace("test-" + i.toString(), 111 + i, 5000);
-            console.info("DFX_DFR_HitraceMeter_0100 " + i.toString() + "startTrace end");
             console.info("DFX_DFR_HitraceMeter_0100 " + i.toString() + " finishTrace start");
             hiTraceMeter.finishTrace("test-" + i.toString(), 111 + i);
             console.info("DFX_DFR_HitraceMeter_0100 " + i.toString() + "finishTrace end");
