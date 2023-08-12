df26a02738e114fa49e15b8198580424648bc174
huyunhui <huyunhui1@huawei.com>
2023-08-12 18:42:59 +0800

    Standardize ArkTs files in xts testcases of esmodule
    Issue: https://gitee.com/openharmony/xts_acts/issues/I7SP5J
    
    Signed-off-by: huyunhui <huyunhui1@huawei.com>

--- arkcompiler/esmodule/esmodule_entry/entry/src/main/ets/test/Util.ts
+++ arkcompiler/esmodule/esmodule_entry/entry/src/main/ets/test/Util.ts
@@ -12,6 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const Sleep = (ms)=> {
+export const Sleep = (ms: number | undefined)=> {
   return new Promise(resolve=>setTimeout(resolve, ms))
 }
