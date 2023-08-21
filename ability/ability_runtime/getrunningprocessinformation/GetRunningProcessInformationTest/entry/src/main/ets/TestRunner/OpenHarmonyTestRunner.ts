8f13334ab5e0a4e3066492ec3ffb5eb2d8e46a0e
xinking129 <xinxin13@huawei.com>
2023-08-21 20:34:44 +0800

    xts new d.ts
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/getrunningprocessinformation/GetRunningProcessInformationTest/entry/src/main/ets/TestRunner/OpenHarmonyTestRunner.ts
+++ ability/ability_runtime/getrunningprocessinformation/GetRunningProcessInformationTest/entry/src/main/ets/TestRunner/OpenHarmonyTestRunner.ts
@@ -14,7 +14,7 @@
  */
 import hilog from '@ohos.hilog';
 import TestRunner from '@ohos.application.testRunner'
-import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry'
+import AbilityDelegatorRegistry from '@ohos.app.ability.abilityDelegatorRegistry'
 
 let abilityDelegator = undefined;
 let abilityDelegatorArguments = undefined;
