5f89af03b95116f1fda520dcb92e7d17b50674ea
lixinyi1094590959 <lixinyi38@huawei.com>
2023-08-26 13:06:59 +0000

    update KvStoreBackupPromiseJsTest.js.
    Signed-off-by: lixinyi1094590959 <lixinyi38@huawei.com>

--- distributeddatamgr/distributedKVStoretest/distributedKVStorejstest/hap/src/main/js/test/KvStoreBackupPromiseJsTest.js
+++ distributeddatamgr/distributedKVStoretest/distributedKVStorejstest/hap/src/main/js/test/KvStoreBackupPromiseJsTest.js
@@ -63,6 +63,7 @@ function publicCloseKvStore() {
                     reject(err);
                 }
                 console.info('Test deleteKVStore data = ' + data);
+                resolve(data);
             });
         });
     })
