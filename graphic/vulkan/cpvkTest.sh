8029522cbd9a7e0f72493660b7da842679273f3e
wangshi <wangshi@kaihong.com>
2023-08-31 11:54:00 +0800

    fix cpvk shell
    Signed-off-by: wangshi <wangshi@kaihong.com>

--- graphic/vulkan/cpvkTest.sh
+++ graphic/vulkan/cpvkTest.sh
@@ -14,17 +14,21 @@
 # limitations under the License.
 
 set -e
-root_vkgl_path=$1
-mkdir -p  "suites/acts/testcases/vkTestdata"
-cp -rf "$root_vkgl_path/third_party/vk-gl-cts/external/vulkancts/data/vulkan" "suites/acts/testcases/vkTestdata"
+
+mkdir -p  "suites/acts/testcases/vulkandata"
+cp -rf "../../third_party/vk-gl-cts/external/vulkancts/mustpass/master/vk-default" "suites/acts/testcases/vulkandata"
+cp -rf "../../third_party/vk-gl-cts/external/vulkancts/mustpass/master/src/fraction-mandatory-tests.txt" "suites/acts/testcases/vulkandata/vk-default"
+cp -rf "../../third_party/vk-gl-cts/external/vulkancts/mustpass/master/src/master.txt" "suites/acts/testcases/vulkandata/vk-default"
+cp -rf "../../third_party/vk-gl-cts/external/vulkancts/mustpass/master/src/test-issues.txt" "suites/acts/testcases/vulkandata/vk-default"
+cp -rf "../../third_party/vk-gl-cts/external/vulkancts/data/vulkan" "suites/acts/testcases/vulkandata"
 cp -rf "xts/acts/deqp_vk_execute" "suites/acts/testcases/vulkandata/vk-default"
 
-tar czvf "vkTestdata.tar.gz" -C "suites/acts/testcases/vkTestdata" "."
-mv "vkTestdata.tar.gz" "suites/acts/testcases/vkTestdata"
+tar czvf "vulkandata.tar.gz" -C "suites/acts/testcases/vulkandata" "."
+mv "vulkandata.tar.gz" "suites/acts/testcases/vulkandata"
 
-cp -rf "graphic/graphic_2d/libdeqp_ohos_platform.z.so" "suites/acts/testcases/vkTestdata"
-cp -rf "graphic/graphic_2d/librosen_context.z.so" "suites/acts/testcases/vkTestdata"
-cp -rf "graphic/graphic_2d/libdeqp_spirv.z.so" "suites/acts/testcases/vkTestdata"
-cp -rf "graphic/graphic_2d/libdeqp_spvremapper.z.so" "suites/acts/testcases/vkTestdata"
-cp -rf "xts/acts/libdeqp_amber.z.so" "suites/acts/testcases/vkTestdata"
-cp -rf "thirdparty/skia/libskia_canvaskit.z.so" "suites/acts/testcases/vkTestdata"
+cp -rf "graphic/graphic_2d/libdeqp_ohos_platform.z.so" "suites/acts/testcases/vulkandata"
+cp -rf "graphic/graphic_2d/librosen_context.z.so" "suites/acts/testcases/vulkandata"
+cp -rf "xts/acts/libdeqp_amber.z.so" "suites/acts/testcases/vulkandata"
+cp -rf "graphic/graphic_2d/libdeqp_spirv.z.so" "suites/acts/testcases/vulkandata"
+cp -rf "graphic/graphic_2d/libdeqp_spvremapper.z.so" "suites/acts/testcases/vulkandata"
+cp -rf "thirdparty/skia/libskia_canvaskit.z.so" "suites/acts/testcases/vulkandata"
