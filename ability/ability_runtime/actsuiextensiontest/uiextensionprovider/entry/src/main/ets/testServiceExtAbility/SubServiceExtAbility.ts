40fa6dffdbd66b0b3b4a4e59f954867050392a43
renqing <renqing8@huawei.com>
2023-09-05 14:44:40 +0800

    给UIExtension增加XTS：多UIExtension连接同一service，处于后台有两个权限测试用例 and 回到桌面和灭屏验证
    Signed-off-by: renqing <renqing8@huawei.com>
    Change-Id: I69e1cc72d5d038a706dc007324c73160f7003bb9

--- ability/ability_runtime/actsuiextensiontest/uiextensionprovider/entry/src/main/ets/testServiceExtAbility/SubServiceExtAbility.ts
+++ ability/ability_runtime/actsuiextensiontest/uiextensionprovider/entry/src/main/ets/testServiceExtAbility/SubServiceExtAbility.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import ServiceExtension from '@ohos.app.ability.ServiceExtensionAbility'
 import SubServiceExtStub from '../IdlServiceExt/SubService/sub_service_ext_stub';
 import { subCallback } from '../IdlServiceExt/SubService/sub_service_ext';
@@ -14,23 +29,23 @@ class ServiceExtStub extends SubServiceExtStub{
 
 export default class SubServiceExtAbility extends ServiceExtension {
   onCreate(want) {
-    Logger.info(`tss onCreate, want: ${want.abilityName}`)
+    Logger.info(`___SubServiceExtAbility onCreate, want: ${want.abilityName}`)
   }
 
   onRequest(want, startId) {
-    Logger.info(`tss onRequest, want: ${want.abilityName}`)
+    Logger.info(`___SubServiceExtAbility onRequest, want: ${want.abilityName}`)
   }
 
   onConnect(want) {
-    Logger.info(`onConnect , want: ${want.abilityName}`)
+    Logger.info(`___SubServiceExtAbility onConnect , want: ${want.abilityName}`)
     return new ServiceExtStub("sub service stub")
   }
 
   onDisconnect(want) {
-    Logger.info(`onDisconnect, want: ${want.abilityName}`)
+    Logger.info(`___SubServiceExtAbility onDisconnect, want: ${want.abilityName}`)
   }
 
   onDestroy() {
-    Logger.info(`onDestroy`)
+    Logger.info(`___SubServiceExtAbility onDestroy`)
   }
 }
