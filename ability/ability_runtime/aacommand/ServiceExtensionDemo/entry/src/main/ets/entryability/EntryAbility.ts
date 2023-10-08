34b0d7c46492fb35a41bcce73838c68e14dae34d
xinking129 <xinxin13@huawei.com>
2023-10-08 16:16:34 +0800

    attach xts
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- ability/ability_runtime/aacommand/ServiceExtensionDemo/entry/src/main/ets/entryability/EntryAbility.ts
+++ ability/ability_runtime/aacommand/ServiceExtensionDemo/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,6 +1,21 @@
+/*
+* Copyright (c) 2023 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
-import window from '@ohos.window';
+import type window from '@ohos.window';
 
 globalThis.StartServiceAbility = () => {
   let want = {
@@ -10,7 +25,7 @@ globalThis.StartServiceAbility = () => {
   };
   globalThis.abilityContext.startServiceExtensionAbility(want, (error) => {
     console.log("Start service extension ability:  " + JSON.stringify(error))
-  })
+  });
 };
 
 globalThis.StartServiceAbilityAbnormally = () => {
@@ -22,7 +37,7 @@ globalThis.StartServiceAbilityAbnormally = () => {
   };
   globalThis.abilityContext.startServiceExtensionAbility(want, (error) => {
     console.log("Start service extension ability abnormally:  " + JSON.stringify(error))
-  })
+  });
 }
 
 globalThis.StopServiceAbility = () => {
@@ -33,7 +48,7 @@ globalThis.StopServiceAbility = () => {
   };
   globalThis.abilityContext.stopServiceExtensionAbility(want, (error) => {
     console.log("Stop service extension ability:  " + JSON.stringify(error));
-  })
+  });
 };
 
 globalThis.StartAbility = () => {
@@ -44,7 +59,7 @@ globalThis.StartAbility = () => {
   };
   globalThis.abilityContext.startAbility(want, (error) => {
     console.log("Start UIAbility: " + JSON.stringify(error));
-  })
+  });
 }
 
 export default class EntryAbility extends UIAbility {
