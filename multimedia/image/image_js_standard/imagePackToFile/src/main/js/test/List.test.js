7d16d82f6b63660373abff0da29871757162f8c8
Cao Chuan <caochuan@huawei.com>
2023-10-26 17:08:38 +0800

    Modify api name
    Signed-off-by: Cao Chuan <caochuan@huawei.com>

--- multimedia/image/image_js_standard/imagePackToFile/src/main/js/test/List.test.js
+++ multimedia/image/image_js_standard/imagePackToFile/src/main/js/test/List.test.js
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import imagePackingToFile from './PackingToFile.test.js'
+import imagePackToFile from './PackToFile.test.js'
 export default function testsuite() {
-    imagePackingToFile()
+    imagePackToFile()
 }
