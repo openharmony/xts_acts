05e48515673dabf2293f350f7f6263a5095d8717
shitijun <shitijun@kaihong.com>
2023-10-27 17:25:59 +0800

    "add vulkan cases change vktest to serveral testsuite"
    Signed-off-by: shitijun <shitijun@kaihong.com>

---
+++ graphic/vktest/src/meshshadertest/ActsVulkanMesh_shaderTest.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include "ActsVulkanMesh_shaderTest.h"
+
+namespace OHOS {
+    using namespace std;
+    using namespace testing::ext;
+
+    // Preset action of the test suite, which is executed before the first test case
+    void ActsVulkanMesh_shaderTest::SetUpTestCase(void)
+    {
+    }
+    // Test suite cleanup action, which is executed after the last test case
+    void ActsVulkanMesh_shaderTest::TearDownTestCase(void)
+    {
+    }
+    // Preset action of the test case
+    void ActsVulkanMesh_shaderTest::SetUp()
+    {
+    }
+    // Cleanup action of the test case
+    void ActsVulkanMesh_shaderTest::TearDown()
+    {
+    }
+
+    HWTEST_F(ActsVulkanMesh_shaderTest, TestVulkanMesh_shaderTestCase, Function | MediumTest | Level2)
+    {
+        printf("------start ActsVulkanMesh_shaderTest------\n");
+        system("/data/local/tmp/vulkan/vk-default/deqp_vk_execute --deqp-caselist-file=/data/local/tmp/vktest/vk-default/mesh-shader.txt --deqp-log-filename=ActsVulkanMesh_shaderTest.qpa");
+        EXPECT_TRUE(true);
+        printf("------end ActsVulkanMesh_shaderTest------\n");
+    }
+}
