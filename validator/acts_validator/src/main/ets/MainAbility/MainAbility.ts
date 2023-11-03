5c5e955ebd5dd4d5a79e9df195a807434f207acb
tiankonglin <tiankonglin@openvalley.net>
2023-11-03 11:32:08 +0800

    新增传感器、屏幕、网络视频播放模块
    Signed-off-by: tiankonglin <tiankonglin@openvalley.net>

--- validator/acts_validator/src/main/ets/MainAbility/MainAbility.ts
+++ validator/acts_validator/src/main/ets/MainAbility/MainAbility.ts
@@ -9,6 +9,7 @@ export default class MainAbility extends Ability {
         console.log("[Demo] MainAbility onCreate")
         globalThis.abilityWant = want;
         globalThis.abilityContext = this.context;
+        globalThis.shotScreenContext = this.context;
         globalThis.showFloatingWindow = false;
         BundleManager.getAppList().then(appList =>{
             globalThis.appList = appList
@@ -23,17 +24,62 @@ export default class MainAbility extends Ability {
 
         console.log("[Demo] MainAbility onWindowStageCreate")
         let AtManager = abilityAccessCtrl.createAtManager();
-        AtManager.requestPermissionsFromUser(this.context,["ohos.permission.READ_MEDIA","ohos.permission.WRITE_MEDIA",
-        "ohos.permission.CAPTURE_SCREEN","ohos.permission.INTERNET","ohos.permission.CAMERA",
+        AtManager.requestPermissionsFromUser(this.context, ["ohos.permission.READ_MEDIA", "ohos.permission.WRITE_MEDIA",
+        "ohos.permission.CAPTURE_SCREEN", "ohos.permission.INTERNET", "ohos.permission.CAMERA",
         "ohos.permission.MICROPHONE", "ohos.permission.START_INVISIBLE_ABILITY",
-        "ohos.permission.SYSTEM_FLOAT_WINDOW","ohos.permission.GET_BUNDLE_INFO_PRIVILEGED"]).then(() => {})
+        "ohos.permission.SYSTEM_FLOAT_WINDOW", "ohos.permission.GET_BUNDLE_INFO_PRIVILEGED",
+        "ohos.permission.GET_INSTALLED_BUNDLE_LIST","ohos.permission.MEDIA_LOCATION","ohos.permission.WRITE_IMAGEVIDEO","ohos.permission.READ_IMAGEVIDEO"]).then(() => {
+        })
         windowStage.loadContent("pages/index", (err, data) => {
-            if (err.code) {
-                console.error('Failed to load the content. Cause:' + JSON.stringify(err));
-                return;
-            }
-            console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
-        });
+        if (err.code) {
+            console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+            return;
+        }
+        console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
+       });
+        windowStage.getMainWindow((err, data) => {
+        if (err.code) {
+            return;
+        }
+        globalThis.mainWindow = data;
+      });
+    }
+    windowStage.loadContent("pages/index", (err, data) => {
+      if (err.code) {
+        console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+        return;
+      }
+      console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
+    });
+    windowStage.getMainWindow((err, data) => {
+      if (err.code) {
+        return;
+      }
+      globalThis.mainWindow = data;
+    });
+  }
+
+        console.log("[Demo] MainAbility onWindowStageCreate")
+    let AtManager = abilityAccessCtrl.createAtManager();
+    AtManager.requestPermissionsFromUser(this.context, ["ohos.permission.READ_MEDIA", "ohos.permission.WRITE_MEDIA",
+      "ohos.permission.CAPTURE_SCREEN", "ohos.permission.INTERNET", "ohos.permission.CAMERA",
+      "ohos.permission.MICROPHONE", "ohos.permission.START_INVISIBLE_ABILITY",
+      "ohos.permission.SYSTEM_FLOAT_WINDOW", "ohos.permission.GET_BUNDLE_INFO_PRIVILEGED",
+      "ohos.permission.GET_INSTALLED_BUNDLE_LIST","ohos.permission.MEDIA_LOCATION","ohos.permission.WRITE_IMAGEVIDEO","ohos.permission.READ_IMAGEVIDEO"]).then(() => {
+    })
+    windowStage.loadContent("pages/index", (err, data) => {
+      if (err.code) {
+        console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+        return;
+      }
+      console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
+    });
+    windowStage.getMainWindow((err, data) => {
+      if (err.code) {
+        return;
+      }
+      globalThis.mainWindow = data;
+    });
     }
 
     onWindowStageDestroy() {
