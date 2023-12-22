c10b47601244fb691d8774137c299bd0eac8b9a8
liuxiyao223 <liuxiyao223@huawei.com>
2023-12-22 15:54:12 +0800

    add websocket code
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

---
+++ communication/netstack_websocket/entry/src/main/cpp/websocketNdkTest.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hilog/log.h"
+#include "napi/native_api.h"
+#include "net_websocket.h"
+#include <cstring>
+#include <signal.h>
+#include <string.h>
+
+#define WEBSOCKET_LOG_TAG "websocket XTS"
+#define WEBSOCKET_LOG_DOMAIN 0xD0015B0
+
+static constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, WEBSOCKET_LOG_DOMAIN, WEBSOCKET_LOG_TAG};
+
+static void OnOpen(struct WebSocket *client, WebSocket_OpenResult openResult)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OnOpen  at line：%{public}d", __LINE__);
+}
+
+static void OnMessage(struct WebSocket *client, char *data, uint32_t length)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OnMessage  at line：%{public}d,data=：%{public}s", __LINE__, data);
+}
+
+static void OnError(struct WebSocket *client, WebSocket_ErrorResult error)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OnError  at line：%{public}d", __LINE__);
+}
+
+static void OnClose(struct WebSocket *client, WebSocket_CloseResult closeResult)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OnClose  at line：%{public}d", __LINE__);
+}
+
+static napi_value OHWebsocketClientConstruct(napi_env env, napi_callback_info)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OHWebsocketClientConstruct start");
+    int ret;
+    struct WebSocket *client = new WebSocket();
+    const char *url = "www.baidu.com";
+    client = OH_WebSocketClient_Constructor(OnOpen, OnMessage, OnError, OnClose);
+    ret = OH_WebSocketClient_Connect(client, url, client->requestOptions);
+    napi_value result;
+    napi_create_int32(env, ret, &result);
+    return result;
+}
+
+static napi_value OHWebSocketClientAddHeader(napi_env env, napi_callback_info)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OHWebSocketClientAddHeader start");
+    napi_value result = nullptr;
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "test case begin:TestConnet,  at line：%{public}d", __LINE__);
+    struct WebSocket *client = new WebSocket();
+    struct WebSocket_Header header1;
+    header1.fieldName = "Content-Type";
+    header1.fieldValue = "application/json";
+    header1.next = nullptr;
+    client = OH_WebSocketClient_Constructor(OnOpen, OnMessage, OnError, OnClose);
+    int ret = OH_WebSocketClient_AddHeader(client, header1);
+    napi_create_int32(env, ret, &result);
+    return result;
+}
+
+static napi_value OHWebSocketClientConnect(napi_env env, napi_callback_info info)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OHWebSocketClientConnect start");
+    struct WebSocket *client = new WebSocket();
+    const char *url1 = "www.baidu.com";
+    client = OH_WebSocketClient_Constructor(OnOpen, OnMessage, OnError, OnClose);
+    int ret = OH_WebSocketClient_Connect(client, url1, client->requestOptions);
+    napi_value result;
+    napi_create_int32(env, ret, &result);
+    return result;
+}
+
+static napi_value OHWebSocketClientSend(napi_env env, napi_callback_info info)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OHWebSocketClientSend start");
+    napi_value result = nullptr;
+    struct WebSocket *client = new WebSocket();
+    client = OH_WebSocketClient_Constructor(OnOpen, OnMessage, OnError, OnClose);
+    const char *data = "Hello, world!";
+    int SendLength = std::strlen(data);
+    int ret = OH_WebSocketClient_Send(client, const_cast<char *>(data), SendLength);
+    napi_create_int32(env, ret, &result);
+    return result;
+}
+
+static napi_value OHWebSocketClientClose(napi_env env, napi_callback_info info)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OHWebSocketClientClose start");
+    napi_value result = nullptr;
+    struct WebSocket *client = new WebSocket();
+    struct WebSocket_CloseOption CloseOption;
+    CloseOption.code = 1000;
+    CloseOption.reason = " ";
+    int ret = OH_WebSocketClient_Close(client, CloseOption);
+    napi_create_int32(env, ret, &result);
+    return result;
+}
+
+static napi_value OHWebsocketClientDestroy(napi_env env, napi_callback_info info)
+{
+    OHOS::HiviewDFX::HiLog::Error(LABEL, "OHWebsocketClientDestroy start");
+    napi_value result = nullptr;
+    struct WebSocket *client = new WebSocket();
+    struct WebSocket_CloseOption CloseOption;
+    CloseOption.code = 1000;
+    CloseOption.reason = " ";
+    int ret = OH_WebSocketClient_Destroy(client);
+    napi_create_int32(env, ret, &result);
+    return result;
+}
+
+EXTERN_C_START
+static napi_value Init(napi_env env, napi_value exports)
+{
+    napi_property_descriptor desc[] = {
+        {"oHWebsocketClientConstruct", nullptr, OHWebsocketClientConstruct, nullptr, nullptr, nullptr, napi_default,
+         nullptr},
+        {"oHWebSocketClientAddHeader", nullptr, OHWebSocketClientAddHeader, nullptr, nullptr, nullptr, napi_default,
+         nullptr},
+        {"oHWebSocketClientConnect", nullptr, OHWebSocketClientConnect, nullptr, nullptr, nullptr, napi_default,
+         nullptr},
+        {"oHWebSocketClientSend", nullptr, OHWebSocketClientSend, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"oHWebSocketClientClose", nullptr, OHWebSocketClientClose, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"oHWebsocketClientDestroy", nullptr, OHWebsocketClientDestroy, nullptr, nullptr, nullptr, napi_default,
+         nullptr},
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    return exports;
+}
+EXTERN_C_END
+
+static napi_module demoModule = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Init,
+    .nm_modname = "entry",
+    .nm_priv = ((void *)0),
+    .reserved = {0},
+};
+
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
+{
+    napi_module_register(&demoModule);
+}
