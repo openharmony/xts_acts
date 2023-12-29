366f40fbfd63bc29007e93cb6451d3ef3c13713c
liyujie <liyujie43@huawei.com>
2023-12-29 15:59:06 +0800

    fixed 7c91626 from https://gitee.com/liyujie43/xts_acts/pulls/11489 add flex three hap
    Signed-off-by: liyujie <liyujie43@huawei.com>
    Change-Id: I91f4c213b1c1c7a273f40e23e653c18f69dd2650

--- arkui/ace_ets_component_common_attrss/ace_ets_component_common_attrss_flex_three/entry/src/main/ets/Application/AbilityStage.ts
+++ arkui/ace_ets_component_common_attrss/ace_ets_component_common_attrss_flex_three/entry/src/main/ets/Application/AbilityStage.ts
@@ -13,8 +13,12 @@
  * limitations under the License.
  */
 
-import flexGrowTest from './flex/FlexGrowTest.test';
+import AbilityStage from '@ohos.app.ability.AbilityStage';
 
-export default function testsuite() {
-  flexGrowTest();
+export default class MyAbilityStage extends AbilityStage {
+  onCreate() {
+    console.log('[Demo] MyAbilityStage onCreate');
+    globalThis.stageOnCreateRun = 1;
+    globalThis.stageContext = this.context;
+  }
 }
