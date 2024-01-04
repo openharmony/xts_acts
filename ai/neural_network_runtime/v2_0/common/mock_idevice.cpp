47e6c883ee017f207a130093017831db6119eb31
w30052974 <wangyifan94@huawei.com>
2024-01-04 15:45:16 +0800

    hdi设备添加
    Signed-off-by: w30052974 <wangyifan94@huawei.com>

--- ai/neural_network_runtime/v2_0/common/mock_idevice.cpp
+++ ai/neural_network_runtime/v2_0/common/mock_idevice.cpp
@@ -12,8 +12,85 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <string>
+
 #include "const.h"
 #include "mock_idevice.h"
+#include "frameworks/native/hdi_device_v2_0.h"
+#include "frameworks/native/hdi_returncode_utils.h"
+#include "common/log.h"
+#include "common/utils.h"
+#include "frameworks/native/nnbackend.h"
+#include "frameworks/native/backend_registrar.h"
+
+namespace OHOS {
+namespace NeuralNetworkRuntime {
+std::shared_ptr<Backend> HDIDeviceV2_0Creator()
+{
+    std::string deviceName;
+    std::string vendorName;
+    std::string version;
+
+    // only one device from HDI now.
+    OHOS::sptr<V2_0::INnrtDevice> iDevice = V2_0::INnrtDevice::Get();
+    if (iDevice == nullptr) {
+        LOGW("Get HDI device failed.");
+        return nullptr;
+    }
+
+    auto ret = iDevice->GetDeviceName(deviceName);
+    int32_t nnrtSuccess = static_cast<int32_t>(V2_0::NNRT_ReturnCode::NNRT_SUCCESS);
+    if (ret != nnrtSuccess) {
+        if (ret < nnrtSuccess) {
+            LOGW("Get device name failed. An error occurred in HDI, errorcode is %{public}d.", ret);
+        } else {
+            OHOS::HDI::Nnrt::V2_0::NNRT_ReturnCode nnrtRet = static_cast<OHOS::HDI::Nnrt::V2_0::NNRT_ReturnCode>(ret);
+            LOGW("Get device name failed. Errorcode is %{public}s.", ConverterRetToString(nnrtRet).c_str());
+        }
+        return nullptr;
+    }
+
+    ret = iDevice->GetVendorName(vendorName);
+    if (ret != nnrtSuccess) {
+        if (ret < nnrtSuccess) {
+            LOGW("Get vendor name failed. An error occurred in HDI, errorcode is %{public}d.", ret);
+        } else {
+            OHOS::HDI::Nnrt::V2_0::NNRT_ReturnCode nnrtRet = static_cast<OHOS::HDI::Nnrt::V2_0::NNRT_ReturnCode>(ret);
+            LOGW("Get vendor name failed. Errorcode is %{public}s.", ConverterRetToString(nnrtRet).c_str());
+        }
+        return nullptr;
+    }
+
+    std::pair<uint32_t, uint32_t> hdiVersion;
+    ret = iDevice->GetVersion(hdiVersion.first, hdiVersion.second);
+    if (ret != nnrtSuccess) {
+        if (ret < nnrtSuccess) {
+            LOGW("Get version failed. An error occurred in HDI, errorcode is %{public}d.", ret);
+        } else {
+            OHOS::HDI::Nnrt::V2_0::NNRT_ReturnCode nnrtRet = static_cast<OHOS::HDI::Nnrt::V2_0::NNRT_ReturnCode>(ret);
+            LOGW("Get version failed. Errorcode is %{public}s.", ConverterRetToString(nnrtRet).c_str());
+        }
+        return nullptr;
+    }
+    version = 'v' + std::to_string(hdiVersion.first) + '_' + std::to_string(hdiVersion.second);
+    const std::string& backendName = GenUniqueName(deviceName, vendorName, version);
+
+    std::shared_ptr<Device> device = CreateSharedPtr<HDIDeviceV2_0>(iDevice);
+    if (device == nullptr) {
+        LOGW("Failed to create device, because fail to create device instance.");
+        return nullptr;
+    }
+
+    std::shared_ptr<Backend> backend = std::make_shared<NNBackend>(device, std::hash<std::string>{}(backendName));
+    if (backend == nullptr) {
+        LOGW("Failed to register backend, because fail to create backend.");
+    }
+    return backend;
+}
+
+REGISTER_BACKEND(HDIDeviceV2_0, HDIDeviceV2_0Creator)
+} // namespace NeuralNetworkRuntime
+} // namespace OHOS
 
 namespace OHOS {
 namespace HDI {
