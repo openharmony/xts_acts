37210221a0a3d0b01d7b55b0a9c9bf614ec93921
liangweitao <liangweitao@openvalley.net>
2024-02-27 14:08:01 +0800

    ActsAmsAbilityMultiinstanceTest/ActsAmsAbilityMultiinstanceHapARelyHap
    Signed-off-by: liangweitao <liangweitao@openvalley.net>

--- ability/ability_runtime/abilitymultiinstance/amsabilitymultiinstanceappa/entry/src/main/ets/Application/AbilityStage.ts
+++ ability/ability_runtime/abilitymultiinstance/amsabilitymultiinstanceappa/entry/src/main/ets/Application/AbilityStage.ts
@@ -12,40 +12,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import AbilityStage from "@ohos.app.ability.AbilityStage"
+
+import AbilityStage from "@ohos.app.ability.AbilityStage";
 
 const ONACCEPTWANT_KEY_NOTSET_ID = -1;
 const ONACCEPTWANT_KEY_UNDEFINED_ID = -2;
-var onAcceptWantCalledSeq;
+let onAcceptWantCalledSeq;
 
 export default class MyAbilityStage extends AbilityStage {
-    onCreate() {
-        console.log("AbilityMultiInstanceAppA MyAbilityStage onCreate")
-        globalThis.stageOnCreateRun = 1;
-        globalThis.stageContext = this.context;
+  onCreate() {
+    console.log("AbilityMultiInstanceAppA MyAbilityStage onCreate");
+    // globalThis.stageOnCreateRun = 1;
+    // globalThis.stageContext = this.context;
+  }
+
+  onAcceptWant(want) {
+    console.log("AbilityMultiInstanceAppA onAcceptWant called want:" + JSON.stringify(want));
+    if ((want.parameters == undefined) || (want.parameters.startId == undefined)) {
+      console.log("AbilityMultiInstanceAppA specified param err");
+      return;
     }
 
-    onAcceptWant(want) {
-        console.log("AbilityMultiInstanceAppA onAcceptWant called want:" + JSON.stringify(want));
-        if ((want.parameters == undefined) || (want.parameters.startId == undefined)) {
-            console.log("AbilityMultiInstanceAppA specified param err");
-            return;
-        }
+    let abilityId = want.parameters.startId.toString();
+    onAcceptWantCalledSeq = "onAcceptWantCalled";
+    onAcceptWantCalledSeq += "Id";
+    onAcceptWantCalledSeq += abilityId;
+    AppStorage.setOrCreate("onAcceptWantCalledSeq", onAcceptWantCalledSeq);
 
-        var abilityId = want.parameters.startId.toString();
-        onAcceptWantCalledSeq = "onAcceptWantCalled";
-        onAcceptWantCalledSeq += "Id";
-        onAcceptWantCalledSeq += abilityId;
-        globalThis.onAcceptWantCalledSeq = onAcceptWantCalledSeq;
-
-        if (want.parameters.startId == ONACCEPTWANT_KEY_NOTSET_ID) {
-            return;
-        } else if (want.parameters.startId == ONACCEPTWANT_KEY_UNDEFINED_ID) {
-           return undefined;
-        }
-
-        globalThis.abilityWant = want;
-        console.log("AbilityMultiInstanceAppA set globalThis result :" + globalThis.onAcceptWantCalledSeq);
-        return abilityId;
+    if (want.parameters.startId == ONACCEPTWANT_KEY_NOTSET_ID) {
+      return;
+    } else if (want.parameters.startId == ONACCEPTWANT_KEY_UNDEFINED_ID) {
+      return undefined;
     }
+    AppStorage.setOrCreate("abilityWant", want);
+    console.log("AbilityMultiInstanceAppA set globalThis result :" + AppStorage.get("onAcceptWantCalledSeq")!);
+    return abilityId;
+  }
 }
