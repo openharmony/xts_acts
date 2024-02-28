836fe8520610b3c34fe652f8028fcccd70419160
lipengfei <lipengfei24875@openvalley.net>
2024-02-28 14:17:58 +0800

    第二批，提交Test文件
    Signed-off-by: lipengfei <lipengfei24875@openvalley.net>

--- arkui/ace_ets_web_dev_webviewController/entry/src/main/ets/test/WebViewOnErrorReceive02Jsunit.test.ts
+++ arkui/ace_ets_web_dev_webviewController/entry/src/main/ets/test/WebViewOnErrorReceive02Jsunit.test.ts
@@ -16,41 +16,42 @@ import { describe, beforeEach, afterEach, it, expect } from "@ohos/hypium";
 import events_emitter from '@ohos.events.emitter';
 import Utils from './Utils.ets';
 import router from '@system.router';
+
 export default function webViewOnErrorReceive02Jsunit() {
-    describe('ActsAceWebDevWebViewOnErrorReceive02Test', function () {
-        beforeEach(async function (done) {
-            let options = {
-                uri: "MainAbility/pages/onErrorReceive02"
-            }
-            try {
-                router.clear();
-                let pages = router.getState();
-                console.info("get onErrorReceive02 state success " + JSON.stringify(pages));
-                if (!("onErrorReceive02" == pages.name)) {
-                    console.info("get onErrorReceive02 state success " + JSON.stringify(pages.name));
-                    let result = await router.push(options);
-                    await Utils.sleep(2000);
-                    console.info("push onErrorReceive02 page success " + JSON.stringify(result));
-                }
-            } catch (err) {
-                console.error("push onErrorReceive02 page error: " + err);
-            }
-            await Utils.sleep(2000);
-            done()
-        })
-        afterEach(async function (done) {
-            done();
-        })
-        /*
-        *tc.number SUB_ACE_BASIC_ETS_API_ONERRORRECEIVE_0200
-        *tc.name testOnErrorReceive02
-        *tc.desc test api onErrorReceive  by load html which contain of "<iframe
-                 src="https://wrong.host.badssl.com"></iframe>"
-        */
-        it('testOnErrorReceive02',0,async function(done){
-            let isMainFrame = false;
-            Utils.registerEvent("testOnErrorReceive02", isMainFrame, 7120, done);
-            sendEventByKey('onErrorReceive02',10,'');
-        })
+  describe('ActsAceWebDevWebViewOnErrorReceive02Test', function () {
+    beforeEach(async function (done) {
+      let options = {
+        uri: "MainAbility/pages/onErrorReceive02"
+      }
+      try {
+        router.clear();
+        let pages = router.getState();
+        console.info("get onErrorReceive02 state success " + JSON.stringify(pages));
+        if (!("onErrorReceive02" == pages.name)) {
+          console.info("get onErrorReceive02 state success " + JSON.stringify(pages.name));
+          let result = await router.push(options);
+          await Utils.sleep(2000);
+          console.info("push onErrorReceive02 page success " + JSON.stringify(result));
+        }
+      } catch (err) {
+        console.error("push onErrorReceive02 page error: " + err);
+      }
+      await Utils.sleep(2000);
+      done()
     })
+    afterEach(async function (done) {
+      done();
+    })
+    /*
+    *tc.number SUB_ACE_BASIC_ETS_API_ONERRORRECEIVE_0200
+    *tc.name testOnErrorReceive02
+    *tc.desc test api onErrorReceive  by load html which contain of "<iframe
+             src="https://wrong.host.badssl.com"></iframe>"
+    */
+    it('testOnErrorReceive02', 0, async function (done) {
+      let isMainFrame = false;
+      Utils.registerEvent("testOnErrorReceive02", isMainFrame, 7120, done);
+      sendEventByKey('onErrorReceive02', 10, '');
+    })
+  })
 }
