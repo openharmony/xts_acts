22a1fb091d1e0f9fd1797af78f697db83b5e85de
lipengfei <lipengfei24875@openvalley.net>
2024-03-06 16:12:59 +0800

    提交actsbmsatomicservicetest
    Signed-off-by: lipengfei <lipengfei24875@openvalley.net>

--- bundlemanager/bundle_standard/bundlemanager/actsbmsatomicserviceetstest/actsbmsatomicservicetest/entry/src/main/ets/TestAbility/TestAbility.ts
+++ bundlemanager/bundle_standard/bundlemanager/actsbmsatomicserviceetstest/actsbmsatomicservicetest/entry/src/main/ets/TestAbility/TestAbility.ts
@@ -16,30 +16,30 @@
 import Ability from '@ohos.app.ability.UIAbility'
 
 export default class TestAbility extends Ability {
-    onCreate(want, launchParam) {
-        console.log('TestAbility onCreate')
-    }
+  onCreate(want, launchParam) {
+    console.log('TestAbility onCreate')
+  }
 
-    onDestroy() {
-        console.log('TestAbility onDestroy')
-    }
+  onDestroy() {
+    console.log('TestAbility onDestroy')
+  }
 
-    onWindowStageCreate(windowStage) {
-        console.log('TestAbility onWindowStageCreate')
-        windowStage.setUIContent(this.context, 'TestAbility/pages/index', null)
+  onWindowStageCreate(windowStage) {
+    console.log('TestAbility onWindowStageCreate')
+    windowStage.setUIContent(this.context, 'TestAbility/pages/index', null)
 
-        globalThis.abilityContext = this.context;
-    }
+    globalThis.abilityContext = this.context;
+  }
 
-    onWindowStageDestroy() {
-        console.log('TestAbility onWindowStageDestroy')
-    }
+  onWindowStageDestroy() {
+    console.log('TestAbility onWindowStageDestroy')
+  }
 
-    onForeground() {
-        console.log('TestAbility onForeground')
-    }
+  onForeground() {
+    console.log('TestAbility onForeground')
+  }
 
-    onBackground() {
-        console.log('TestAbility onBackground')
-    }
+  onBackground() {
+    console.log('TestAbility onBackground')
+  }
 };
