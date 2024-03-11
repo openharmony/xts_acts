b3dde9b18ea61b1bec3b2f82104f7c5f7a6296df
luowei164 <luowei164@h-partners.com>
2024-03-11 10:34:42 +0800

    fixed 63868d6 from https://gitee.com/luowei164/xts_acts/pulls/12732 电话服务xts代码优化
    Signed-off-by: luowei164 <luowei164@h-partners.com>

--- telephony/telephonyjstest/call_manager/call_manager_ims2_call/src/main/js/test/CallManageAll.test.js
+++ telephony/telephonyjstest/call_manager/call_manager_ims2_call/src/main/js/test/CallManageAll.test.js
@@ -112,6 +112,7 @@ export default function CallManageImsCall() {
                 expect(call.CallState.CALL_STATE_IDLE).assertEqual(0);
                 expect(call.CallState.CALL_STATE_RINGING).assertEqual(1);
                 expect(call.CallState.CALL_STATE_OFFHOOK).assertEqual(2);
+		expect(call.CallState.CALL_STATE_ANSWERED).assertEqual(3);
                 console.log("Telephony_CallManager_getCallState_Async_0100 : data =" + JSON.stringify(data));
                 done();
             });
