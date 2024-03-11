9106122be662f293b046ba569824b128ffa3fe4e
tiankonglin <tiankonglin@openvalley.net>
2024-03-11 11:39:27 +0800

    第四批适配
    Signed-off-by: tiankonglin <tiankonglin@openvalley.net>

--- arkui/ace_ets_component_api11_completion/entry/src/main/ets/TestAbility/TestAbility.ts
+++ arkui/ace_ets_component_api11_completion/entry/src/main/ets/TestAbility/TestAbility.ts
@@ -15,46 +15,46 @@
 import Ability from '@ohos.app.ability.UIAbility'
 
 export default class TestAbility extends Ability {
-    onCreate(want, launchParam) {
-        console.log('TestAbility onCreate')
-    }
+  onCreate(want, launchParam) {
+    console.log('TestAbility onCreate')
+  }
 
-    onDestroy() {
-        console.log('TestAbility onDestroy')
-    }
+  onDestroy() {
+    console.log('TestAbility onDestroy')
+  }
 
-    onWindowStageCreate(windowStage) {
-        console.log('TestAbility onWindowStageCreate')
-        windowStage.loadContent("TestAbility/pages/index", (err, data) => {
-            if (err.code) {
-                console.error('Failed to load the content. Cause:' + JSON.stringify(err));
-                return;
-            }
-            console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
-        });
+  onWindowStageCreate(windowStage) {
+    console.log('TestAbility onWindowStageCreate')
+    windowStage.loadContent("TestAbility/pages/index", (err, data) => {
+      if (err.code) {
+        console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+        return;
+      }
+      console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data))
+    });
 
-        globalThis.abilityContext = this.context;
-        let windowClass = null;
-        windowStage.getMainWindow((err, data) => {
-            if (err.code) {
-                console.error('Failed to obtain the main window. Cause: ' + JSON.stringify(err));
-                return;
-            }
-            windowClass = data;
-            globalThis.uiContext = windowClass.getUIContext();
-        });
+    globalThis.abilityContext = this.context;
+    let windowClass = null;
+    windowStage.getMainWindow((err, data) => {
+      if (err.code) {
+        console.error('Failed to obtain the main window. Cause: ' + JSON.stringify(err));
+        return;
+      }
+      windowClass = data;
+      globalThis.uiContext = windowClass.getUIContext();
+    });
 
-    }
+  }
 
-    onWindowStageDestroy() {
-        console.log('TestAbility onWindowStageDestroy')
-    }
+  onWindowStageDestroy() {
+    console.log('TestAbility onWindowStageDestroy')
+  }
 
-    onForeground() {
-        console.log('TestAbility onForeground')
-    }
+  onForeground() {
+    console.log('TestAbility onForeground')
+  }
 
-    onBackground() {
-        console.log('TestAbility onBackground')
-    }
+  onBackground() {
+    console.log('TestAbility onBackground')
+  }
 };
