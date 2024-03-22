0710bd3121aa78e2da45015f2be81346e76fe3bd
sun-mingss <sunming34@huawei.com>
2024-03-22 17:04:51 +0800

    Arkui社区ace_ets_component_api测试套拆分
    Signed-off-by: sun-mingss <sunming34@huawei.com>

--- arkui/arkui_ets_test_ui/arkui_ets_module_commonAttrsDialog/arkui_ets_module_commonAttrsDialog_api11/entry/src/main/ets/Application/AbilityStage.ts
+++ arkui/arkui_ets_test_ui/arkui_ets_module_commonAttrsDialog/arkui_ets_module_commonAttrsDialog_api11/entry/src/main/ets/Application/AbilityStage.ts
@@ -12,16 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import AbilityStage from "@ohos.app.ability.AbilityStage"
 
-import listJsunit from './ListTest/ListJsunit.test';
-import swiperJsunit from './SwiperTest/SwiperJsunit.test';
-import ListDeleteTest from './ListTest/ListDelete.test'
-import ListSwipeTest from './ListTest/ListSwipe.test'
-import ListReboundTest from './ListTest/ListRebound.test'
-export default function testsuite() {
-  listJsunit();
-  swiperJsunit();
-  ListDeleteTest();
-  ListSwipeTest();
-  ListReboundTest();
+export default class MyAbilityStage extends AbilityStage {
+    onCreate() {
+        console.log("[Demo] MyAbilityStage onCreate")
+        globalThis.stageOnCreateRun = 1;
+        globalThis.stageContext = this.context;
+    }
 }
