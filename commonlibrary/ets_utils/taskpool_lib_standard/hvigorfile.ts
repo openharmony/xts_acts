3af534f274fd88ab17ffd3560ce3d70c1e9567d5
caolili123 <caolili14@huawei.com>
2024-04-08 15:11:29 +0800

    issue:https://gitee.com/openharmony/xts_acts/issues/I9ERHW
    Signed-off-by: caolili123 <caolili14@huawei.com>

--- commonlibrary/ets_utils/taskpool_lib_standard/hvigorfile.ts
+++ commonlibrary/ets_utils/taskpool_lib_standard/hvigorfile.ts
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2023 Huawei Device Co., Ltd.
+/**
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,9 @@
  * limitations under the License.
  */
 
-import worker from '@ohos.worker';
-const parentPort = worker.workerPort;
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
-parentPort.onmessage = function(e) {
+export default {
+    system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
 }
