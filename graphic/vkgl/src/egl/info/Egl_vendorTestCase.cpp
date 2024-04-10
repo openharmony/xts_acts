e45ad9c73c401f749353b07baf648282e2a8a49a
huruitao <huruitao@kaihong.com>
2024-04-10 17:15:04 +0800

    "modifile graphic/vkgl/src/egl/info/Egl_vendorTestCase.cpp 适配egl，修改case格式"
    Signed-off-by: huruitao <huruitao@kaihong.com>

--- graphic/vkgl/src/egl/info/Egl_vendorTestCase.cpp
+++ graphic/vkgl/src/egl/info/Egl_vendorTestCase.cpp
@@ -23,5 +23,5 @@ using namespace std;
 using namespace testing::ext;
 using namespace OHOS;
 
-static SHRINK_HWTEST_F(ActsEgl0001TS, TC000002, "dEQP-EGL.info.ve"
-    ,"ndor");
+static SHRINK_HWTEST_F(ActsEgl0001TS, TC000002, "dEQP-EGL.info.ve",
+    "ndor");
