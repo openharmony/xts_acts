89da4903085d68e774cd178583cbf8a77409fed8
txl <tangxianglei@huawei.com>
2024-04-22 12:03:53 +0800

    精准编译优化,增加依赖计算脚本
    Signed-off-by: txl <tangxianglei@huawei.com>

--- get_dependency.py
+++ get_dependency.py
@@ -6,15 +6,17 @@ import sys
 precise_repo_xts = sys.argv[1]
 DEEP = int(sys.argv[2]) if len(sys.argv) > 2 else -1
 home = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
-acts_home = os.getcwd()
 manifest_home = os.path.join(home , ".repo")
 preloader_path = os.path.join(home,"out","preloader")
 precise_gni_name = "precise_run.gni"
-depends_tree_name = "depends_tree.gni"
+depends_tree_name = "depends_tree.txt"
+f_depends_name = "f_depends.txt"
+MAX_PRECISE_PR_NUM = 20
 
 # 首层依赖关系dist
 dependency_dist = {}
-
+# 全量编译白名单
+whitelist = {"test/xts/acts", "build"}
 
 def get_f_dependes_dist (path):
     count = 0
@@ -63,9 +65,10 @@ def getbundname (path_list):
 
 def get_all_dependencies(nodes, dependencies):
     visited = set()  # 存储已访问的节点
-    string_buffer = ""
-    def dfs(node,num,string_buffer):
-        string_buffer += "  " * num + node
+    string_buffer = []
+
+    def dfs(node, num, string_buffer):
+        string_buffer.append("  " * num + node)  # 追加内容到列表
         if node in visited:
             return
         if DEEP != -1 and num > DEEP:
@@ -73,11 +76,12 @@ def get_all_dependencies(nodes, dependencies):
         visited.add(node)
         if node in dependencies:
             for dependency in dependencies[node]:
-                dfs(dependency,num+1)
-    for node in nodes:
-        dfs(node,0,string_buffer)
+                dfs(dependency, num + 1, string_buffer)  # 传递可变对象进行修改
 
-    return visited,string_buffer
+    for node in nodes:
+        dfs(node, 0, string_buffer)
+
+    return visited, string_buffer
 
 def write_precise_gni(depends_list,file_name):
     # 将数组转换为字符串形式，例如使用逗号分隔
@@ -94,20 +98,27 @@ def write_file(depends_list,file_name):
 
 
 def run(precise_repo_xts_list):
-    # 计算首层依赖map
-    get_f_dependes_dist(home)
-    # 获取bundle_name_list
-    bundlelist = getbundname(precise_repo_xts_list)
-    print(bundlelist)
-    # 获取全部需要跑的bundlename
-    depends_list, depends_tree = get_all_dependencies(bundlelist,dependency_dist)
-    print(depends_list)
+    # pr数>MAX或在白名单中的
+    if len(precise_repo_xts_list)>=MAX_PRECISE_PR_NUM or any(item in precise_repo_xts_list for item in whitelist):
+        depends_tree = "all"
+        depends_list = {"all"}
+    else:
+        # 计算首层依赖map
+        get_f_dependes_dist(home)
+        # 首层依赖树打印
+        # write_file(dependency_dist, os.path.join(preloader_path,f_depends_name))
+        # 获取bundle_name_list
+        bundlelist = getbundname(precise_repo_xts_list)
+        print(bundlelist)
+        # 获取全部需要跑的bundlename
+        depends_list, depends_tree = get_all_dependencies(bundlelist,dependency_dist)
+        print(depends_list)
+    # 依赖树输出
     write_file(depends_tree, os.path.join(preloader_path,depends_tree_name))
     # 写入gni文件
     write_precise_gni(depends_list, os.path.join(preloader_path,precise_gni_name))
 
 
 if __name__ == '__main__':
-    print("counting dependencies start")
+    print("start")
     run(precise_repo_xts.split(':'))
-    print("counting dependencies finished")
