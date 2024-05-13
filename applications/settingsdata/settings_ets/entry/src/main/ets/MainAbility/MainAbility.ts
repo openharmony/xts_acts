13a4d0b5ec0ad942b983b4de92a1ab7f897e5a91
zhuzhengjun <zhuzhengjun@openvalley.net>
2024-05-13 10:56:52 +0800

    【XTS语法适配】ActsSettingsEtsTest语法适配
    Signed-off-by: zhuzhengjun <zhuzhengjun@openvalley.net>

--- applications/settingsdata/settings_ets/entry/src/main/ets/MainAbility/MainAbility.ts
+++ applications/settingsdata/settings_ets/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -12,40 +12,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Ability from '@ohos.app.ability.UIAbility'
+import Ability from '@ohos.app.ability.UIAbility';
+import Want from '@ohos.app.ability.Want';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import window from '@ohos.window';
 
 export default class MainAbility extends Ability {
-    onCreate(want,launchParam){
-        // Ability is creating, initialize resources for this ability
-        console.log("[Demo] MainAbility onCreate")
-        globalThis.abilityWant = want;
-    }
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    // Ability is creating, initialize resources for this ability
+    console.log("[Demo] MainAbility onCreate");
+    globalThis.abilityWant = want;
+  }
 
-    onDestroy() {
-        // Ability is destroying, release resources for this ability
-        console.log("[Demo] MainAbility onDestroy")
-    }
+  onDestroy() {
+    // Ability is destroying, release resources for this ability
+    console.log("[Demo] MainAbility onDestroy");
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log("[Demo] MainAbility onWindowStageCreate windowStage="+ windowStage)
-        globalThis.windowStage = windowStage
-        globalThis.abilityContext = this.context
-        windowStage.setUIContent(this.context, "MainAbility/pages/index/index", null)
-    }
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    console.log("[Demo] MainAbility onWindowStageCreate windowStage=" + windowStage);
+    globalThis.windowStage = windowStage;
+    globalThis.abilityContext = this.context;
+    windowStage.loadContent("MainAbility/pages/index/index", (err, data) => {
+      if (err.code) {
+        console.error('Failed to load the content. Cause:' + JSON.stringify(err));
+        return;
+      }
+      console.info('Succeeded in loading the content. Data: ' + JSON.stringify(data));
+    });
+  }
 
-    onWindowStageDestroy() {
-        //Main window is destroyed, release UI related resources
-        console.log("[Demo] MainAbility onWindowStageDestroy")
-    }
+  onWindowStageDestroy() {
+    //Main window is destroyed, release UI related resources
+    console.log("[Demo] MainAbility onWindowStageDestroy");
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log("[Demo] MainAbility onForeground")
-    }
+  onForeground() {
+    // Ability has brought to foreground
+    console.log("[Demo] MainAbility onForeground");
+  }
 
-    onBackground() {
-        // Ability has back to background
-        console.log("[Demo] MainAbility onBackground")
-    }
+  onBackground() {
+    // Ability has back to background
+    console.log("[Demo] MainAbility onBackground");
+  }
 };
