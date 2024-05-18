f0acc52450fba6828181fa0b760c0702c9a3a896
huruitao <huruitao@kaihong.com>
2024-05-18 18:22:50 +0800

    update huks_signverify_callback_BasicTest
    Signed-off-by: huruitao <huruitao@kaihong.com>

---
+++ security/security_huks_basic/huks_signverify_callback_BasicTest/entry/src/ohosTest/js/test/List.test.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SecurityHuksDSABasicAbort63KBCallbackJsunit from './SignVerify/SecurityHuksDSABasicAbort63KBCallbackJsunit.test.js'
+import SecurityHuksDSABasicAbort65KBCallbackJsunit from './SignVerify/SecurityHuksDSABasicAbort65KBCallbackJsunit.test.js'
+import SecurityHuksDSABasicFinish63KBCallbackJsunit from './SignVerify/SecurityHuksDSABasicFinish63KBCallbackJsunit.test.js'
+import SecurityHuksDSABasicFinish65KBCallbackJsunit from './SignVerify/SecurityHuksDSABasicFinish65KBCallbackJsunit.test.js'
+import SecurityHuksECCBasicAbort63KBCallbackJsunit from './SignVerify/SecurityHuksECCBasicAbort63KBCallbackJsunit.test.js'
+import SecurityHuksECCBasicAbort65KBCallbackJsunit from './SignVerify/SecurityHuksECCBasicAbort65KBCallbackJsunit.test.js'
+import SecurityHuksECCBasicFinish63KBCallbackJsunit from './SignVerify/SecurityHuksECCBasicFinish63KBCallbackJsunit.test.js'
+import SecurityHuksECCBasicFinish65KBCallbackJsunit from './SignVerify/SecurityHuksECCBasicFinish65KBCallbackJsunit.test.js'
+import SecurityHuksED25519BasicCallbackJsunit from './SignVerify/SecurityHuksED25519BasicCallbackJsunit.test.js'
+import SecurityHuksRSABasicAbort63KBCallbackJsunit from './SignVerify/SecurityHuksRSABasicAbort63KBCallbackJsunit.test.js'
+import SecurityHuksRSABasicAbort65KBCallbackJsunit from './SignVerify/SecurityHuksRSABasicAbort65KBCallbackJsunit.test.js'
+import SecurityHuksRSABasicFinish63KBCallbackJsunit from './SignVerify/SecurityHuksRSABasicFinish63KBCallbackJsunit.test.js'
+import SecurityHuksRSABasicFinish65KBCallbackJsunit from './SignVerify/SecurityHuksRSABasicFinish65KBCallbackJsunit.test.js'
+import SecurityHuksSM2BasicAbort63KBCallbackJsunit from './SignVerify/SecurityHuksSM2BasicAbort63KBCallbackJsunit.test.js'
+import SecurityHuksSM2BasicAbort65KBCallbackJsunit from './SignVerify/SecurityHuksSM2BasicAbort65KBCallbackJsunit.test.js'
+import SecurityHuksSM2BasicFinish63KBCallbackJsunit from './SignVerify/SecurityHuksSM2BasicFinish63KBCallbackJsunit.test.js'
+import SecurityHuksSM2BasicFinish65KBCallbackJsunit from './SignVerify/SecurityHuksSM2BasicFinish65KBCallbackJsunit.test.js'
+export default function testsuite() {
+SecurityHuksDSABasicAbort63KBCallbackJsunit()
+SecurityHuksDSABasicAbort65KBCallbackJsunit()
+SecurityHuksDSABasicFinish63KBCallbackJsunit()
+SecurityHuksDSABasicFinish65KBCallbackJsunit()
+SecurityHuksECCBasicAbort63KBCallbackJsunit()
+SecurityHuksECCBasicAbort65KBCallbackJsunit()
+SecurityHuksECCBasicFinish63KBCallbackJsunit()
+SecurityHuksECCBasicFinish65KBCallbackJsunit()
+SecurityHuksED25519BasicCallbackJsunit()
+SecurityHuksRSABasicAbort63KBCallbackJsunit()
+SecurityHuksRSABasicAbort65KBCallbackJsunit()
+SecurityHuksRSABasicFinish63KBCallbackJsunit()
+SecurityHuksRSABasicFinish65KBCallbackJsunit()
+SecurityHuksSM2BasicAbort63KBCallbackJsunit()
+SecurityHuksSM2BasicAbort65KBCallbackJsunit()
+SecurityHuksSM2BasicFinish63KBCallbackJsunit()
+SecurityHuksSM2BasicFinish65KBCallbackJsunit()
+}
