bae93710e87379b0f4b4ff8b77de74cac192a3d9
qu-xianfei <quxianfei1@huawei.com>
2024-05-20 13:11:35 +0800

    add hvigor sample
    Signed-off-by: qu-xianfei <quxianfei1@huawei.com>
    Change-Id: I797e4d7419480847d798c27cbf021ca747d72c13

--- demo/hvigor_sample/demo_ndk_fa_js/entry/src/main/cpp/hello.cpp
+++ demo/hvigor_sample/demo_ndk_fa_js/entry/src/main/cpp/hello.cpp
@@ -18,26 +18,18 @@ static napi_value Add(napi_env env, napi_callback_info info)
 {
     size_t argc = 2;
     napi_value args[2] = {nullptr};
-
-    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
-
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
     napi_valuetype valuetype0;
     napi_typeof(env, args[0], &valuetype0);
-
     napi_valuetype valuetype1;
     napi_typeof(env, args[1], &valuetype1);
-
     double value0;
     napi_get_value_double(env, args[0], &value0);
-
     double value1;
     napi_get_value_double(env, args[1], &value1);
-
     napi_value sum;
     napi_create_double(env, value0 + value1, &sum);
-
     return sum;
-
 }
 
 EXTERN_C_START
