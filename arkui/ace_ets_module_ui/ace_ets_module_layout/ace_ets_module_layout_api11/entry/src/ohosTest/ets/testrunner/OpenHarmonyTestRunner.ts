6355c13536f7b71eb153e76fc0b17600f5b32218
pavelyevivan <pavelyev.ivan@huawei-partners.com>
2024-05-20 14:36:06 +0300

    Adding hvigor for ace_ets_module_layout_api11
    Signed-off-by: pavelyevivan <pavelyev.ivan@huawei-partners.com>

--- arkui/ace_ets_module_ui/ace_ets_module_layout/ace_ets_module_layout_api11/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
+++ arkui/ace_ets_module_ui/ace_ets_module_layout/ace_ets_module_layout_api11/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
@@ -12,33 +12,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import TestRunner from '@ohos.application.testRunner'
-import AbilityDelegatorRegistry from '@ohos.application.abilityDelegatorRegistry'
+
+import hilog from '@ohos.hilog';
+import TestRunner from '@ohos.application.testRunner';
+import AbilityDelegatorRegistry from '@ohos.app.ability.abilityDelegatorRegistry';
 
 var abilityDelegator = undefined
 var abilityDelegatorArguments = undefined
 
-function translateParamsToString(parameters) {
-    const keySet = new Set([
-        '-s class', '-s notClass', '-s suite', '-s it',
-        '-s level', '-s testType', '-s size', '-s timeout',
-        '-s dryRun'
-    ])
-    let targetParams = '';
-    for (const key in parameters) {
-        if (keySet.has(key)) {
-            targetParams = `${targetParams} ${key} ${parameters[key]}`
-        }
-    }
-    return targetParams.trim()
-}
-
 async function onAbilityCreateCallback() {
-    console.log("onAbilityCreateCallback");
+    hilog.info(0x0000, 'testTag', '%{public}s', 'onAbilityCreateCallback');
 }
 
 async function addAbilityMonitorCallback(err: any) {
-    console.info("addAbilityMonitorCallback : " + JSON.stringify(err))
+    hilog.info(0x0000, 'testTag', 'addAbilityMonitorCallback : %{public}s', JSON.stringify(err) ?? '');
 }
 
 export default class OpenHarmonyTestRunner implements TestRunner {
@@ -46,11 +33,11 @@ export default class OpenHarmonyTestRunner implements TestRunner {
     }
 
     onPrepare() {
-        console.info("OpenHarmonyTestRunner OnPrepare ")
+        hilog.info(0x0000, 'testTag', '%{public}s', 'OpenHarmonyTestRunner OnPrepare ');
     }
 
     async onRun() {
-        console.log('OpenHarmonyTestRunner onRun run')
+        hilog.info(0x0000, 'testTag', '%{public}s', 'OpenHarmonyTestRunner onRun run');
         abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
         abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
         var testAbilityName = abilityDelegatorArguments.bundleName + '.MainAbility'
@@ -60,19 +47,18 @@ export default class OpenHarmonyTestRunner implements TestRunner {
         };
         abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
         var cmd = 'aa start -d 0 -a com.arkui.ace.layout.MainAbility' + ' -b ' + abilityDelegatorArguments.bundleName
-        cmd += ' '+translateParamsToString(abilityDelegatorArguments.parameters)
-        var debug = abilityDelegatorArguments.parameters["-D"]
+        var debug = abilityDelegatorArguments.parameters['-D']
         if (debug == 'true')
         {
             cmd += ' -D'
         }
-        console.info('cmd : '+cmd)
+        hilog.info(0x0000, 'testTag', 'cmd : %{public}s', cmd);
         abilityDelegator.executeShellCommand(cmd,
             (err: any, d: any) => {
-                console.info('executeShellCommand : err : ' + JSON.stringify(err));
-                console.info('executeShellCommand : data : ' + d.stdResult);
-                console.info('executeShellCommand : data : ' + d.exitCode);
+                hilog.info(0x0000, 'testTag', 'executeShellCommand : err : %{public}s', JSON.stringify(err) ?? '');
+                hilog.info(0x0000, 'testTag', 'executeShellCommand : data : %{public}s', d.stdResult ?? '');
+                hilog.info(0x0000, 'testTag', 'executeShellCommand : data : %{public}s', d.exitCode ?? '');
             })
-        console.info('OpenHarmonyTestRunner onRun end')
+        hilog.info(0x0000, 'testTag', '%{public}s', 'OpenHarmonyTestRunner onRun end');
     }
-};
+}
