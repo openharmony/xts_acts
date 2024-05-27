20054a425dfc12743fbfe902152859eb33a8d54a
shitijun <shitijun@kaihong.com>
2024-05-27 18:39:22 +0800

    update hvigor:actsmoveabilitytobackgroundtest
    Signed-off-by: shitijun <shitijun@kaihong.com>

--- ability/ability_runtime/actsuiabilitycontext/actsmoveabilitytobackgroundtest/entry/src/ohosTest/ets/entryability/EntryAbility.ts
+++ ability/ability_runtime/actsuiabilitycontext/actsmoveabilitytobackgroundtest/entry/src/ohosTest/ets/entryability/EntryAbility.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,9 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { AbilityConstant, UIAbility, Want } from '@kit.AbilityKit';
-import { hilog } from '@kit.PerformanceAnalysisKit';
-import { window } from '@kit.ArkUI';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import hilog from '@ohos.hilog';
+import UIAbility from '@ohos.app.ability.UIAbility';
+import Want from '@ohos.app.ability.Want';
+import window from '@ohos.window';
+import { publishEvent } from '../utils/Utils';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
@@ -28,6 +31,7 @@ export default class EntryAbility extends UIAbility {
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+    globalThis.entryAbilityContext = this.context;
 
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
@@ -51,5 +55,6 @@ export default class EntryAbility extends UIAbility {
   onBackground() {
     // Ability has back to background
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+    publishEvent("Acts_onBackground_MoveAbilityToBackground_Event_1");
   }
 }
