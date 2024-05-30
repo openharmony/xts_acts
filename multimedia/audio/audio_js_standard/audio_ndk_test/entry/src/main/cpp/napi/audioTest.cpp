b89f478591532ffa009056b80ecdb9dba3008a3a
张日洪 <zhangrihong6@h-partners.com>
2024-05-30 16:09:34 +0800

    处理get的值非set的值
    Signed-off-by: 张日洪 <zhangrihong6@h-partners.com>

--- multimedia/audio/audio_js_standard/audio_ndk_test/entry/src/main/cpp/napi/audioTest.cpp
+++ multimedia/audio/audio_js_standard/audio_ndk_test/entry/src/main/cpp/napi/audioTest.cpp
@@ -3092,11 +3092,11 @@ static napi_value AudioAudioInternalRecordingSuccess01(napi_env env, napi_callba
 static napi_value AudioAudioInternalRecordingSuccess02(napi_env env, napi_callback_info info)
 {
     OH_AudioStreamBuilder *builder = CreateRenderBuilder();
+    OH_AudioStream_PrivacyType type = AUDIO_STREAM_PRIVACY_TYPE_PRIVATE;
+    OH_AudioStream_Result result = OH_AudioStreamBuilder_SetRendererPrivacy(builder, type);
     OH_AudioRenderer *audioRenderer = nullptr;
     OH_AudioStreamBuilder_GenerateRenderer(builder, &audioRenderer);
-    OH_AudioStream_PrivacyType type = AUDIO_STREAM_PRIVACY_TYPE_PRIVATE;
     OH_AudioStream_PrivacyType type_get;
-    OH_AudioStream_Result result = OH_AudioStreamBuilder_SetRendererPrivacy(builder, type);
     OH_AudioStream_Result result1 = OH_AudioRenderer_GetRendererPrivacy(audioRenderer, &type_get);
     if (result == AUDIOSTREAM_SUCCESS && result1 == AUDIOSTREAM_SUCCESS
         && type_get == AUDIO_STREAM_PRIVACY_TYPE_PRIVATE) {
