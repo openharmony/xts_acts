7f069321c8d57c4910fb46e66aa8dd117a96b8e6
guohuanhuan <guohuanhuan4@huawei.com>
2024-06-05 15:29:55 +0800

    添加版权头
    Signed-off-by: guohuanhuan <guohuanhuan4@huawei.com>

--- graphic/component3D/AppScope/resources/rawfile/assets/app/shaders/shader/compile_shader.bat
+++ graphic/component3D/AppScope/resources/rawfile/assets/app/shaders/shader/compile_shader.bat
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 wsl.exe rsync -azP -e 'ssh -p 2222' huxiaoming@123.60.114.129:~/OHOS_20231204/foundation/graphic/graphic_3d/lume/Lume_3D/assets/3d/shaders/shader/London.frag.spv .
 wsl.exe rsync -azP -e 'ssh -p 2222' huxiaoming@123.60.114.129:~/OHOS_20231204/foundation/graphic/graphic_3d/lume/Lume_3D/assets/3d/shaders/shader/London.frag.spv.gl .
 wsl.exe rsync -azP -e 'ssh -p 2222' huxiaoming@123.60.114.129:~/OHOS_20231204/foundation/graphic/graphic_3d/lume/Lume_3D/assets/3d/shaders/shader/London.frag.spv.gles .
