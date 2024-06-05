91f04a9b4fd34496cb860f1b47b3c9daa8ba9982
yaopengfei <yaopengfei@kaihong.com>
2024-06-05 17:51:03 +0800

    修改门禁问题
    Signed-off-by: yaopengfei <yaopengfei@kaihong.com>

--- graphic/acts_drawing_native/DrawingNativeCanvasTest.h
+++ graphic/acts_drawing_native/DrawingNativeCanvasTest.h
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef DrawingNativeCanvasTest_H
-#define DrawingNativeCanvasTest_H
+#ifndef DRAWING_NATIVE_CANVAS_TEST_H
+#define DRAWING_NATIVE_CANVAS_TEST_H
 
 #include "gtest/gtest.h"
 
@@ -26,4 +26,4 @@ class DrawingNativeCanvasTest : public testing::Test {};
 } // namespace Rosen
 } // namespace OHOS
 
-#endif // DrawingNativeCanvasTest_H
+#endif // DRAWING_NATIVE_CANVAS_TEST_H
