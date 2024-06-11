d6ba8c51bd0e623b60ccb8aadedcc96a3f5976a5
jingxin <jingxin10@huawei.com>
2024-06-11 16:44:23 +0800

    c
    Signed-off-by: jingxin <jingxin10@huawei.com>

--- arkui/ace_c_arkui_test/entry/src/main/cpp/arkApi/arkApi.cpp
+++ arkui/ace_c_arkui_test/entry/src/main/cpp/arkApi/arkApi.cpp
@@ -14,8 +14,10 @@
  */
 #include <arkui/native_gesture.h>
 #include <arkui/native_node_napi.h>
+#include <arkui/native_type.h>
 #include "common/common.h"
 #include <ace/xcomponent/native_interface_xcomponent.h>
+#define VALUE_0 10
 
 namespace ArkUICapiTest {
 
@@ -136,9 +138,9 @@ static napi_value TestArkUIAddApi015(napi_env env, napi_callback_info info)
 {
     char *content = "C textStyle Test";
     auto ret = OH_ArkUI_ImageAnimatorFrameInfo_CreateFromString(content);
-    OH_ArkUI_ImageAnimatorFrameInfo_SetHeight(ret, 10);
+    OH_ArkUI_ImageAnimatorFrameInfo_SetHeight(ret, VALUE_0);
     auto value = OH_ArkUI_ImageAnimatorFrameInfo_GetHeight(ret);
-    ASSERT_EQ(value, 10);
+    ASSERT_EQ(value, VALUE_0);
     NAPI_END;
 }
 
@@ -146,9 +148,9 @@ static napi_value TestArkUIAddApi016(napi_env env, napi_callback_info info)
 {
     char *content = "C textStyle Test";
     auto ret = OH_ArkUI_ImageAnimatorFrameInfo_CreateFromString(content);
-    OH_ArkUI_ImageAnimatorFrameInfo_SetTop(ret, 10);
+    OH_ArkUI_ImageAnimatorFrameInfo_SetTop(ret, VALUE_0);
     auto value = OH_ArkUI_ImageAnimatorFrameInfo_GetTop(ret);
-    ASSERT_EQ(value, 10);
+    ASSERT_EQ(value, VALUE_0);
     NAPI_END;
 }
 
@@ -156,9 +158,9 @@ static napi_value TestArkUIAddApi017(napi_env env, napi_callback_info info)
 {
     char *content = "C textStyle Test";
     auto ret = OH_ArkUI_ImageAnimatorFrameInfo_CreateFromString(content);
-    OH_ArkUI_ImageAnimatorFrameInfo_SetLeft(ret, 10);
+    OH_ArkUI_ImageAnimatorFrameInfo_SetLeft(ret, VALUE_0);
     auto value = OH_ArkUI_ImageAnimatorFrameInfo_GetLeft(ret);
-    ASSERT_EQ(value, 10);
+    ASSERT_EQ(value, VALUE_0);
     NAPI_END;
 }
 
@@ -166,9 +168,9 @@ static napi_value TestArkUIAddApi018(napi_env env, napi_callback_info info)
 {
     char *content = "C textStyle Test";
     auto ret = OH_ArkUI_ImageAnimatorFrameInfo_CreateFromString(content);
-    OH_ArkUI_ImageAnimatorFrameInfo_SetDuration(ret, 10);
+    OH_ArkUI_ImageAnimatorFrameInfo_SetDuration(ret, VALUE_0);
     auto value = OH_ArkUI_ImageAnimatorFrameInfo_GetDuration(ret);
-    ASSERT_EQ(value, 10);
+    ASSERT_EQ(value, VALUE_0);
     NAPI_END;
 }
 
