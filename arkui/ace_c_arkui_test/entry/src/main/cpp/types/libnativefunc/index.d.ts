ab095f5db11556652d5b28bb8da44638149b04dd
15029377126 <renyuchao2@h-partners.com>
2024-06-14 01:59:15 +0000

    “xts接口覆盖AnimateOption、DrawableDescriptor”
    Signed-off-by: 15029377126 <renyuchao2@h-partners.com>

--- arkui/ace_c_arkui_test/entry/src/main/cpp/types/libnativefunc/index.d.ts
+++ arkui/ace_c_arkui_test/entry/src/main/cpp/types/libnativefunc/index.d.ts
@@ -4923,3 +4923,17 @@ export const testRelativeContaineBarrierOption003: () => number;
 export const customComponentEventNullTest001: () => number;
 
 export const customComponentEventNullTest002: () => number;
+
+export const testCommonAttrsAnimate001: () => number;
+
+export const testCommonAttrsAnimate002: () => number;
+
+export const testCommonAttrsAnimate003: () => number;
+
+export const testCommonAttrsAnimate004: () => number;
+
+export const testDrawableDescriptorCreate001: () => number;
+
+export const testDrawableDescriptorCreate002: () => number;
+
+export const testDrawableDescriptorCreate003: () => number;
