eb7b8783031c8d56d770029fceeb89855a29eda6
huruitao <huruitao@kaihong.com>
2024-06-14 09:40:44 +0800

    theme/screenlock_ets
    Signed-off-by: huruitao <huruitao@kaihong.com>

--- theme/screenlock_ets/entry/hvigorfile.ts
+++ theme/screenlock_ets/entry/hvigorfile.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,11 +12,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import screenLockJSUnit from './screenLockTest.test';
-import ScreenLockPromiseTest from './screenLockPromiseTest.test';
-
-export default function testsuite() {
-  screenLockJSUnit();
-  ScreenLockPromiseTest()
-}
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { legacyHapTasks } from '@ohos/hvigor-ohos-plugin';
