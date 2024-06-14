ffa5ce4691dfd0c4c54b62687f93d0293167bd68
w30052974 <wangyifan94@huawei.com>
2024-06-14 17:11:44 +0800

    hdi device add
    Signed-off-by: w30052974 <wangyifan94@huawei.com>

--- ai/neural_network_runtime/v1_0/common/mock_idevice.cpp
+++ ai/neural_network_runtime/v1_0/common/mock_idevice.cpp
@@ -12,10 +12,69 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <string>
 #include <sys/mman.h>
 
 #include "const.h"
 #include "mock_idevice.h"
+#include "hdi_device_v1_0.h"
+#include "common/log.h"
+#include "common/utils.h"
+#include "nnbackend.h"
+#include "backend_registrar.h"
+
+namespace OHOS {
+namespace NeuralNetworkRuntime {
+std::shared_ptr<Backend> HDIDeviceV1_0Creator()
+{
+    std::string deviceName;
+    std::string vendorName;
+    std::string version;
+
+    // only one device from HDI now.
+    OHOS::sptr<V1_0::INnrtDevice> iDevice = V1_0::INnrtDevice::Get();
+    if (iDevice == nullptr) {
+        LOGW("Get HDI device failed.");
+        return nullptr;
+    }
+
+    auto hdiRet = iDevice->GetDeviceName(deviceName);
+    if (hdiRet != HDF_SUCCESS) {
+        LOGW("Get device name failed. ErrorCode=%d", hdiRet);
+        return nullptr;
+    }
+
+    hdiRet = iDevice->GetVendorName(vendorName);
+    if (hdiRet != HDF_SUCCESS) {
+        LOGW("Get vendor name failed. ErrorCode=%d", hdiRet);
+        return nullptr;
+    }
+
+    std::pair<uint32_t, uint32_t> hdiVersion;
+    hdiRet = iDevice->GetVersion(hdiVersion.first, hdiVersion.second);
+    if (hdiRet != HDF_SUCCESS) {
+        LOGW("Get version failed. ErrorCode=%d", hdiRet);
+        return nullptr;
+    }
+    version = 'v' + std::to_string(hdiVersion.first) + '_' + std::to_string(hdiVersion.second);
+    const std::string& backendName = GenUniqueName(deviceName, vendorName, version);
+
+    std::shared_ptr<Device> device = CreateSharedPtr<HDIDeviceV1_0>(iDevice);
+    if (device == nullptr) {
+        LOGW("Failed to register device, because fail to create device instance.");
+        return nullptr;
+    }
+
+    std::shared_ptr<Backend> backend = std::make_shared<NNBackend>(device, std::hash<std::string>{}(backendName));
+    if (backend == nullptr) {
+        LOGW("Failed to register backend, because fail to create backend.");
+    }
+    return backend;
+}
+
+REGISTER_BACKEND(HDIDeviceV1_0, HDIDeviceV1_0Creator)
+} // namespace NeuralNetworkRuntime
+} // namespace OHOS
 
 namespace OHOS {
 namespace HDI {
