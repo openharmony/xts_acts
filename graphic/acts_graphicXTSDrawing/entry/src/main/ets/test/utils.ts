311575b6ce79dccf8d570f6a65f1349816b40420
Xuefei <xuefei@kaihong.com>
2024-06-19 14:37:08 +0800

    arkts用例brush、region
    Signed-off-by: Xuefei <xuefei@kaihong.com>

--- graphic/acts_graphicXTSDrawing/entry/src/main/ets/test/utils.ts
+++ graphic/acts_graphicXTSDrawing/entry/src/main/ets/test/utils.ts
@@ -28,3 +28,7 @@ export function getEnumCount(obj: Object): number {
   });
   return count;
 }
+
+export function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
