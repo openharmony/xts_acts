8a821583e0e2c6dbe325d7c07ad930da06f6cbe7
yaopengfei <yaopengfei@kaihong.com>
2024-06-21 11:24:51 +0800

    ActsDrawingNativeTestPart3-image
    Signed-off-by: yaopengfei <yaopengfei@kaihong.com>

--- graphic/acts_drawing_native/DrawingNativeImageFilterTest.cpp
+++ graphic/acts_drawing_native/DrawingNativeImageFilterTest.cpp
@@ -13,13 +13,15 @@
  * limitations under the License.
  */
 
-#include "gtest/gtest.h"
-
 #include "drawing_bitmap.h"
 #include "drawing_color.h"
 #include "drawing_color_filter.h"
+#include "drawing_error_code.h"
 #include "drawing_image.h"
 #include "drawing_image_filter.h"
+#include "gtest/gtest.h"
+#include <iostream>
+#include <random>
 
 using namespace testing;
 using namespace testing::ext;
@@ -30,48 +32,217 @@ namespace Drawing {
 class DrawingNativeImageFilterTest : public testing::Test {};
 
 /*
- * @tc.name: OH_Drawing_ImageFilterCreateBlur
- * @tc.desc: test for OH_Drawing_ImageFilterCreateBlur.
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0100
+ * @tc.name: testImageFilterCreateBlurNormal
+ * @tc.desc: Test for creating a blur image filter with normal parameters.
  * @tc.size  : SmallTest
  * @tc.type  : Function
- * @tc.level : Level 1
+ * @tc.level : Level 0
  */
-HWTEST_F(DrawingNativeImageFilterTest, OH_Drawing_ImageFilterCreateBlur, TestSize.Level1) {
-    OH_Drawing_ImageFilter *f = OH_Drawing_ImageFilterCreateBlur(1.0, 1.0, OH_Drawing_TileMode::MIRROR, nullptr);
-    EXPECT_NE(f, nullptr);
-    OH_Drawing_ImageFilterDestroy(f);
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateBlurNormal, TestSize.Level0) {
+    // 1、OH_Drawing_ImageFilterCreateBlur枚举值OH_Drawing_TileMode遍历
+    OH_Drawing_TileMode modes[] = {CLAMP, REPEAT, MIRROR, DECAL};
+    for (OH_Drawing_TileMode mode : modes) {
+        OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(1.0f, 1.0f, mode, nullptr);
+        EXPECT_NE(filter, nullptr);
+        OH_Drawing_ImageFilterDestroy(filter);
+    }
+    // 2、OH_Drawing_ImageFilterCreateBlur接口sigmaX、sigmaY传入小数
+    OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(1.0f, 1.0f, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 3、OH_Drawing_ImageFilterCreateBlur接口sigmaX、sigmaY传入整数
+    filter = OH_Drawing_ImageFilterCreateBlur(1, 1, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 4、OH_Drawing_ImageFilterCreateBlur接口input入参NULL
+    filter = OH_Drawing_ImageFilterCreateBlur(1.0f, 1.0f, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 5、OH_Drawing_ImageFilterCreateBlur接口input入参非NULL
+    filter = OH_Drawing_ImageFilterCreateBlur(1.0f, 1.0f, OH_Drawing_TileMode::MIRROR, filter);
+    // 6、释放内存
+    OH_Drawing_ImageFilterDestroy(filter);
 }
 
 /*
- * @tc.name: OH_Drawing_ImageFilterCreateFromColorFilter
- * @tc.desc: test for OH_Drawing_ImageFilterCreateFromColorFilter.
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0101
+ * @tc.name: testImageFilterCreateBlurNULL
+ * @tc.desc: Test for creating a blur image filter with NULL parameters.
  * @tc.size  : SmallTest
  * @tc.type  : Function
- * @tc.level : Level 1
+ * @tc.level : Level 3
  */
-HWTEST_F(DrawingNativeImageFilterTest, OH_Drawing_ImageFilterCreateFromColorFilter, TestSize.Level1) {
-    OH_Drawing_ColorFilter *colorFilter =
-        OH_Drawing_ColorFilterCreateBlendMode(0xff0000ff, OH_Drawing_BlendMode::BLEND_MODE_SRC);
-    OH_Drawing_ImageFilter *f = OH_Drawing_ImageFilterCreateFromColorFilter(colorFilter, nullptr);
-    EXPECT_NE(f, nullptr);
-    OH_Drawing_ColorFilterDestroy(colorFilter);
-    OH_Drawing_ImageFilterDestroy(f);
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateBlurNULL, TestSize.Level3) {
+    // 1、OH_Drawing_ImageFilterCreateBlur第一个参数为空
+    OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(0, 1.0f, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 2、OH_Drawing_ImageFilterCreateBlur第二个参数为空
+    filter = OH_Drawing_ImageFilterCreateBlur(1.0f, 0, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 3、释放内存
+    OH_Drawing_ImageFilterDestroy(filter);
 }
 
 /*
- * @tc.name: OH_Drawing_ImageFilterDestroy
- * @tc.desc: test for OH_Drawing_ImageFilterDestroy.
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0102
+ * @tc.name: testImageFilterCreateBlurMultipleCalls
+ * @tc.desc: Test for multiple calls of creating a blur image filter.
  * @tc.size  : SmallTest
  * @tc.type  : Function
- * @tc.level : Level 1
+ * @tc.level : Level 3
  */
-HWTEST_F(DrawingNativeImageFilterTest, OH_Drawing_ImageFilterDestroy, TestSize.Level1) {
-    OH_Drawing_ColorFilter *colorFilter =
-        OH_Drawing_ColorFilterCreateBlendMode(0xff0000ff, OH_Drawing_BlendMode::BLEND_MODE_SRC);
-    OH_Drawing_ImageFilter *f = OH_Drawing_ImageFilterCreateFromColorFilter(colorFilter, nullptr);
-    EXPECT_NE(f, nullptr);
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateBlurMultipleCalls, TestSize.Level3) {
+    // 1、OH_Drawing_ImageFilterCreateBlur（sigmaX、sigmaY传入随机数，OH_Drawing_TileMode随机枚举值），调用10次
+    for (int i = 0; i < 10; i++) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dis(0, 10);
+        OH_Drawing_ImageFilter *filter =
+            OH_Drawing_ImageFilterCreateBlur(dis(gen), dis(gen), OH_Drawing_TileMode::MIRROR, nullptr);
+        EXPECT_NE(filter, nullptr);
+        OH_Drawing_ImageFilterDestroy(filter);
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0103
+ * @tc.name: testImageFilterCreateBlurAbnormal
+ * @tc.desc: Test for creating a blur image filter with abnormal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateBlurAbnormal, TestSize.Level3) {
+    // 1、OH_Drawing_ImageFilterCreateBlur接口sigmaX、sigmaY传入负数
+    OH_Drawing_ImageFilter *filter =
+        OH_Drawing_ImageFilterCreateBlur(-1.0f, -1.0f, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 2、释放内存
+    OH_Drawing_ImageFilterDestroy(filter);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0104
+ * @tc.name: testImageFilterCreateBlurMaximum
+ * @tc.desc: Test for creating a blur image filter with maximum values.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateBlurMaximum, TestSize.Level3) {
+    // 1、OH_Drawing_ImageFilterCreateBlur接口sigmaX、sigmaY传入极大值
+    OH_Drawing_ImageFilter *filter =
+        OH_Drawing_ImageFilterCreateBlur(FLT_MAX, FLT_MAX, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 2、释放内存
+    OH_Drawing_ImageFilterDestroy(filter);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0200
+ * @tc.name: testImageFilterCreateFromColorFilterNormal
+ * @tc.desc: Test for creating an image filter from a color filter with normal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 0
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateFromColorFilterNormal, TestSize.Level0) {
+    // 1、OH_Drawing_ImageFilterCreateBlur
+    OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(1, 1, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 2、OH_Drawing_ColorFilterCreateLinearToSrgbGamma
+    OH_Drawing_ColorFilter *colorFilter = OH_Drawing_ColorFilterCreateLinearToSrgbGamma();
+    // 3、OH_Drawing_ImageFilterCreateFromColorFilter参数input为null
+    OH_Drawing_ImageFilter *imageFilter = OH_Drawing_ImageFilterCreateFromColorFilter(colorFilter, nullptr);
+    // 4、OH_Drawing_ImageFilterCreateFromColorFilter参数input不为null
+    imageFilter = OH_Drawing_ImageFilterCreateFromColorFilter(colorFilter, filter);
+    // 5、释放内存
+    OH_Drawing_ImageFilterDestroy(imageFilter);
     OH_Drawing_ColorFilterDestroy(colorFilter);
-    OH_Drawing_ImageFilterDestroy(f);
+    OH_Drawing_ImageFilterDestroy(filter);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0201
+ * @tc.name: testImageFilterCreateFromColorFilterNULL
+ * @tc.desc: Test for creating an image filter from a color filter with NULL parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateFromColorFilterNULL, TestSize.Level3) {
+    OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(1, 1, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 1、OH_Drawing_ImageFilterCreateFromColorFilter第一个参数为空，通过OH_Drawing_ErrorCodeGet查看错误码
+    OH_Drawing_ImageFilterCreateFromColorFilter(nullptr, filter);
+    EXPECT_EQ(OH_Drawing_ErrorCodeGet(), OH_Drawing_ErrorCode::OH_DRAWING_ERROR_INVALID_PARAMETER);
+    // 2、释放内存
+    OH_Drawing_ImageFilterDestroy(filter);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0202
+ * @tc.name: testImageFilterCreateFromColorFilterMultipleCalls
+ * @tc.desc: Test for multiple calls of creating an image filter from a color filter.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterCreateFromColorFilterMultipleCalls, TestSize.Level3) {
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_ColorFilter *colorFilter = OH_Drawing_ColorFilterCreateLinearToSrgbGamma();
+        OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateFromColorFilter(colorFilter, nullptr);
+        EXPECT_NE(filter, nullptr);
+        OH_Drawing_ImageFilterDestroy(filter);
+        OH_Drawing_ColorFilterDestroy(colorFilter);
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0300
+ * @tc.name: testImageFilterDestroyNormal
+ * @tc.desc: Test for destroying an image filter with normal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 0
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterDestroyNormal, TestSize.Level0) {
+    // 1、OH_Drawing_ImageFilterCreateBlur
+    OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(1, 1, OH_Drawing_TileMode::MIRROR, nullptr);
+    // 2、OH_Drawing_ImageFilterDestroy
+    OH_Drawing_ImageFilterDestroy(filter);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0301
+ * @tc.name: testImageFilterDestroyNULL
+ * @tc.desc: Test for destroying an image filter with NULL parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterDestroyNULL, TestSize.Level3) {
+    // 1、OH_Drawing_ImageFilterDestroy参数为空
+    OH_Drawing_ImageFilterDestroy(nullptr);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_FILTER_0302
+ * @tc.name: testImageFilterDestroyMultipleCalls
+ * @tc.desc: Test for multiple calls of destroying an image filter.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageFilterTest, testImageFilterDestroyMultipleCalls, TestSize.Level3) {
+    // 1、OH_Drawing_ImageFilterCreateFromColorFilter，调用10次
+    OH_Drawing_ColorFilter *colorFilter;
+    OH_Drawing_ImageFilter *filter;
+    for (int i = 0; i < 10; i++) {
+        colorFilter = OH_Drawing_ColorFilterCreateLinearToSrgbGamma();
+        filter = OH_Drawing_ImageFilterCreateFromColorFilter(colorFilter, nullptr);
+    }
+    // 2、OH_Drawing_ImageFilterDestroy，调用10次
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_ImageFilterDestroy(filter);
+        filter = nullptr;
+    }
+    // 3、OH_Drawing_ImageFilterCreateBlur-OH_Drawing_ImageFilterDestroy来回调用10次
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_ImageFilter *filter = OH_Drawing_ImageFilterCreateBlur(1, 1, OH_Drawing_TileMode::MIRROR, nullptr);
+        OH_Drawing_ImageFilterDestroy(filter);
+    }
+    OH_Drawing_ImageFilterDestroy(filter);
+    OH_Drawing_ColorFilterDestroy(colorFilter);
 }
 
 } // namespace Drawing
--- graphic/acts_drawing_native/DrawingNativeImageTest.cpp
+++ graphic/acts_drawing_native/DrawingNativeImageTest.cpp
@@ -13,10 +13,12 @@
  * limitations under the License.
  */
 
-#include "gtest/gtest.h"
-
 #include "drawing_bitmap.h"
+#include "drawing_error_code.h"
 #include "drawing_image.h"
+#include "gtest/gtest.h"
+#include <cstdlib>
+#include <ctime>
 
 using namespace testing;
 using namespace testing::ext;
@@ -27,16 +29,348 @@ namespace Drawing {
 class DrawingNativeImageTest : public testing::Test {};
 
 /*
- * @tc.name: OH_Drawing_ImageDestroy
- * @tc.desc: test for OH_Drawing_ImageDestroy.
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0100
+ * @tc.name: testImageCreateDestroyNormal
+ * @tc.desc: Test for creating and destroying an image object with normal parameters.
  * @tc.size  : SmallTest
  * @tc.type  : Function
- * @tc.level : Level 1
+ * @tc.level : Level 0
  */
-HWTEST_F(DrawingNativeImageTest, OH_Drawing_ImageDestroy, TestSize.Level1) {
+HWTEST_F(DrawingNativeImageTest, testImageCreateDestroyNormal, TestSize.Level0) {
+    // 1、OH_Drawing_ImageCreate
     OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    // 2、OH_Drawing_ImageDestroy
     OH_Drawing_ImageDestroy(image);
-    EXPECT_TRUE(true);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0101
+ * @tc.name: testImageCreateDestroyNULL
+ * @tc.desc: Test for destroying an image object with a NULL parameter.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageCreateDestroyNULL, TestSize.Level3) {
+    // 1、OH_Drawing_ImageDestroy第一个参数传空
+    OH_Drawing_ImageDestroy(nullptr);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0102
+ * @tc.name: testImageCreateDestroyMultipleCalls
+ * @tc.desc: Test for multiple calls of creating and destroying an image object.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageCreateDestroyMultipleCalls, TestSize.Level3) {
+    // 1、OH_Drawing_ImageCreate-OH_Drawing_ImageDestroy来回调用10次
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+        OH_Drawing_ImageDestroy(image);
+    }
+    // 2、OH_Drawing_ImageCreate连续调用10次
+    OH_Drawing_Image *image;
+    for (int i = 0; i < 10; i++) {
+        image = OH_Drawing_ImageCreate();
+    }
+    // 3、OH_Drawing_ImageDestroy连续调用10次
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_ImageDestroy(image);
+        image = nullptr;
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0200
+ * @tc.name: testImageBuildFromBitmapNormal
+ * @tc.desc: Test for building an image from a bitmap with normal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 0
+ */
+HWTEST_F(DrawingNativeImageTest, testImageBuildFromBitmapNormal, TestSize.Level0) {
+    // 1、OH_Drawing_ImageCreate
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    // 2、OH_Drawing_BitmapCreate
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    // 3、OH_Drawing_ImageBuildFromBitmap构造图片内容成功
+    OH_Drawing_ImageBuildFromBitmap(image, bitmap);
+    EXPECT_NE(bitmap, nullptr);
+    // 4、OH_Drawing_ImageBuildFromBitmap构造图片内容失败
+    OH_Drawing_ImageBuildFromBitmap(image, nullptr);
+    // 5、释放内存
+    OH_Drawing_ImageDestroy(image);
+    OH_Drawing_BitmapDestroy(bitmap);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0201
+ * @tc.name: testImageBuildFromBitmapNULL
+ * @tc.desc: Test for building an image from a bitmap with NULL parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageBuildFromBitmapNULL, TestSize.Level3) {
+    // 1、OH_Drawing_ImageCreate
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    // 2、OH_Drawing_BitmapCreate
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    // 3、OH_Drawing_ImageBuildFromBitmap第一个参数传空，通过OH_Drawing_ErrorCodeGet查看错误码
+    OH_Drawing_ImageBuildFromBitmap(nullptr, bitmap);
+    EXPECT_EQ(OH_Drawing_ErrorCodeGet(), OH_Drawing_ErrorCode::OH_DRAWING_ERROR_INVALID_PARAMETER);
+    // 4、OH_Drawing_ImageBuildFromBitmap第二个参数传空，通过OH_Drawing_ErrorCodeGet查看错误码
+    OH_Drawing_ImageBuildFromBitmap(image, nullptr);
+    EXPECT_EQ(OH_Drawing_ErrorCodeGet(), OH_Drawing_ErrorCode::OH_DRAWING_ERROR_INVALID_PARAMETER);
+    // 5、释放内存
+    OH_Drawing_ImageDestroy(image);
+    OH_Drawing_BitmapDestroy(bitmap);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0202
+ * @tc.name: testImageBuildFromBitmapMultipleCalls
+ * @tc.desc: Test for multiple calls of building an image from a bitmap.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageBuildFromBitmapMultipleCalls, TestSize.Level3) {
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+        OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+        OH_Drawing_BitmapFormat cFormat{COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+        uint32_t width = 200 + i * 10;
+        uint32_t height = 200 + i * 10;
+        OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+        OH_Drawing_ImageBuildFromBitmap(image, bitmap);
+        OH_Drawing_ImageDestroy(image);
+        OH_Drawing_BitmapDestroy(bitmap);
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0300
+ * @tc.name: testImageGetWidthHeightNormal
+ * @tc.desc: Test for getting width and height of an image with normal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 0
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetWidthHeightNormal, TestSize.Level0) {
+    // 1、OH_Drawing_ImageCreate
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    OH_Drawing_BitmapFormat cFormat{COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+    constexpr uint32_t width = 200;
+    constexpr uint32_t height = 200;
+    OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+    OH_Drawing_ImageBuildFromBitmap(image, bitmap);
+    // 2、OH_Drawing_ImageGetWidth
+    int32_t width_ = OH_Drawing_ImageGetWidth(image);
+    EXPECT_EQ(width_, 200);
+    // 3、OH_Drawing_ImageGetHeight
+    int32_t height_ = OH_Drawing_ImageGetHeight(image);
+    EXPECT_EQ(height_, 200);
+    // 4、释放内存
+    OH_Drawing_ImageDestroy(image);
+    OH_Drawing_BitmapDestroy(bitmap);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0301
+ * @tc.name: testImageGetWidthHeightNULL
+ * @tc.desc: Test for getting width and height of an image with NULL parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetWidthHeightNULL, TestSize.Level3) {
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    OH_Drawing_BitmapFormat cFormat{COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+    uint32_t width = 200;
+    uint32_t height = 200;
+    OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+    OH_Drawing_ImageBuildFromBitmap(image, bitmap);
+    // 1、OH_Drawing_ImageGetWidth参数传空，通过OH_Drawing_ErrorCodeGet查看错误码
+    OH_Drawing_ImageGetWidth(nullptr);
+    EXPECT_EQ(OH_Drawing_ErrorCodeGet(), OH_Drawing_ErrorCode::OH_DRAWING_ERROR_INVALID_PARAMETER);
+    // 2、OH_Drawing_ImageGetHeight参数传空，通过OH_Drawing_ErrorCodeGet查看错误码
+    OH_Drawing_ImageGetHeight(nullptr);
+    EXPECT_EQ(OH_Drawing_ErrorCodeGet(), OH_Drawing_ErrorCode::OH_DRAWING_ERROR_INVALID_PARAMETER);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0302
+ * @tc.name: testImageGetWidthHeightMultipleCalls
+ * @tc.desc: Test for multiple calls of getting width and height of an image.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetWidthHeightMultipleCalls, TestSize.Level3) {
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+        OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+        OH_Drawing_BitmapFormat cFormat{COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+        uint32_t width = 200 + i * 10;
+        uint32_t height = 200 + i * 10;
+        OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+        OH_Drawing_ImageBuildFromBitmap(image, bitmap);
+        int32_t width_ = OH_Drawing_ImageGetWidth(image);
+        EXPECT_EQ(width_, 200 + i * 10);
+        int32_t height_ = OH_Drawing_ImageGetHeight(image);
+        EXPECT_EQ(height_, 200 + i * 10);
+        OH_Drawing_ImageDestroy(image);
+        OH_Drawing_BitmapDestroy(bitmap);
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0400
+ * @tc.name: testImageGetImageInfoNormal
+ * @tc.desc: Test for getting image info with normal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 0
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetImageInfoNormal, TestSize.Level0) {
+    OH_Drawing_ColorFormat cfs[] = {
+        COLOR_FORMAT_UNKNOWN,   COLOR_FORMAT_ALPHA_8,   COLOR_FORMAT_RGB_565,
+        COLOR_FORMAT_ARGB_4444, COLOR_FORMAT_RGBA_8888, COLOR_FORMAT_BGRA_8888,
+    };
+    OH_Drawing_AlphaFormat afs[] = {
+        ALPHA_FORMAT_UNKNOWN,
+        ALPHA_FORMAT_OPAQUE,
+        ALPHA_FORMAT_PREMUL,
+        ALPHA_FORMAT_UNPREMUL,
+    };
+    for (OH_Drawing_ColorFormat cf : cfs) {
+        for (OH_Drawing_AlphaFormat af : afs) {
+            OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+            OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+            OH_Drawing_BitmapFormat cFormat{cf, af};
+            uint32_t width = 400;
+            uint32_t height = 400;
+            OH_Drawing_Image_Info imageInfo;
+            OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+            OH_Drawing_ImageGetImageInfo(image, &imageInfo);
+            OH_Drawing_ImageDestroy(image);
+            OH_Drawing_BitmapDestroy(bitmap);
+        }
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0401
+ * @tc.name: testImageGetImageInfoNULL
+ * @tc.desc: Test for getting image info with NULL parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetImageInfoNULL, TestSize.Level3) {
+    // 1、OH_Drawing_ImageCreate
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    // 2、OH_Drawing_Image_Info
+    OH_Drawing_BitmapFormat cFormat{COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+    uint32_t width = 400;
+    uint32_t height = 400;
+    OH_Drawing_Image_Info imageInfo;
+    OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+    OH_Drawing_ImageGetImageInfo(image, &imageInfo);
+    // 3、OH_Drawing_ImageGetImageInfo参数传空，通过OH_Drawing_ErrorCodeGet查看错误码
+    OH_Drawing_ImageGetImageInfo(nullptr, &imageInfo);
+    EXPECT_EQ(OH_Drawing_ErrorCodeGet(), OH_Drawing_ErrorCode::OH_DRAWING_ERROR_INVALID_PARAMETER);
+    // 4、释放内存
+    OH_Drawing_ImageDestroy(image);
+    OH_Drawing_BitmapDestroy(bitmap);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0402
+ * @tc.name: testImageGetImageInfoMultipleCalls
+ * @tc.desc: Test for multiple calls of getting image info.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetImageInfoMultipleCalls, TestSize.Level3) {
+    OH_Drawing_ColorFormat cf[] = {
+        COLOR_FORMAT_UNKNOWN,   COLOR_FORMAT_ALPHA_8,   COLOR_FORMAT_RGB_565,
+        COLOR_FORMAT_ARGB_4444, COLOR_FORMAT_RGBA_8888, COLOR_FORMAT_BGRA_8888,
+    };
+    OH_Drawing_AlphaFormat af[] = {
+        ALPHA_FORMAT_UNKNOWN,
+        ALPHA_FORMAT_OPAQUE,
+        ALPHA_FORMAT_PREMUL,
+        ALPHA_FORMAT_UNPREMUL,
+    };
+    for (int i = 0; i < 10; i++) {
+        OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+        OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+        srand(static_cast<unsigned int>(time(0)));
+        OH_Drawing_BitmapFormat cFormat{cf[rand() % 5 + 1], af[rand() % 3 + 1]};
+        uint32_t width = rand() % 100 + 1;
+        uint32_t height = rand() % 100 + 1;
+        OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+        OH_Drawing_Image_Info imageInfo;
+        OH_Drawing_ImageGetImageInfo(image, &imageInfo);
+        OH_Drawing_ImageDestroy(image);
+        OH_Drawing_BitmapDestroy(bitmap);
+    }
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0403
+ * @tc.name: testImageGetImageInfoAbnormal
+ * @tc.desc: Test for getting image info with abnormal parameters.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetImageInfoAbnormal, TestSize.Level3) {
+    // 1、OH_Drawing_ImageCreate
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    // 2、OH_Drawing_ImageGetImageInfo调用OH_Drawing_Image_Info创建时width=-400，Height=-400
+    OH_Drawing_BitmapFormat cFormat{COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+    uint32_t width = -400;
+    uint32_t height = -400;
+    OH_Drawing_Image_Info imageInfo;
+    OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+    OH_Drawing_ImageGetImageInfo(image, &imageInfo);
+    // 3、释放内存
+    OH_Drawing_ImageDestroy(image);
+    OH_Drawing_BitmapDestroy(bitmap);
+}
+
+/*
+ * @tc.number: SUB_BASIC_GRAPHICS_SPECIAL_API_C_DRAWING_IMAGE_0404
+ * @tc.name: testImageGetImageInfoMaximum
+ * @tc.desc: Test for getting image info with maximum values.
+ * @tc.size  : SmallTest
+ * @tc.type  : Function
+ * @tc.level : Level 3
+ */
+HWTEST_F(DrawingNativeImageTest, testImageGetImageInfoMaximum, TestSize.Level3) {
+    // 1、OH_Drawing_ImageCreate
+    OH_Drawing_Image *image = OH_Drawing_ImageCreate();
+    OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
+    // 2、OH_Drawing_ImageGetImageInfo调用OH_Drawing_Image_Info创建时width=极大值，Height=极大值
+    OH_Drawing_BitmapFormat cFormat {COLOR_FORMAT_RGBA_8888, ALPHA_FORMAT_OPAQUE};
+    uint32_t width = UINT32_MAX;
+    uint32_t height = UINT32_MAX;
+    OH_Drawing_Image_Info imageInfo;
+    OH_Drawing_BitmapBuild(bitmap, width, height, &cFormat);
+    OH_Drawing_ImageGetImageInfo(image, &imageInfo);
+    // 3、释放内存
+    OH_Drawing_ImageDestroy(image);
+    OH_Drawing_BitmapDestroy(bitmap);
 }
 
 } // namespace Drawing
