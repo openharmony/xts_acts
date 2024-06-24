6a5ddff0fb1bb79ddf06476a4f4fa3c874350742
dongwei <dongwei@kaihong.com>
2024-06-24 11:42:29 +0800

    ActsNetworkMgrStatisticsNetTest hvigor整改
    Signed-off-by: dongwei <dongwei@kaihong.com>

--- telephony/networkmanager/statistics/statisticswifi/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
+++ telephony/networkmanager/statistics/statisticswifi/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
@@ -1,6 +1,5 @@
-
 /*
- * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -47,7 +46,7 @@ export default class OpenHarmonyTestRunner implements TestRunner {
             onAbilityCreate: onAbilityCreateCallback,
         };
         abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
-        var cmd = 'aa start -d 0 -a TestAbility' + ' -b ' + abilityDelegatorArguments.bundleName
+        var cmd = 'aa start -d 0 -m entry_test' + ' -b ' + abilityDelegatorArguments.bundleName
         var debug = abilityDelegatorArguments.parameters['-D']
         if (debug == 'true')
         {
