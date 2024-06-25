b325fa830ccaef1c564ddbdc940edef63869aecb
dongwei <dongwei@kaihong.com>
2024-06-25 11:41:48 +0800

    ActsNetWorkMgrNetworkSecurityTest hvigor整改
    Signed-off-by: dongwei <dongwei@kaihong.com>

--- telephony/networkmanager/networksecurity/networksecuritytest/hvigorfile.ts
+++ telephony/networkmanager/networksecurity/networksecuritytest/hvigorfile.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,20 +13,5 @@
  * limitations under the License.
  */
 
-@Entry
-@Component
-struct Index {
-  @State message: string = 'Hello World';
-
-  build() {
-    Row() {
-      Column() {
-        Text(this.message)
-          .fontSize(50)
-          .fontWeight(FontWeight.Bold)
-      }
-      .width('100%')
-    }
-    .height('100%')
-  }
-}
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { appTasks } from '@ohos/hvigor-ohos-plugin';
