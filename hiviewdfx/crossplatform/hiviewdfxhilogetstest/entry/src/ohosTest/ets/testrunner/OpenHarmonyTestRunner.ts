dc1f8c2ed1755e70011173285e0b5b99fe42d7bb
zhangmenghan <zhangmenghan@kaihong.com>
2024-06-25 09:37:58 +0800

    hiviewdfx/crossplatform/hiviewdfxhilogetstest
    Signed-off-by: zhangmenghan <zhangmenghan@kaihong.com>

--- hiviewdfx/crossplatform/hiviewdfxhilogetstest/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
+++ hiviewdfx/crossplatform/hiviewdfxhilogetstest/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -47,8 +47,7 @@ export default class OpenHarmonyTestRunner implements TestRunner {
         abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
         var cmd = 'aa start -d 0 -a TestAbility' + ' -b ' + abilityDelegatorArguments.bundleName
         var debug = abilityDelegatorArguments.parameters['-D']
-        if (debug == 'true')
-        {
+        if (debug == 'true') {
             cmd += ' -D'
         }
         hilog.info(0x0000, 'testTag', 'cmd : %{public}s', cmd);
