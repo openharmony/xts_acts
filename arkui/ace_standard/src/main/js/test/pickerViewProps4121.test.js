a02be2027030ff81eba7538b6e7013e11b545bda
张晓冉 <zhangxiaoran7@huawei.com>
2024-07-08 10:11:06 +0800

    revert arkui 3 files and git lfs vedio
    Signed-off-by: 张晓冉 <zhangxiaoran7@huawei.com>

---
+++ arkui/ace_standard/src/main/js/test/pickerViewProps4121.test.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ import router from '@system.router';
+ import {describe, beforeAll,afterAll, it, expect} from '@ohos/hypium';
+ 
+ 
+ export default function pickerViewPropsJsTest4121() {  describe('pickerViewPropsJsTest4121', function () {
+ 
+     async function sleep(time) {
+         return new Promise((resolve, reject) => {
+             setTimeout(() => {
+                 resolve()
+             }, time)
+         }).then(() => {
+             console.info(`sleep ${time} over...`)
+         })
+     }
+ 
+     async function backToIndex() {
+         let backToIndexPromise = new Promise((resolve, reject) => {
+             setTimeout(() => {
+                 router.back({
+                     uri: 'pages/index/index'
+                 });
+                 resolve();
+             }, 500);
+         })
+         let clearPromise = new Promise((resolve, reject) => {
+             setTimeout(() => {
+                 router.clear();
+                 resolve();
+             }, 500);
+         })
+         await backToIndexPromise.then(() => {
+             return clearPromise;
+         })
+     }
+ 
+     /**
+     * run before testcase
+     */
+     beforeAll(async function (done) {
+         console.info('[pickerViewPropsJsTest] before each called')
+ 
+         let result;
+         let options = {
+             uri: 'pages/pickerView/prop4121/index'
+         }
+         try {
+             result = router.push(options)
+             console.info("push pickerViewProps page success " + JSON.stringify(result));
+         } catch (err) {
+             console.error("push pickerViewProps page error " + JSON.stringify(result));
+         }
+         await sleep(35000)
+         done()
+     })
+ 
+     /**
+     * run after testcase
+     */
+     afterAll(async function () {
+         console.info('[pickerViewPropsJsTest] after each called')
+         await backToIndex()
+         await sleep(1000)
+     })
+
+ 
+     /**
+      * @tc.number    SUB_ACE_BASIC_COMPONENT_JS_API_0100
+      * @tc.name      testPickerViewPickerViewTime
+      * @tc.desc      ACE
+      */
+     it('testPickerViewPickerViewTime', 0, async function (done) {
+         console.info('testPickerViewPickerViewTime START');
+         console.info("[pickerViewProps] get globalThis.value is: " + JSON.stringify(globalThis.value));
+ 
+         let obj = JSON.parse(globalThis.value.pickerViewTime);
+         console.info("[pickerViewProps] get inspector value is: " + JSON.stringify(obj));
+         console.info("[pickerViewProps] get inspector attrs value is: " + JSON.stringify(obj.$attrs));
+ 
+         expect(obj.$type).assertEqual('picker-view')
+         expect(obj.$attrs.id).assertEqual('pickerViewTime')
+         expect(obj.$attrs.type).assertEqual('time')
+         console.info("[pickerViewProps] get type value is: " + JSON.stringify(obj.$attrs.type));
+         done();
+     });
+
+ });
+}
