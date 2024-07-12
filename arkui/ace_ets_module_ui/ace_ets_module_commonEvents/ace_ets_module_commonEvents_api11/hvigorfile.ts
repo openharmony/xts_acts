bd7175753f1572626be0948b05b19b0b54ffbc5e
pavelyevivan <pavelyev.ivan@huawei-partners.com>
2024-07-12 12:11:56 +0300

    Add hvigor for ce_ets_module_commonEvents_api11
    Signed-off-by: pavelyevivan <pavelyev.ivan@huawei-partners.com>
    Change-Id: Ic53a00ccde1206459d397233bcb4ece0ae63fef9

--- arkui/ace_ets_module_ui/ace_ets_module_commonEvents/ace_ets_module_commonEvents_api11/hvigorfile.ts
+++ arkui/ace_ets_module_ui/ace_ets_module_commonEvents/ace_ets_module_commonEvents_api11/hvigorfile.ts
@@ -1,5 +1,4 @@
-// @ts-nocheck
-/**
+/*
  * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,19 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export default class Utils {
 
-  static sleep(time: number) {
-    return new Promise((resolve: (value: string) => void) => {
-      setTimeout(() => {
-        resolve()
-      }, time)
-    }).then(() => {
-      console.info(`sleep ${time} over...`)
-    })
-  }
-}
-
-
-
-
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { appTasks } from '@ohos/hvigor-ohos-plugin';
