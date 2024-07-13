4a61f3d7881bd15ed889ddf36ad7bca24bf4288a
yuanyuecheng24298 <yuanyuecheng@talkweb.com.cn>
2024-07-13 16:49:18 +0800

    【XTS语法适配】ActsOnAndOffScreenTest 语法适配
    Signed-off-by: yuanyuecheng24298 <yuanyuecheng@talkweb.com.cn>

--- ability/ability_runtime/actssupportfunction/stageonandoffscreen/entry/src/main/ets/MainAbility3/MainAbility3.ts
+++ ability/ability_runtime/actssupportfunction/stageonandoffscreen/entry/src/main/ets/MainAbility3/MainAbility3.ts
@@ -14,48 +14,49 @@
  */
 import Ability from '@ohos.app.ability.UIAbility';
 import commonEvent from '@ohos.commonEvent';
+import commonEventManager from '@ohos.commonEventManager';
 
 var TAG3 = 'StageOnAndOffScreen:MainAbility3:';
 var listPush3 = "StageOnAndOffScreen_MainAbility3_";
+
 export default class MainAbility3 extends Ability {
+  onCreate(want, launchParam) {
+    console.log(TAG3 + 'onCreate');
+  }
 
-    onCreate(want, launchParam) {
-        console.log(TAG3 + 'onCreate');
-    }
+  onDestroy() {
+    console.log(TAG3 + 'onDestroy');
+  }
 
-    onDestroy() {
-        console.log(TAG3 + 'onDestroy');
-    }
+  onWindowStageCreate(windowStage) {
+    console.log(TAG3 + 'onWindowStageCreate');
 
-    onWindowStageCreate(windowStage) {
-        console.log(TAG3 + 'onWindowStageCreate');
+    windowStage.loadContent("pages/third", (err, data) => {
+      if (err.code) {
+        console.log(TAG3 + 'Failed to load the content. Cause:' + JSON.stringify(err));
+        return;
+      }
+      console.log(TAG3 + 'Succeeded in loading the content. Data: ' + JSON.stringify(data));
+    });
+  }
 
-        windowStage.loadContent("pages/third", (err, data) => {
-            if (err.code) {
-                console.log(TAG3 + 'Failed to load the content. Cause:' + JSON.stringify(err));
-                return;
-            }
-            console.log(TAG3 + 'Succeeded in loading the content. Data: ' + JSON.stringify(data));
-        });
-    }
+  onWindowStageDestroy() {
+    console.log(TAG3 + 'onWindowStageDestroy');
+  }
 
-    onWindowStageDestroy() {
-        console.log(TAG3 + 'onWindowStageDestroy');
-    }
+  onForeground() {
+    console.log(TAG3 + 'onForeground');
 
-    onForeground() {
-        console.log(TAG3 + 'onForeground');
+    commonEventManager.publish(listPush3 + "onForeground", (err) => {
+      console.log(TAG3 + listPush3 + "onForeground");
+    });
+  }
 
-        commonEvent.publish(listPush3 + "onForeground", (err) => {
-            console.log(TAG3 + listPush3 + "onForeground");
-        });
-    }
+  onBackground() {
+    console.log(TAG3 + 'onBackground');
 
-    onBackground() {
-        console.log(TAG3 + 'onBackground');
-
-        commonEvent.publish(listPush3 + "onBackground", (err) => {
-            console.log(TAG3 + listPush3 + "onBackground");
-        });
-    }
+    commonEventManager.publish(listPush3 + "onBackground", (err) => {
+      console.log(TAG3 + listPush3 + "onBackground");
+    });
+  }
 };
