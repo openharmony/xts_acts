a99703db1229f421e37fa570308c5c0b5505ae08
zhuzhengjun <zhuzhengjun@openvalley.net>
2024-07-13 14:09:16 +0800

    【XTS语法适配】ActsAbilityMonitorTest
    Signed-off-by: zhuzhengjun <zhuzhengjun@openvalley.net>

--- ability/ability_runtime/abilitymontior/entry/src/main/ets/MainAbility/MainAbility.ts
+++ ability/ability_runtime/abilitymontior/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -18,36 +18,36 @@ import { Hypium } from '@ohos/hypium'
 import testsuite from '../test/List.test'
 
 export default class MainAbility extends Ability {
-    onCreate(want, launchParam) {
-        console.log('MainAbility onCreate')
-        globalThis.abilityWant = want;
-        globalThis.abilityWant.parameters.timeout = 15000;
-        var abilityDelegator: any
-        abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
-        var abilityDelegatorArguments: any
-        abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
-        console.info('start run testcase!!!')
-        Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite)
-    }
+  onCreate(want, launchParam) {
+    console.log('MainAbility onCreate')
+    globalThis.abilityWant = want;
+    globalThis.abilityWant.parameters.timeout = 15000;
+    var abilityDelegator: any
+    abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
+    var abilityDelegatorArguments: any
+    abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
+    console.info('start run testcase!!!')
+    Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite)
+  }
 
-    onDestroy() {
-        console.log('MainAbility onDestroy')
-    }
+  onDestroy() {
+    console.log('MainAbility onDestroy')
+  }
 
-    onWindowStageCreate(windowStage) {
-        console.log('MainAbility onWindowStageCreate')
-        windowStage.setUIContent(this.context, 'MainAbility/pages/index', null)
-    }
+  onWindowStageCreate(windowStage) {
+    console.log('MainAbility onWindowStageCreate')
+    windowStage.setUIContent(this.context, 'MainAbility/pages/index', null)
+  }
 
-    onWindowStageDestroy() {
-        console.log('MainAbility onWindowStageDestroy')
-    }
+  onWindowStageDestroy() {
+    console.log('MainAbility onWindowStageDestroy')
+  }
 
-    onForeground() {
-        console.log('MainAbility onForeground')
-    }
+  onForeground() {
+    console.log('MainAbility onForeground')
+  }
 
-    onBackground() {
-        console.log('MainAbility onBackground')
-    }
+  onBackground() {
+    console.log('MainAbility onBackground')
+  }
 };
