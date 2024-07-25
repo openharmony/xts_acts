4185fbabffb51010c6bf272659bb0182f57dfd4d
gct <guochaotao@h-partners.com>
2024-07-25 16:22:35 +0800

    修复bug & 修改部分不正确的注释
    Signed-off-by: gct <guochaotao@h-partners.com>

--- multimedia/photoAccess/common.ts
+++ multimedia/photoAccess/common.ts
@@ -100,7 +100,6 @@ export function photoFetchOption(testNum, key, value) : photoAccessHelper.FetchO
       photoKeys.DATE_ADDED_MS,
       photoKeys.DATE_MODIFIED_MS,
       photoKeys.DYNAMIC_RANGE_TYPE,
-      COVER_POSITION,
       'all_exif',
     ],
     predicates: predicates
