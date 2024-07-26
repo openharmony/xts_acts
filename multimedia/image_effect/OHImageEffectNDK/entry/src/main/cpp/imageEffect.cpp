88e4766572cb1e77ee367270a5e07c4fb9ad55fe
刘志豪 <liuzhihao25@huawei.com>
2024-07-26 14:59:29 +0800

    添加图片渲染接口的测试用例
    Signed-off-by: 刘志豪 <liuzhihao25@huawei.com>
    (cherry picked from commit c54a747e394c8648075ebca2bef1722d52208a26)

--- multimedia/image_effect/OHImageEffectNDK/entry/src/main/cpp/imageEffect.cpp
+++ multimedia/image_effect/OHImageEffectNDK/entry/src/main/cpp/imageEffect.cpp
@@ -64,6 +64,7 @@
 
 const std::string g_jpgUri = std::string("file:///data/test/resource/image_effect_1k_test1.jpg");
 static std::string imagePath;
+static std::string outImagePath;
 static napi_value savePixelMapForPath(napi_env env, napi_callback_info info)
 {
     napi_value result = nullptr;
@@ -75,6 +76,23 @@ static napi_value savePixelMapForPath(napi_env env, napi_callback_info info)
     napi_value ret;
     int status = -1;
     if (imagePath.length() > 0) {
+        status = 0;
+    }
+    napi_create_int32(env, status, &ret);
+    return ret;
+}
+
+static napi_value creatOutPixeMapForPath(napi_env env, napi_callback_info info)
+{
+    napi_value result = nullptr;
+    napi_get_undefined(env, &result);
+    size_t argc = 1;
+    napi_value args[1] = {nullptr};
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    outImagePath = CommonUtils::GetStringArgument(env, args[0]);
+    napi_value ret;
+    int status = -1;
+    if (outImagePath.length() > 0) {
         status = 0;
     }
     napi_create_int32(env, status, &ret);
@@ -1116,6 +1134,12 @@ static napi_value OHEffectFilterReleaseFilterNames(napi_env env, napi_callback_i
 
 static napi_value OHEffectFilterRender(napi_env env, napi_callback_info info)
 {
+    size_t argc = 1;
+    napi_value args[1] = {nullptr};
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    int32_t index;
+    napi_get_value_int32(env, args[0], &index);
+    
     std::shared_ptr<OH_PixelmapNative> pixelmapNativePtr = PixelMapHelper::Decode(imagePath);
     OH_PixelmapNative *inputPixelmap = pixelmapNativePtr.get();
     OH_PixelmapNative *outputPixelmap = inputPixelmap;
@@ -1153,6 +1177,21 @@ static napi_value OHEffectFilterRender(napi_env env, napi_callback_info info)
     // 创建滤镜。比如：创建对比度效果器
     OH_EffectFilter *filter = OH_EffectFilter_Create(CUSTOM_FILTER);
     // 生效滤镜效果
+    switch (index) {
+    case CASE_INDEX_1:
+        outputPixelmap = nullptr;
+        break;
+    case CASE_INDEX_2:
+        outputPixelmap = inputPixelmap;
+        break;
+    case CASE_INDEX_3: {
+        std::shared_ptr<OH_PixelmapNative> outPixePtr = PixelMapHelper::Decode(outImagePath);
+        outputPixelmap = outPixePtr.get();
+    }
+        break;
+    default:
+        break;
+    }
     errorCode = OH_EffectFilter_Render(filter, inputPixelmap, outputPixelmap);
     // 销毁滤镜实例
     OH_EffectFilter_Release(filter);
@@ -1160,8 +1199,6 @@ static napi_value OHEffectFilterRender(napi_env env, napi_callback_info info)
     napi_create_int32(env, errorCode, &ret);
     return ret;
 }
-
-
 /**-------------------------------------------------- Effect -------------------------------------------------------**/
 static napi_value OHImageEffectCreate(napi_env env, napi_callback_info info)
 {
@@ -1564,7 +1601,7 @@ static napi_value OHImageEffectSetInputNativeBuffer(napi_env env, napi_callback_
     OH_NativeBuffer *buffer = OH_NativeBuffer_Alloc(&config);
     if (code == EFFECT_SUCCESS && buffer != nullptr) {
         code = OH_ImageEffect_SetInputNativeBuffer(imageEffect, buffer);
-    }
+    } 
     napi_value ret;
     napi_create_int32(env, code, &ret);
     return ret;
@@ -1938,16 +1975,22 @@ static napi_value OHImageEffectStart(napi_env env, napi_callback_info info)
     
     // 4、设置输入的Pixelmap
     errorCode = OH_ImageEffect_SetInputPixelmap(imageEffect, inputPixelmap);
-
+    if (errorCode != EFFECT_SUCCESS) {
+        napi_value ret;
+        napi_create_int32(env, errorCode, &ret);
+        return ret;
+    }
     // 5、设置输出的Pixelmap（可选），不调用该接口时会在输入Pixelmap上直接生效滤镜效果
     errorCode = OH_ImageEffect_SetOutputPixelmap(imageEffect, outputPixelmap);
-
+    if (errorCode != EFFECT_SUCCESS) {
+        napi_value ret;
+        napi_create_int32(env, errorCode, &ret);
+        return ret;
+    }
     // 6、执行生效滤镜效果
     errorCode = OH_ImageEffect_Start(imageEffect);
-
     // 7、释放imageEffect实例资源
     OH_ImageEffect_Release(imageEffect);
-
     napi_value ret;
     napi_create_int32(env, errorCode, &ret);
     return ret;
@@ -1974,13 +2017,25 @@ static napi_value OHImageEffectStop(napi_env env, napi_callback_info info)
 
     // 4、设置输入的Pixelmap
     errorCode = OH_ImageEffect_SetInputPixelmap(imageEffect, inputPixelmap);
-
+    if (errorCode != EFFECT_SUCCESS) {
+        napi_value ret;
+        napi_create_int32(env, errorCode, &ret);
+        return ret;
+    }
     // 5、设置输出的Pixelmap（可选），不调用该接口时会在输入Pixelmap上直接生效滤镜效果
     errorCode = OH_ImageEffect_SetOutputPixelmap(imageEffect, outputPixelmap);
-
+    if (errorCode != EFFECT_SUCCESS) {
+        napi_value ret;
+        napi_create_int32(env, errorCode, &ret);
+        return ret;
+    }
     // 6、执行生效滤镜效果
     errorCode = OH_ImageEffect_Start(imageEffect);
-
+    if (errorCode != EFFECT_SUCCESS) {
+        napi_value ret;
+        napi_create_int32(env, errorCode, &ret);
+        return ret;
+    }
     // 7、执行生效滤镜效果
     errorCode = OH_ImageEffect_Stop(imageEffect);
 
@@ -1994,6 +2049,7 @@ static napi_value OHImageEffectStop(napi_env env, napi_callback_info info)
 
 napi_property_descriptor desc1[] = {
     {"savePixelMapForPath", nullptr, savePixelMapForPath, nullptr, nullptr, nullptr, napi_default, nullptr},
+    {"creatOutPixeMapForPath", nullptr, creatOutPixeMapForPath, nullptr, nullptr, nullptr, napi_default, nullptr},
     {"OHEffectBufferInfoCreate", nullptr, OHEffectBufferInfoCreate, nullptr, nullptr, nullptr, napi_default, nullptr},
     {"OHEffectBufferInfoSetAddr", nullptr, OHEffectBufferInfoSetAddr, nullptr, nullptr, nullptr, napi_default, nullptr},
     {"OHEffectBufferInfoGetAddr", nullptr, OHEffectBufferInfoGetAddr, nullptr, nullptr, nullptr, napi_default, nullptr},
