ac45f040e985a39a5684e34bbfd6187a96ac1116
fuwei <fuwei45@huawei.com>
2024-08-06 11:24:52 +0000

    add new getxattr
    Signed-off-by: fuwei <fuwei45@huawei.com>

---
+++ storage/storagefileiov9jstest/entry/src/ohosTest/js/test/members/getxattr.test.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    FILE_CONTENT, prepareFile, nextFileName, describe, it, expect
+  } from '../Common';
+import fs from '@ohos.file.fs';
+
+export default function fileIOGetxattr() {
+describe('fileIO_test_getxattr', function () {
+
+  /**
+   * @tc.number SUB_BASIC_FM_FileAPI_FileIo_GETXATTR_Async_0100
+   * @tc.name test_FileIO_Getxattr_Async_001
+   * @tc.desc Test getxattr() interface.
+   * This interface shall work properly in normal case.
+   * @tc.size MediumTest
+   * @tc.type Function
+   * @tc.level Level 3
+   * @tc.require
+   */  
+  it('test_FileIO_Getxattr_Async_001', 3, async function (done) {
+    let fpath = await nextFileName('test_FileIO_Getxattr_Async_001');
+    let attrKey = 'user.comment';
+    let attrValue = 'Test file1.';
+    expect(prepareFile(fpath, FILE_CONTENT)).assertTrue();
+    await fs.setxattr(fpath, attrKey, attrValue);
+    
+    try {
+      fs.getxattr(fpath, attrKey).then((attrValue) => {
+        console.log("test_FileIO_Getxattr_Async_001 the value is: " + attrValue);
+        expect(attrValue).assertEqual('Test file1.');
+      }).catch((err) => {
+        console.error("test_FileIO_Getxattr_Async_001 Failed to get extended attribute: " + err.message + ", error code: " + err.code);
+      });
+      done();
+    } catch (e) {
+      console.log('test_FileIO_Getxattr_Async_001 has failed for ' + e.message + ', code: ' + e.code);
+      expect(false).assertTrue();
+    }
+   });
+
+  /**
+   * @tc.number SUB_BASIC_FM_FileAPI_FileIo_GETXATTR_Sync_0200
+   * @tc.name test_FileIO_Getxattr_Sync_002
+   * @tc.desc Test getxattr() interface.
+   * This interface shall work properly in normal case.
+   * @tc.size MediumTest
+   * @tc.type Function
+   * @tc.level Level 3
+   * @tc.require
+   */  
+  it('test_FileIO_Getxattr_Sync_002', 3, async function () {
+    let fpath = await nextFileName('test_FileIO_Getxattr_Sync_002');
+    let attrKey = 'user.comment';
+    let attrValue = 'Test file.';
+    expect(prepareFile(fpath, FILE_CONTENT)).assertTrue();
+    await fs.setxattr(fpath, attrKey, attrValue);
+    
+    try {
+      let attrValue = fs.getxattrSync(fpath, attrKey);
+      console.log("test_FileIO_Getxattr_Async_002 the value is: " + attrValue);
+      expect(attrValue).assertEqual('Test file.');
+    } catch (e) {
+      console.log('test_FileIO_Getxattr_Sync_002 has failed for ' + e.message + ', code: ' + e.code);
+      expect(false).assertTrue();
+    }
+   });
+});
+}
