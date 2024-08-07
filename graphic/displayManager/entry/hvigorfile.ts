c9e116dccfec83d225f0f28656a65545ddb37071
杜蕊 <durui15@huawei.com>
2024-08-07 16:22:32 +0800

    diaplayManager新增用例
    Signed-off-by: 杜蕊 <durui15@huawei.com>

--- graphic/displayManager/entry/hvigorfile.ts
+++ graphic/displayManager/entry/hvigorfile.ts
@@ -12,12 +12,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import AbilityStage from "@ohos.app.ability.AbilityStage"
-
-export default class MyAbilityStage extends AbilityStage {
-    onCreate() {
-        console.log("[Demo] MyAbilityStage onCreate")
-        globalThis.stageOnCreateRun = 1;
-        globalThis.stageContext = this.context;
-    }
-}
+export { hapTasks } from '@ohos/hvigor-ohos-plugin';
