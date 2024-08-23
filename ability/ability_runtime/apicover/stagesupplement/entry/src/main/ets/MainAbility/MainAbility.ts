3e89e461c74920496f19083fdc124dc2d9a0a54a
lixiaogang30 <lixiaogang30@huawei.com>
2024-08-23 10:54:15 +0800

    添加接口覆盖用例SUB_Ability_AbilityRuntime_ProcessCache_3000,增加对module.json5中获取到的wearable字段断言
    Signed-off-by: lixiaogang30 <lixiaogang30@huawei.com>

--- ability/ability_runtime/apicover/stagesupplement/entry/src/main/ets/MainAbility/MainAbility.ts
+++ ability/ability_runtime/apicover/stagesupplement/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -27,7 +27,13 @@ let lastRequestWant;
 export default class MainAbility extends Ability {
   onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
     console.info(printLog1 + "onCreate");
-
+    if (want.action === 'SUB_Ability_AbilityRuntime_ProcessCache_3000') {
+      this.context.getApplicationContext().killAllProcesses().then(data => {
+        console.log('killAllProcesses success',JSON.stringify(data));
+      }).catch(err => {
+        console.log('killAllProcesses fail',JSON.stringify(err.code));
+      });
+    };
     launchWant = this.launchWant
     lastRequestWant = this.lastRequestWant
     commonEvent.publish(listPush1 + "onCreate", (err) => {
