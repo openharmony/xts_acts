2c3a1cc7d0c712a88e2a4c4a852b6565329e981e
oddzz <zhengyingna1@huawei.com>
2024-08-28 05:57:50 +0000

    update multimodalinput/multimodalinput_ndk_system/entry/src/main/cpp/inputndksys.cpp.
    Signed-off-by: oddzz <zhengyingna1@huawei.com>

--- multimodalinput/multimodalinput_ndk_system/entry/src/main/cpp/inputndksys.cpp
+++ multimodalinput/multimodalinput_ndk_system/entry/src/main/cpp/inputndksys.cpp
@@ -379,5 +379,5 @@ static napi_module demoModule = {
 
 extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
 {
-    napi_module_register(&demoModule); 
+    napi_module_register(&demoModule);
 }
