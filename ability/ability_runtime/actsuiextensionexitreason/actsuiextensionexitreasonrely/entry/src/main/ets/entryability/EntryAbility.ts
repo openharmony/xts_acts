ebc58f3801952a019a4905d1659a03ebbfe925d0
zhangmenghan <zhangmenghan@kaihong.com>
2024-09-04 11:40:46 +0800

    actsuiextensionexitreason
    Signed-off-by: zhangmenghan <zhangmenghan@kaihong.com>

--- ability/ability_runtime/actsuiextensionexitreason/actsuiextensionexitreasonrely/entry/src/main/ets/entryability/EntryAbility.ts
+++ ability/ability_runtime/actsuiextensionexitreason/actsuiextensionexitreasonrely/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2024 Huawei Device Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-import UIAbility from '@ohos.app.ability.UIAbility';
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { AbilityConstant, UIAbility, Want } from '@kit.AbilityKit';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 import AbilityDelegatorRegistry from '@ohos.app.ability.abilityDelegatorRegistry';
@@ -39,14 +39,14 @@ let setWant = (want) => {
 }
 
 export default class EntryAbility extends UIAbility {
-  onCreate(want, launchParam) {
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
     console.info('====> [UIExtensionForResultAsCallerRely] onCreate._0100——0008：%{public}s',
-      want.parameters?.UIExtensionAbilityName);
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate' + '_0100——0009');
-    setWant(want);
+    want.parameters?.UIExtensionAbilityName);
+  hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate' + '_0100——0009');
+  setWant(want);
   }
 
-  onNewWant(want, launchParam) {
+  onNewWant(want: Want, launchParam: AbilityConstant.LaunchParam) {
     console.info('====> [UIExtensionForResultAsCallerRely] onNewWant._0100——0010：%{public}s',
       want.parameters?.UIExtensionAbilityName);
     setWant(want);
--- ability/ability_runtime/actsuiextensionexitreason/actsuiextensionexitreasonrely/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
+++ ability/ability_runtime/actsuiextensionexitreason/actsuiextensionexitreasonrely/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
@@ -1,17 +1,17 @@
 /*
-* Copyright (c) 2024 Huawei Device Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import hilog from '@ohos.hilog';
 import TestRunner from '@ohos.application.testRunner';
 import AbilityDelegatorRegistry from '@ohos.app.ability.abilityDelegatorRegistry';
@@ -47,8 +47,7 @@ export default class OpenHarmonyTestRunner implements TestRunner {
         abilityDelegator.addAbilityMonitor(lMonitor, addAbilityMonitorCallback)
         var cmd = 'aa start -d 0 -a TestAbility' + ' -b ' + abilityDelegatorArguments.bundleName
         var debug = abilityDelegatorArguments.parameters['-D']
-        if (debug == 'true')
-        {
+        if (debug == 'true') {
             cmd += ' -D'
         }
         hilog.info(0x0000, 'testTag', 'cmd : %{public}s', cmd);
