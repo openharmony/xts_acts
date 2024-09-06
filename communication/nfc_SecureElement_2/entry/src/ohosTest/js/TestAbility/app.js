fd21d41fd75ddfcbf470c2205191131221ce4c8a
xuxiaoyuan94 <xuxiaoyuan072@chinasoftinc.com>
2024-09-06 09:41:46 +0800

    codecheck
    Signed-off-by: xuxiaoyuan94 <xuxiaoyuan072@chinasoftinc.com>

--- communication/nfc_SecureElement_2/entry/src/ohosTest/js/TestAbility/app.js
+++ communication/nfc_SecureElement_2/entry/src/ohosTest/js/TestAbility/app.js
@@ -20,8 +20,8 @@ import testsuite from '../test/List.test';
 export default {
     onCreate() {
         hilog.info(0x0000, 'testTag', '%{public}s', 'TestApplication onCreate');
-        var abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
-        var abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
+        let abilityDelegator = AbilityDelegatorRegistry.getAbilityDelegator()
+        let abilityDelegatorArguments = AbilityDelegatorRegistry.getArguments()
         hilog.info(0x0000, 'testTag', '%{public}s', 'start run testcase!!!');
         Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite)
     },
