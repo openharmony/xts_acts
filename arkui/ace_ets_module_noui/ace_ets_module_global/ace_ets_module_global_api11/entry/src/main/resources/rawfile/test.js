505d0e68e43a0ee9f9e5943a1c9b26aeeacc0ba9
Andrey Khudenkikh <khudenkikh.andrey@huawei-partners.com>
2024-09-18 16:24:12 +0300

    hvigor conversion ace_ets_module_global_api11
    Signed-off-by: Andrey Khudenkikh <khudenkikh.andrey@huawei-partners.com>
    Change-Id: Ia1083269462cf6c087b638e630d2833debbff0fd

--- arkui/ace_ets_module_noui/ace_ets_module_global/ace_ets_module_global_api11/entry/src/main/resources/rawfile/test.js
+++ arkui/ace_ets_module_noui/ace_ets_module_global/ace_ets_module_global_api11/entry/src/main/resources/rawfile/test.js
@@ -12,4 +12,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-document.body.innerHTML+="<p>test file</p>"
+document.body.innerHTML += "<p>test file</p>"
