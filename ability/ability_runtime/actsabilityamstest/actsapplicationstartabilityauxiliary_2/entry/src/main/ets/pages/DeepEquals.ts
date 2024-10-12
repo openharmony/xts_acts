4947e1b47776360e38da34af8c88aacc3e588d76
tang-lizhen <tang_lizhen@runkaihong.com.cn>
2024-10-12 14:10:57 +0800

    版权和许可声明
    Signed-off-by: tang-lizhen <tang_lizhen@runkaihong.com.cn>

--- ability/ability_runtime/actsabilityamstest/actsapplicationstartabilityauxiliary_2/entry/src/main/ets/pages/DeepEquals.ts
+++ ability/ability_runtime/actsabilityamstest/actsapplicationstartabilityauxiliary_2/entry/src/main/ets/pages/DeepEquals.ts
@@ -1,3 +1,18 @@
+/*
+  * Copyright (C) 2024 HiHope Open Source Organization.
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *    http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+*/
+
 export class DeepEquals {
   public static deepEqual<T>(obj1: T, obj2: T): boolean {
     if (obj1 === obj2) return true; // 如果两者引用相同，直接返回true
