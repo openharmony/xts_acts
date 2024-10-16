0d06a936a4d6acae1e3442bfcac6f794cd1d4ca1
guotao <guotao76@huawei.com>
2024-10-16 16:14:55 +0800

    hvigor
    Signed-off-by: guotao <guotao76@huawei.com>

---
+++ distributeddatamgr/relationalStoretest/relationalStoreNdktest/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export const CreateValueObject: () => number;
+export const CreateValuesBucket: () => number;
+export const CreatePredicatesOne: () => number;
+export const CreatePredicatesTwo: () => number;
+export const CreatePredicatesThree: () => number;
+export const GetOrOpenOne: () => number;
+export const GetOrOpenTwo: () => number;
+export const GetOrOpenThree: () => number;
+export const GetOrOpenFour: () => number;
+export const CloseStoreOne: () => number;
+export const CloseStoreTwo: () => number;
+export const CloseStoreThree: () => number;
+export const DeleteStoreOne: () => number;
+export const DeleteStoreTwo: () => number;
+export const DeleteStoreThree: () => number;
+export const InsertOne: () => number;
+export const InsertTwo: () => number;
+export const InsertThree: () => number;
+export const InsertFour: () => number;
+export const InsertFive: () => number;
+export const UpdateOne: () => number;
+export const UpdateTwo: () => number;
+export const UpdateThree: () => number;
+export const UpdateFour: () => number;
+export const UpdateFive: () => number;
+export const UpdateSix: () => number;
+export const UpdateSeven: () => number;
+export const DeleteOne: () => number;
+export const DeleteTwo: () => number;
+export const DeleteThree: () => number;
+export const DeleteFour: () => number;
+export const DeleteFive: () => number;
+export const QueryOne: () => number;
+export const QueryTwo: () => number;
+export const QueryThree: () => number;
+export const QueryFour: () => number;
+export const ExecuteOne: () => number;
+export const ExecuteTwo: () => number;
+export const ExecuteThree: () => number;
+export const ExecuteFour: () => number;
+export const ExecuteQueryOne: () => number;
+export const ExecuteQueryTwo: () => number;
+export const ExecuteQueryThree: () => number;
+export const ExecuteQueryFour: () => number;
+export const BeginTransactionOne: () => number;
+export const BeginTransactionTwo: () => number;
+export const RollBackOne: () => number;
+export const RollBackTwo: () => number;
+export const CommitOne: () => number;
+export const CommitTwo: () => number;
+export const BackupOne: () => number;
+export const BackupTwo: () => number;
+export const BackupThree: () => number;
+export const RestoreOne: () => number;
+export const RestoreTwo: () => number;
+export const RestoreThree: () => number;
+export const GetVersionOne: () => number;
+export const GetVersionTwo: () => number;
+export const GetVersionThree: () => number;
+export const SetVersionOne: () => number;
+export const SetVersionTwo: () => number;
+export const LockRowOne: () => number;
+export const UnLockRowOne: () => number;
+export const QueryLockRowOne: () => number;
+
+
