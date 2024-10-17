579c5fae6c3550e5e36bed2b8f5a67124f48f214
刘从来 <liuconglai@h-partners.com>
2024-10-17 06:39:19 +0000

    update arkui/ace_c_arkui_test_api13/entry/src/main/cpp/napi_func_init.cpp.
    Signed-off-by: 刘从来 <liuconglai@h-partners.com>

--- arkui/ace_c_arkui_test_api13/entry/src/main/cpp/napi_func_init.cpp
+++ arkui/ace_c_arkui_test_api13/entry/src/main/cpp/napi_func_init.cpp
@@ -12,8 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
- #include "common/common.h"
+#include "common/common.h"
 
 namespace ArkUICapiTest {
 EXTERN_C_START
