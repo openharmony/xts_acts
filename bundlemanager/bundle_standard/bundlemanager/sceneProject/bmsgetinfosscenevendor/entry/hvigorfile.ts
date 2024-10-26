3b41d41af8fb1e2478b645843140c95e1d01923c
yinqinglin <yinqinglin@huawei.com>
2024-10-26 11:13:15 +0800

    包管理子系统bmsgetinfosscenevendor hvigor适配
    Signed-off-by: yinqinglin <yinqinglin@huawei.com>

--- bundlemanager/bundle_standard/bundlemanager/sceneProject/bmsgetinfosscenevendor/entry/hvigorfile.ts
+++ bundlemanager/bundle_standard/bundlemanager/sceneProject/bmsgetinfosscenevendor/entry/hvigorfile.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,14 +12,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const injectRef = Object.getPrototypeOf(global) || global
-injectRef.regeneratorRuntime = require('@babel/runtime/regenerator')
-
-export default {
-    data: {
-        title: ""
-    },
-    onInit() {
-        this.title = this.$t('strings.world');
-    }
-}
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+export { legacyHapTasks } from '@ohos/hvigor-ohos-plugin';
