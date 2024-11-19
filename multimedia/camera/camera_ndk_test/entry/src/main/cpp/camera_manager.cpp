1e006707f20edd4f0a7f49f8b687bfb939432f81
13428608419 <zhihuangcheng@huawei.com>
2024-11-19 16:07:40 +0800

    XTS-Hvigor整改：camera_ndk_test
    Signed-off-by: 13428608419 <zhihuangcheng@huawei.com>

--- multimedia/camera/camera_ndk_test/entry/src/main/cpp/camera_manager.cpp
+++ multimedia/camera/camera_ndk_test/entry/src/main/cpp/camera_manager.cpp
@@ -15,11 +15,7 @@
 
 #include "camera_manager_ndk.h"
 #include <unistd.h>
-#include <ohcamera/camera_device.h>
-
-
-// #define LOG(fmt, ...)           \
-//         (void)printf(fmt, ##__VA_ARGS__);           
+#include <ohcamera/camera_device.h>   
 
 #define CAMERA_LOG_TAG "CAMERA_TAGLOG"
 #define CAMERA_LOG_DOMAIN 0x3200
