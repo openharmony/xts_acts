2476ebfc2b4bd5f0ecd6dc119f100b56b7669ca9
oh_ci <maguangsheng1@huawei.com>
2024-11-19 01:42:25 +0000

    回退 'Pull Request !19266 : 【startup】5.0.1代码同步'

--- startup/startup_standard/init_ndkTest/entry/src/main/cpp/initndk.cpp
+++ startup/startup_standard/init_ndkTest/entry/src/main/cpp/initndk.cpp
@@ -310,7 +310,7 @@ static napi_module demoModule = {
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
-    .nm_modname = "libinitndk",
+    .nm_modname = "init",
     .nm_priv = ((void *)0),
     .reserved = {0},
 };
