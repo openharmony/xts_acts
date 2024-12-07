e5fb1c6846c7ba68c899c4240698d44ff6c67161
zhengxiaoqing <zhaoxiaoqing17@h-partners.com>
2024-12-07 17:18:04 +0800

    add esimtest
    Signed-off-by: zhengxiaoqing <zhaoxiaoqing17@h-partners.com>

--- telephony/telephone/esimtest/hvigorw.bat
+++ telephony/telephone/esimtest/hvigorw.bat
@@ -1,5 +1,18 @@
+@rem Copyright (C) 2024 Huawei Device Co., Ltd.
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
 @rem
-@rem ----------------------------------------------------------------------------
+@rem     http://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@echo off
+@rem
+@rem  -------------------------------------------------------------------
 @rem  Hvigor startup script for Windows, version 1.0.0
 @rem
 @rem  Required ENV vars:
@@ -7,9 +20,8 @@
 @rem    NODE_HOME - location of a Node home dir
 @rem    or
 @rem    Add %NODE_HOME%/bin to the PATH environment variable
-@rem ----------------------------------------------------------------------------
+@rem  -------------------------------------------------------------------
 @rem
-@echo off
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
