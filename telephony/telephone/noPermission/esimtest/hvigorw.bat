f444188eddf0ae19f4b16a05353f28a3734dece8
zhengxiaoqing <zhaoxiaoqing17@h-partners.com>
2024-12-16 19:14:38 +0800

    add esimtest
    Signed-off-by: zhengxiaoqing <zhaoxiaoqing17@h-partners.com>

--- telephony/telephone/noPermission/esimtest/hvigorw.bat
+++ telephony/telephone/noPermission/esimtest/hvigorw.bat
@@ -23,14 +23,10 @@
 @rem  -------------------------------------------------------------------
 @rem
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
-
 set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
+set NODE_EXE_PATH=""
+set APP_HOME=.
 set WRAPPER_MODULE_PATH=%APP_HOME%\hvigor\hvigor-wrapper.js
 set NODE_EXE=node.exe
 @rem set NODE_OPTS="--max-old-space-size=4096"
@@ -42,25 +38,26 @@ if not defined NODE_OPTS set NODE_OPTS="--"
 
 @rem Find node.exe
 if defined NODE_HOME (
-  set NODE_HOME=%NODE_HOME:"=%
-  set NODE_EXE_PATH=%NODE_HOME%/%NODE_EXE%
+    set NODE_HOME=%NODE_HOME:"=%
+    set "PATH=%PATH%;%NODE_HOME%"
+    set NODE_EXE_PATH=%NODE_HOME%/%NODE_EXE%
 )
 
 %NODE_EXE% --version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" (
-  "%NODE_EXE%" "%NODE_OPTS%" "%WRAPPER_MODULE_PATH%" %*
+    "%NODE_EXE%" "%NODE_OPTS%" "%WRAPPER_MODULE_PATH%" %*
 ) else if exist "%NODE_EXE_PATH%" (
-  "%NODE_EXE%" "%NODE_OPTS%" "%WRAPPER_MODULE_PATH%" %*
+    "%NODE_EXE_PATH%" "%NODE_OPTS%" "%WRAPPER_MODULE_PATH%" %*
 ) else (
-  echo.
-  echo ERROR: NODE_HOME is not set and no 'node' command could be found in your PATH.
-  echo.
-  echo Please set the NODE_HOME variable in your environment to match the
-  echo location of your NodeJs installation.
+    echo.
+    echo ERROR: NODE_HOME is not set and no 'node' command could be found in your PATH.
+    echo.
+    echo Please set the NODE_HOME variable in your environment to match the
+    echo location of your NodeJs installation.
 )
 
-if "%ERRORLEVEL%" == "0" (
-  if "%OS%" == "Windows_NT" endlocal
+if "%ERRORLEVEL%" == "0"(
+    if "%OS%" == "Windows_NT" endlocal
 ) else (
-  exit /b %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
