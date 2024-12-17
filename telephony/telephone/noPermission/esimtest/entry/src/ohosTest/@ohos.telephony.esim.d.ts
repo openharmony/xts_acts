9002b94018783ac08bb8feedcfca290d59677767
zhengxiaoqing <zhaoxiaoqing17@h-partners.com>
2024-12-17 20:49:53 +0800

    add esimtest
    Signed-off-by: zhengxiaoqing <zhaoxiaoqing17@h-partners.com>

--- telephony/telephone/noPermission/esimtest/entry/src/ohosTest/@ohos.telephony.esim.d.ts
+++ telephony/telephone/noPermission/esimtest/entry/src/ohosTest/@ohos.telephony.esim.d.ts
@@ -533,7 +533,7 @@ declare namespace eSIM {
      iccid: string;
 
      /**
-      * The service provider name for the profile.
+      * The service-provider name for the profile.
       * 
       * @type { string }
       * @syscap SystemCapability.Telephony.CoreService.Esim
@@ -773,7 +773,7 @@ declare namespace eSIM {
     nickName: string;
 
     /**
-     * The service provider name.
+     * The service-provider name for the profile.
      * 
      * @type { string }
      * @syscap SystemCapability.Telephony.CoreService.Esim
@@ -1524,7 +1524,8 @@ declare namespace eSIM {
     forceDisableProfile: boolean;
 
     /**
-     * Specifies whether the user allows the service provider to enforce this Profile Policy Rule (PPR) after being informed of its restrictions.
+     * Specifies whether the user allows the service-provider to enforce this Profile Policy Rule (PPR)
+     * after being informed of its restrictions.
      *
      * @type { boolean }
      * @syscap SystemCapability.Telephony.CoreService.Esim
