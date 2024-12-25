79db0428b88ffcb544fab785cd3c98480adb6e99
zhengxiaoqing <zhaoxiaoqing17@h-partners.com>
2024-12-25 14:52:44 +0800

    修复esimtest及适配最新的接口文档
    Signed-off-by: zhengxiaoqing <zhaoxiaoqing17@h-partners.com>

--- telephony/telephone/esimtest/entry/hvigorfile.ts
+++ telephony/telephone/esimtest/entry/hvigorfile.ts
@@ -12,10 +12,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+import { getHvigorNode } from '@ohos/hvigor';
+// FA模型此处改为：
+// import { legacyHapTasks as hapTasks } from '@ohos/hvigor-ohos-plugin';
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import * as path from 'path';
+import { executeOnlineSign } from '../hw_sign/sign.js'
 
-export default {
-    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+const mModule = getHvigorNode(__filename);
+const ohosPlugin = hapTasks(mModule);
+
+const onlineSignHapTaskName = "onlineSignHap";
+let curTargetName = "default";
+const mModuleName = mModule.getName();
+const projectRootPath = process.cwd();
+
+// 若是feature模块签名，此处填写依赖的entry模块名称
+const entryName = '';
+
+ohosPlugin.getNeedExecTargetServiceList().forEach(targetServices => {
+
+    curTargetName = targetServices.getTargetData().getTargetName();
+
+    // 注册在线签名任务和创建任务依赖
+    const onlineSignTask = mModule.task(() => {
+        // 构建的未签名的hap的输出根目录
+        const moduleBuildOutputDir = path.resolve(projectRootPath, mModuleName, `build/default/outputs/${curTargetName }/`);
+
+        // 未签名的hap包路径
+        const inputFile = path.resolve(moduleBuildOutputDir, `${mModuleName}${entryName? '-' + entryName: ''}-${curTargetName }-unsigned.hap`);
+        // 签名后的hap包路径
+        const outputFile = path.resolve(moduleBuildOutputDir, `${mModuleName}${entryName? '-' + entryName: ''}-${curTargetName }-signed.hap`);
+
+        executeOnlineSign(inputFile, outputFile);
+
+        // FA模型此处改为：
+        // }, onlineSignHapTaskName).dependsOn(`${curTargetName}@LegacyPackageHap`);
+    }, onlineSignHapTaskName).dependsOn(`${curTargetName}@PackageHap`);
+
+    // 使用在线签名,可以把离线签名任务disable掉
+    if (onlineSignTask.getEnabled()) {
+        mModule.getTaskByName(`${curTargetName}@SignHap`).setEnabled(false);
+    }
+}) ;
+
+// 将在线签名任务挂接在assembleHap任务上,如果需要在IDE上使用,assembleHap任务是固定的
+mModule.getTaskByName("assembleHap").dependsOn(onlineSignHapTaskName);
+
+module.exports = {
+    ohos: ohosPlugin
 }
