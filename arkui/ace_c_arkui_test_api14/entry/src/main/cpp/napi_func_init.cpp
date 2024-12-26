0827bb90a2baecc2b7bd67d03b58e6e2e4a7141f
yiying <jiaoyiying3@h-partners.com>
2024-12-26 11:02:25 +0800

    xts接口覆盖:OH_NativeXComponent_RegisterKeyEventCallbackWithResult
    Signed-off-by: yiying <jiaoyiying3@h-partners.com>

--- arkui/ace_c_arkui_test_api14/entry/src/main/cpp/napi_func_init.cpp
+++ arkui/ace_c_arkui_test_api14/entry/src/main/cpp/napi_func_init.cpp
@@ -27,8 +27,6 @@ static napi_value Init(napi_env env, napi_value exports)
     napi_property_descriptor desc[] = {
         {"testNativeXComponent_RegisterKeyEventCallbackWithResult001", nullptr,
          testNativeXComponent_RegisterKeyEventCallbackWithResult001, nullptr, nullptr, nullptr, napi_default, nullptr},
-        {"tabStop_001", nullptr, tabStop_001, nullptr, nullptr, nullptr, napi_default,
-         nullptr},
     };
     if (napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc) != napi_ok) {
         OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "Init", "napi_define_properties failed");
