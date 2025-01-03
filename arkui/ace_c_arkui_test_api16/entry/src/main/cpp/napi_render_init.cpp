247c87e517de86ecd6512a37f1d5cb5124ab6d24
刘从来 <liuconglai@h-partners.com>
2025-01-03 06:11:17 +0000

    update arkui/ace_c_arkui_test_api16/entry/src/main/cpp/napi_render_init.cpp.
    Signed-off-by: 刘从来 <liuconglai@h-partners.com>

--- arkui/ace_c_arkui_test_api16/entry/src/main/cpp/napi_render_init.cpp
+++ arkui/ace_c_arkui_test_api16/entry/src/main/cpp/napi_render_init.cpp
@@ -30,7 +30,7 @@ static napi_value Init(napi_env env, napi_value exports)
     napi_property_descriptor desc[] = {
         {"getContext", nullptr, PluginManager::GetContext, nullptr, nullptr, nullptr, napi_default, nullptr},
         {"testPostFrameCallback", nullptr, PostFrameCallbackTest::testPostFrameCallback001,
-         nullptr, nullptr, nullptr, napi_default, nullptr}, 
+         nullptr, nullptr, nullptr, napi_default, nullptr},
     };
     if (napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc) != napi_ok) {
         OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "Init", "napi_define_properties failed");
