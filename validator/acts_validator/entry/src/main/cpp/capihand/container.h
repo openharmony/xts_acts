11176fd613e3cf402a69b54d19b84ab017c23351
l30033680 <344529497@qq.com>
2025-02-22 17:38:10 +0800

    Signed-off-by: l30033680 <344529497@qq.com>

---
+++ validator/acts_validator/entry/src/main/cpp/capihand/container.h
@@ -0,0 +1,46 @@
+//
+// Created on 2023/12/5.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef MyApplication_container_H
+#define MyApplication_container_H
+
+#include <string>
+#include <unordered_map>
+
+#include <ace/xcomponent/native_interface_xcomponent.h>
+#include <napi/native_api.h>
+
+const unsigned int LOG_PRINT_DOMAIN = 0xFF00;
+
+namespace NativeXComponentSample {
+
+    class Container {
+    public:
+        explicit Container(const std::string &id);
+        ~Container() = default;
+        static Container *GetInstance(const std::string &id);
+        static void Release(const std::string &id);
+        void Export(napi_env env, napi_value exports);
+        void OnSurfaceChanged(OH_NativeXComponent *component, void *window);
+        void OnTouchEvent(OH_NativeXComponent *component, void *window);
+        void OnMouseEvent(OH_NativeXComponent *component, void *window);
+        void OnHoverEvent(OH_NativeXComponent *component, bool isHover);
+        void OnKeyEvent(OH_NativeXComponent *component, void *window);
+        void RegisterCallback(OH_NativeXComponent *nativeXComponent);
+
+    public:
+        static std::unordered_map<std::string, Container *> instance_;
+        std::string id_;
+
+    private:
+        OH_NativeXComponent_Callback containerCallback_;
+        OH_NativeXComponent_MouseEvent_Callback mouseCallback_;
+    };
+
+} // namespace NativeXComponentSample
+
+
+#endif // MyApplication_container_H
---
+++ validator/acts_validator/entry/src/main/cpp/init.cpp
@@ -0,0 +1,43 @@
+//
+// Created on 2023/12/5.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#include <hilog/log.h>
+#include "capihand/manager.h"
+
+namespace NativeXComponentSample {
+    EXTERN_C_START
+    static napi_value Init(napi_env env, napi_value exports) {
+        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Init", "Init begins");
+        if ((env == nullptr) || (exports == nullptr)) {
+            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "Init", "env or exports is null");
+            return nullptr;
+        }
+
+        napi_property_descriptor desc[] = {
+            {"getContext", nullptr, Manager::GetContext, nullptr, nullptr, nullptr, napi_default, nullptr},
+            {"createNativeNode", nullptr, Manager::CreateNativeNode, nullptr, nullptr, nullptr, napi_default, nullptr},
+            {"upDateNativeNode", nullptr, Manager::UpdateNativeNode, nullptr, nullptr, nullptr, napi_default, nullptr}};
+
+        if (napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc) != napi_ok) {
+            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "Init", "napi_define_properties failed");
+            return nullptr;
+        }
+
+        Manager::GetInstance()->Export(env, exports);
+        return exports;
+    }
+    EXTERN_C_END
+
+    static napi_module nativeNodeModule = {.nm_version = 1,
+                                           .nm_flags = 0,
+                                           .nm_filename = nullptr,
+                                           .nm_register_func = Init,
+                                           .nm_modname = "nativeNode",
+                                           .nm_priv = ((void *)0),
+                                           .reserved = {0}};
+
+    extern "C" __attribute__((constructor)) void RegisterModule(void) { napi_module_register(&nativeNodeModule); }
+} // namespace NativeXComponentSample
