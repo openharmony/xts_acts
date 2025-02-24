c5e751860a1241d9f10e85c6a00f920e024c40e1
万胜 <wansheng11@h-partners.com>
2025-02-24 07:54:52 +0000

    update multimedia/audio/audio_js_standard/audio_ndk_test/entry/src/main/cpp/audioTest.cpp.
    Signed-off-by: 万胜 <wansheng11@h-partners.com>

--- multimedia/audio/audio_js_standard/audio_ndk_test/entry/src/main/cpp/audioTest.cpp
+++ multimedia/audio/audio_js_standard/audio_ndk_test/entry/src/main/cpp/audioTest.cpp
@@ -4876,8 +4876,6 @@ napi_property_descriptor desc1[] = {
         nullptr, nullptr, nullptr, napi_default, nullptr},
     {"audioRenderGetAudioTimestampInfoInterval", nullptr, AudioRenderGetAudioTimestampInfoInterval,
         nullptr, nullptr, nullptr, napi_default, nullptr},
-    {"audioRenderGetAudioSpeedTimestampInfo", nullptr, AudioRenderGetAudioSpeedTimestampInfo,
-        nullptr, nullptr, nullptr, napi_default, nullptr},
     {"audioRenderGetFrameSizeInCallback", nullptr, AudioRenderGetFrameSizeInCallback,
         nullptr, nullptr, nullptr, napi_default, nullptr},
     {"audioRenderGenerate", nullptr, AudioRenderGenerate, nullptr, nullptr, nullptr, napi_default, nullptr},
