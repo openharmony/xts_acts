2b0b2874ffa08a4fae5461b659b6cd7a5299a3d3
刘从来 <liuconglai@h-partners.com>
2025-03-11 12:03:40 +0000

    update node_util_move_test.cpp.
    Signed-off-by: 刘从来 <liuconglai@h-partners.com>

--- arkui/ace_c_arkui_test_api16/entry/src/main/cpp/move/node_util_move_test.cpp
+++ arkui/ace_c_arkui_test_api16/entry/src/main/cpp/move/node_util_move_test.cpp
@@ -21,14 +21,10 @@
 #include <arkui/native_node_napi.h>
 
 static ArkUI_NativeNodeAPI_1 *nodeAPI = nullptr;
-
-static int NUMBER_3 = 3;
-static int NUMBER_4 = 4;
-static int NUMBER_5 = 5;
-static int NUMBER_40 = 40;
-static int NUMBER_41 = 41;
-static int NUMBER_42 = 42;
-
+static int NUMBER_0 = 0;
+static int NUMBER_1 = 1;
+static int NUMBER_2 = 2;
+static int NUMBER_9 = 9;
 static ArkUI_NodeHandle column;
 static ArkUI_NodeHandle column1;
 static ArkUI_NodeHandle column2;
@@ -40,13 +36,12 @@ static ArkUI_NodeHandle text4;
 static ArkUI_NodeHandle stack;
 static ArkUI_NodeHandle bt0;
 static ArkUI_NodeHandle bt1;
-
 struct A {
-   ArkUI_NodeHandle child;
-   ArkUI_NodeHandle parent;
-   ArkUI_NodeHandle parent2;
-   int32_t index;
-   int32_t index2;
+    ArkUI_NodeHandle child;
+    ArkUI_NodeHandle parent;
+    ArkUI_NodeHandle parent2;
+    int32_t index;
+    int32_t index2;
 };
 
 namespace ArkUICapiTest {
@@ -65,31 +60,28 @@ static ArkUI_NodeHandle CreateChildNode(ArkUI_NativeNodeAPI_1 *nodeAPI)
     nodeAPI->setAttribute(column, NODE_WIDTH, &item);
     nodeAPI->setAttribute(column1, NODE_WIDTH, &item);
     nodeAPI->setAttribute(column2, NODE_WIDTH, &item);
-    value[0].f32 = 500;
-    nodeAPI->setAttribute(column, NODE_HEIGHT, &item);
-    value[0].f32 = 250;
-    nodeAPI->setAttribute(column1, NODE_HEIGHT, &item);
-    nodeAPI->setAttribute(column2, NODE_HEIGHT, &item);
-
-    value[0].u32 = 0xFF000000;  // 0xFF000000
-    nodeAPI->setAttribute(column1, NODE_BACKGROUND_COLOR, &item);
-    value[0].u32 = 0xFFCCCCCC;
-    nodeAPI->setAttribute(column2, NODE_BACKGROUND_COLOR, &item);
+    ArkUI_NumberValue value1[] = {480};
+    ArkUI_AttributeItem item1 = {value1, sizeof(value1) / sizeof(ArkUI_NumberValue)};
+    nodeAPI->setAttribute(column, NODE_HEIGHT, &item1);
+    ArkUI_NumberValue value2[] = {250};
+    ArkUI_AttributeItem item2 = {value2, sizeof(value2) / sizeof(ArkUI_NumberValue)};
+    nodeAPI->setAttribute(column1, NODE_HEIGHT, &item2);
+    nodeAPI->setAttribute(column2, NODE_HEIGHT, &item2);
+    
     ArkUI_NodeHandle row0 = nodeAPI->createNode(ARKUI_NODE_ROW);
-
     bt0 = nodeAPI->createNode(ARKUI_NODE_BUTTON);
     ArkUI_AttributeItem bt0_id = {.string = "bt0_id"};
     nodeAPI->setAttribute(bt0, NODE_ID, &bt0_id);
     ArkUI_AttributeItem bt0_item = {.string = "appendCol2"};
     nodeAPI->setAttribute(bt0, NODE_BUTTON_LABEL, &bt0_item);
-    nodeAPI->registerNodeEvent(bt0, NODE_ON_CLICK, 0, nullptr);
+    nodeAPI->registerNodeEvent(bt0, NODE_ON_CLICK, NUMBER_0, nullptr);
 
     bt1 = nodeAPI->createNode(ARKUI_NODE_BUTTON);
     ArkUI_AttributeItem bt1_id = {.string = "bt1_id"};
     nodeAPI->setAttribute(bt1, NODE_ID, &bt1_id);
     ArkUI_AttributeItem bt1_item = {.string = "error code"};
     nodeAPI->setAttribute(bt1, NODE_BUTTON_LABEL, &bt1_item);
-    nodeAPI->registerNodeEvent(bt1, NODE_ON_CLICK, 9, nullptr);
+    nodeAPI->registerNodeEvent(bt1, NODE_ON_CLICK, NUMBER_9, nullptr);
     
     nodeAPI->addChild(column, column1);
     nodeAPI->addChild(row0, bt0);
@@ -101,33 +93,39 @@ static ArkUI_NodeHandle CreateChildNode(ArkUI_NativeNodeAPI_1 *nodeAPI)
     text2 = nodeAPI->createNode(ARKUI_NODE_XCOMPONENT);
     text3 = nodeAPI->createNode(ARKUI_NODE_XCOMPONENT);
     text4 = nodeAPI->createNode(ARKUI_NODE_XCOMPONENT);
-    value[0].u32 = 0xFFFF00FF;  // red    
-    nodeAPI->setAttribute(text, NODE_BACKGROUND_COLOR, &item);
-    
+
+    ArkUI_NumberValue background_color_value1[] = {{.u32 = COLOR_RED}};
+    ArkUI_AttributeItem background_color_item1 = {background_color_value1,
+        sizeof(background_color_value1) / sizeof(ArkUI_NumberValue)};
+    nodeAPI->setAttribute(text, NODE_BACKGROUND_COLOR, &background_color_item1);
     ArkUI_AttributeItem itemStringStyle0 = {.string = "xc1"};
     nodeAPI->setAttribute(text, NODE_ID, &itemStringStyle0);
+    ArkUI_NumberValue background_color_value2[] = {{.u32 = COLOR_GREEN}};
+    ArkUI_AttributeItem background_color_item2 = {background_color_value2,
+        sizeof(background_color_value2) / sizeof(ArkUI_NumberValue)};
+    ArkUI_NumberValue background_color_value3[] = {{.u32 = COLOR_BLUE}};
+    ArkUI_AttributeItem background_color_item3 = {background_color_value3,
+        sizeof(background_color_value3) / sizeof(ArkUI_NumberValue)};
+    nodeAPI->setAttribute(text1, NODE_BACKGROUND_COLOR, &background_color_item2);
+    nodeAPI->setAttribute(text2, NODE_BACKGROUND_COLOR, &background_color_item3);
+    nodeAPI->setAttribute(text3, NODE_BACKGROUND_COLOR, &background_color_item2);
+    nodeAPI->setAttribute(text4, NODE_BACKGROUND_COLOR, &background_color_item3);
     
-    value[0].u32 = 0xFF00FF00;  // green    
-    nodeAPI->setAttribute(text1, NODE_BACKGROUND_COLOR, &item);
-    value[0].u32 = 0xFF00FFFF;  // blue    
-    nodeAPI->setAttribute(text2, NODE_BACKGROUND_COLOR, &item);
-    value[0].u32 = 0xFF00FF00;  // green    
-    nodeAPI->setAttribute(text3, NODE_BACKGROUND_COLOR, &item);
-    value[0].u32 = 0xFF00FFFF;  // blue    
-    nodeAPI->setAttribute(text4, NODE_BACKGROUND_COLOR, &item);
-    
-    value[0].f32 = 100;
-    nodeAPI->setAttribute(text, NODE_WIDTH, &item);
-    nodeAPI->setAttribute(text1, NODE_WIDTH, &item);
-    nodeAPI->setAttribute(text2, NODE_WIDTH, &item);
-    nodeAPI->setAttribute(text3, NODE_WIDTH, &item);
-    nodeAPI->setAttribute(text4, NODE_WIDTH, &item);
-    value[0].f32 = 30;
-    nodeAPI->setAttribute(text, NODE_HEIGHT, &item);
-    nodeAPI->setAttribute(text1, NODE_HEIGHT, &item);
-    nodeAPI->setAttribute(text2, NODE_HEIGHT, &item);
-    nodeAPI->setAttribute(text3, NODE_HEIGHT, &item);
-    nodeAPI->setAttribute(text4, NODE_HEIGHT, &item);
+    ArkUI_NumberValue value5[] = {100};
+    ArkUI_AttributeItem item5 = {value5, sizeof(value5) / sizeof(ArkUI_NumberValue)};
+    nodeAPI->setAttribute(text, NODE_WIDTH, &item5);
+    nodeAPI->setAttribute(text1, NODE_WIDTH, &item5);
+    nodeAPI->setAttribute(text2, NODE_WIDTH, &item5);
+    nodeAPI->setAttribute(text3, NODE_WIDTH, &item5);
+    nodeAPI->setAttribute(text4, NODE_WIDTH, &item5);
+
+    ArkUI_NumberValue value6[] = {30};
+    ArkUI_AttributeItem item6 = {value6, sizeof(value6) / sizeof(ArkUI_NumberValue)};
+    nodeAPI->setAttribute(text, NODE_HEIGHT, &item6);
+    nodeAPI->setAttribute(text1, NODE_HEIGHT, &item6);
+    nodeAPI->setAttribute(text2, NODE_HEIGHT, &item6);
+    nodeAPI->setAttribute(text3, NODE_HEIGHT, &item6);
+    nodeAPI->setAttribute(text4, NODE_HEIGHT, &item6);
             
     nodeAPI->addChild(column1, text);
     nodeAPI->addChild(column1, text1);
@@ -159,44 +157,41 @@ napi_value NodeUtilMoveTest::testNodeUtilMove001(napi_env env, napi_callback_inf
     a->child = text;
     a->parent = column2;
     a->parent2 = column1;
-    a->index = 0;
-    a->index2 = 2;
+    a->index = NUMBER_0;
+    a->index2 = NUMBER_2;
 
-    nodeAPI->registerNodeEvent(column1, NODE_ON_CLICK, 1, a);
-    nodeAPI->registerNodeEvent(column2, NODE_ON_CLICK, 2, a);
+    nodeAPI->registerNodeEvent(column1, NODE_ON_CLICK, NUMBER_1, a);
+    nodeAPI->registerNodeEvent(column2, NODE_ON_CLICK, NUMBER_2, a);
     
     nodeAPI->registerNodeEventReceiver([](ArkUI_NodeEvent *event) {
         auto targetId = OH_ArkUI_NodeEvent_GetTargetId(event);
         auto *nodeAPI = reinterpret_cast<ArkUI_NativeNodeAPI_1 *>(
             OH_ArkUI_QueryModuleInterfaceByName(ARKUI_NATIVE_NODE, "ArkUI_NativeNodeAPI_1"));
-        if (targetId == 0) {
+        if (targetId == NUMBER_0) {
             nodeAPI->addChild(column, column2);
         }
-        if (targetId == 1) {
+        if (targetId == NUMBER_1) {
             auto err = OH_ArkUI_NodeUtils_MoveTo(text, column2, 0);
             OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "kkk click1 = %{public}d", err);
             auto parent =  OH_ArkUI_NodeUtils_GetParentInPageTree(text);
             auto getParentId = nodeAPI->getAttribute(parent, NODE_ID)->string;
-            
             if (strcmp(getParentId, "column2_id") == 0) {
-                OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "Manager", "kkk666 getChild1 :%{public}s ", getParentId);
                 ArkUI_NumberValue background_color_value[] = {{.u32 = COLOR_GREEN}};
                 ArkUI_AttributeItem background_color_item = {background_color_value,
                     sizeof(background_color_value) / sizeof(ArkUI_NumberValue)};
                 nodeAPI->setAttribute(bt0, NODE_BACKGROUND_COLOR, &background_color_item);
             }
         }
-        if (targetId == 9) {
+        if (targetId == NUMBER_9) {
             auto err1 = OH_ArkUI_NodeUtils_MoveTo(nullptr, column1, 0);
             auto err2 = OH_ArkUI_NodeUtils_MoveTo(text, column2, 2);
             OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "kkk click2 = %{public}d", err1);
             OH_LOG_Print(LOG_APP, LOG_INFO, LOG_PRINT_DOMAIN, "Manager", "kkk click2 = %{public}d", err2);
-            
-            if (err1 == 401 && err2 == 0) {
+            if (err1 == ARKUI_ERROR_CODE_PARAM_INVALID && err2 == ARKUI_ERROR_CODE_NO_ERROR) {
                 ArkUI_NumberValue background_color_value[] = {{.u32 = COLOR_GREEN}};
                 ArkUI_AttributeItem background_color_item = {background_color_value,
                     sizeof(background_color_value) / sizeof(ArkUI_NumberValue)};
-                nodeAPI->setAttribute(bt1, NODE_BACKGROUND_COLOR, &background_color_item); 
+                nodeAPI->setAttribute(bt1, NODE_BACKGROUND_COLOR, &background_color_item);
             }
         }
         
