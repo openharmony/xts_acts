17af48406ad94b5d930f9f81abb610ed918f9ae1
kangqiao <kangqiao1@huawei.com>
2025-03-11 10:16:21 +0800

    add testcases for failed to connect service
    Signed-off-by: kangqiao <kangqiao1@huawei.com>

---
+++ powermgr/battery_manager/entry/src/ohosTest/js/test/BatteryService.test.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import batteryInfo from '@ohos.batteryInfo';
+import { describe, beforeAll, beforeEach, afterEach, afterAll, it, expect, TestType, Size, Level } from '@ohos/hypium';
+
+export default function BatteryServiceTest() {
+  describe('BatteryServiceTest', () => {
+    // Defines a test suite. Two parameters are supported: test suite name and test suite function.
+    beforeAll(() => {
+      // Presets an action, which is performed only once before all test cases of the test suite start.
+      // This API supports only one parameter: preset action function.
+      // hdc shell pkill powermgr
+    })
+    beforeEach(() => {
+      // Presets an action, which is performed before each unit test case starts.
+      // The number of execution times is the same as the number of test cases defined by **it**.
+      // This API supports only one parameter: preset action function.
+    })
+    afterEach(() => {
+      // Presets a clear action, which is performed after each unit test case ends.
+      // The number of execution times is the same as the number of test cases defined by **it**.
+      // This API supports only one parameter: clear action function.
+    })
+    afterAll(() => {
+      // Presets a clear action, which is performed after all test cases of the test suite end.
+      // This API supports only one parameter: clear action function.
+    })
+
+    /**
+     * @tc.number SUB_PowerSystem_BatteryService_JSTest_0010
+     * @tc.name testBatteryServiceTest_0100
+     * @tc.desc BatteryService stop test
+     * @tc.level: Level 3
+     * @tc.type: Function
+     * @tc.size: MediumTest
+     */
+    it('BatteryServiceTest_0100', TestType.FUNCTION | Size.MEDIUMTEST | Level.LEVEL3, async (done) => {
+      let TAG = 'BatteryServiceTest_0100';
+      try {
+        let ret = batteryInfo.setBatteryConfig('a', 'b');
+        console.info(`${TAG} ret: ${ret}`);
+      } catch (error) {
+        console.error(`${TAG} error: ${JSON.stringify(error)}`);
+        expect(error.code).assertEqual(4600101);
+        done();
+      }
+    });
+
+    /**
+     * @tc.number SUB_PowerSystem_BatteryService_JSTest_0020
+     * @tc.name testBatteryServiceTest_0200
+     * @tc.desc BatteryService stop test
+     * @tc.level: Level 3
+     * @tc.type: Function
+     * @tc.size: MediumTest
+     */
+    it('BatteryServiceTest_0200', TestType.FUNCTION | Size.MEDIUMTEST | Level.LEVEL3, async (done) => {
+      let TAG = 'BatteryServiceTest_0200';
+      try {
+        let ret = batteryInfo.getBatteryConfig('a');
+        console.info(`${TAG} ret: ${ret}`);
+      } catch (error) {
+        console.error(`${TAG} error: ${JSON.stringify(error)}`);
+        expect(error.code).assertEqual(4600101);
+        done();
+      }
+    });
+
+    /**
+     * @tc.number SUB_PowerSystem_BatteryService_JSTest_0030
+     * @tc.name testBatteryServiceTest_0300
+     * @tc.desc BatteryService stop test
+     * @tc.level: Level 3
+     * @tc.type: Function
+     * @tc.size: MediumTest
+     */
+    it('BatteryServiceTest_0300', TestType.FUNCTION | Size.MEDIUMTEST | Level.LEVEL3, async (done) => {
+      let TAG = 'BatteryServiceTest_0300';
+      try {
+        let ret = batteryInfo.isBatteryConfigSupported('a');
+        console.info(`${TAG} ret: ${ret}`);
+      } catch (error) {
+        console.error(`${TAG} error: ${JSON.stringify(error)}`);
+        expect(error.code).assertEqual(4600101);
+        done();
+      }
+    });
+  });
+}
