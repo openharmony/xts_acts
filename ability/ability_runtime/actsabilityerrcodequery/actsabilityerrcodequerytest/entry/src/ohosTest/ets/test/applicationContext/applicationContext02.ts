edbad9bb7a53a41ce93afd28123ddfe58be20eff
dongwei <dongwei@kaihong.com>
2025-03-26 18:15:49 +0800

    新建目录 修改applicationCtontext用例
    Signed-off-by: dongwei <dongwei@kaihong.com>

--- ability/ability_runtime/actsabilityerrcodequery/actsabilityerrcodequerytest/entry/src/ohosTest/ets/test/applicationContext/applicationContext02.ts
+++ ability/ability_runtime/actsabilityerrcodequery/actsabilityerrcodequerytest/entry/src/ohosTest/ets/test/applicationContext/applicationContext02.ts
@@ -16,9 +16,10 @@
 import { UIAbility, Want } from '@kit.AbilityKit';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
-import { commonEventManager } from '@kit.BasicServicesKit';
+import { BusinessError, commonEventManager } from '@kit.BasicServicesKit';
 
 export default class applicationContext02 extends UIAbility {
+
   onCreate(want) {
     console.info('applicationContext02 onCreate');
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
@@ -26,6 +27,7 @@ export default class applicationContext02 extends UIAbility {
     console.info('applicationContext02 JSON.stringify(want) is', JSON.stringify(want));
     console.info('applicationContext02 JSON.stringify(want) is', JSON.stringify(want.parameters));
     let applicationContext = globalThis.applicationContext02.getApplicationContext();
+    globalThis.applicationContext = applicationContext
   };
 
   onDestroy() {
@@ -43,9 +45,6 @@ export default class applicationContext02 extends UIAbility {
       }
       hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
     });
-    commonEventManager.publish('ACTS_TEST_DESTROY', function () {
-      console.info('====>SUB_ChildProcessManager_sendableContextManager_0800 publish ACTS_TEST_DESTROY');
-    });
   }
 
   onWindowStageDestroy() {
@@ -55,12 +54,48 @@ export default class applicationContext02 extends UIAbility {
 
   onForeground() {
     // Ability has brought to foreground
+    let want1: Want = {
+      bundleName:"ohos.acts.applicationContextAssist",
+      abilityName:"EntryAbility",
+      parameters: {
+        appLinkingOnly: false,
+        demo_num: 111
+      }
+    }
     console.info('applicationContext02 onForeground');
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+    globalThis.applicationContext02.startAbility(want1, (err: BusinessError) => {
+      if (err.code) {
+        console.error(`Failed to startAbility. Code: ${err.code}, message: ${err.message}`);
+      }
+      console.info('context.startAbility applicationContext03 succeed');
+    })
   }
 
   onBackground() {
     console.info('applicationContext02 onBackground');
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+    let want: Want = {
+      bundleName:"ohos.example.myapplication",
+      abilityName:"applicationContext02",
+      parameters: {
+        appLinkingOnly: false,
+        demo_num: 111
+      }
+    }
+    setTimeout(() => {
+      try {
+        hilog.info(0x0000, 'testTag', '%{public}s', 'globalThis.applicationContext.restartApp begin');
+        globalThis.applicationContext.restartApp(want);
+        hilog.info(0x0000, 'testTag', '%{public}s', 'globalThis.applicationContext.restartApp succeed,case failed');
+      } catch (error) {
+        console.log(`applicationContext.restartApp fail, error: ${JSON.stringify(error)}`);
+        if(error.code==16000053){
+          commonEventManager.publish('ACTS_TEST_DESTROY', function () {
+            console.info('====>SUB_ChildProcessManager_sendableContextManager_0800 publish ACTS_TEST_DESTROY');
+          });
+        }
+      }
+    }, 500)
   }
 }
