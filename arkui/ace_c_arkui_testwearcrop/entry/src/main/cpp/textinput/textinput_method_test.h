3f9d6d708733085b5ba8038a5a53a594eab4f855
张伟伟 <zhangweiwei87@h-partners.com>
2025-04-01 14:47:09 +0800

    XTS适配输入法
    Signed-off-by: 张伟伟 <zhangweiwei87@h-partners.com>

--- arkui/ace_c_arkui_testwearcrop/entry/src/main/cpp/textinput/textinput_method_test.h
+++ arkui/ace_c_arkui_testwearcrop/entry/src/main/cpp/textinput/textinput_method_test.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,17 +13,18 @@
  * limitations under the License.
  */
 
+#ifndef ARKUI_CAPI_XTS_TEXTINPUT_METHOD_TEST_H
+#define ARKUI_CAPI_XTS_TEXTINPUT_METHOD_TEST_H
 
+#include "../common/common.h"
+#include <ace/xcomponent/native_interface_xcomponent.h>
 
-export const calendarPickerEnabledTest: (a: string) => void;
+namespace ArkUICapiTest {
 
-
-export const calendarPickerOnChangeTest: (a: string) => void;
-
-export const textAreaOnPasteTest: (a: string) => void;
-
-export const textInputOnCutTest: (a: string) => void;
-
-export const textInputOnPasteTest: (a: string) => void;
-
-export const InputMethodTest001: (a: string) => void;
+class TextInputMethodTest {
+public:
+    ~TextInputMethodTest();
+    static napi_value InputMethodTest001(napi_env env, napi_callback_info info);
+};
+} // namespace ArkUICapiTest
+#endif // ARKUI_CAPI_XTS_TEXTINPUT_METHOD_TEST_H
