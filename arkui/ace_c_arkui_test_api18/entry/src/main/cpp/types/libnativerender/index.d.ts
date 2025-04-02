69cfd51df72e26e4072ee6f06eec50475eb45cd2
zhanghang <zhanghang160@huawei-partners.com>
2025-04-02 09:54:19 +0800

    fix:SwiperIndicator XTS Add
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>

--- arkui/ace_c_arkui_test_api18/entry/src/main/cpp/types/libnativerender/index.d.ts
+++ arkui/ace_c_arkui_test_api18/entry/src/main/cpp/types/libnativerender/index.d.ts
@@ -18,3 +18,5 @@ export const testDatePickerMode: (a: string) => void;
 export const textPickerEnableHapticFeedback: (a: string) => void;
 
 export const datePickerEnableHapticFeedback: (a: string) => void;
+
+export const swiperIndicatorTest: (a: string) => void;
