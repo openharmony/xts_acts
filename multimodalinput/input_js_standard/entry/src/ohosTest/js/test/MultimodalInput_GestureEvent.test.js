bcc0c7c443db21c74ff043ebc51163b79ada072b
kangqiao <kangqiao1@huawei.com>
2025-04-14 11:43:42 +0800

    update
    Signed-off-by: kangqiao <kangqiao1@huawei.com>

--- multimodalinput/input_js_standard/entry/src/ohosTest/js/test/MultimodalInput_GestureEvent.test.js
+++ multimodalinput/input_js_standard/entry/src/ohosTest/js/test/MultimodalInput_GestureEvent.test.js
@@ -74,48 +74,6 @@ export default function MultimodalInput_GestureEvent_test() {
     })
 
     /**
-     * @tc.number Multimodalinput_GestureEventTest_005
-     * @tc.name Multimodalinput_GestureEvent_test_SUB_MMI_GestureEvent_005
-     * @tc.desc GestureEvent interface On monitor pinch
-     */
-    it('Multimodalinput_GestureEvent_test_SUB_MMI_GestureEvent_005', 0, function () {
-      let label = '02800/MultimodalinputJs';
-      console.info(`${label}:Multimodalinput_GestureEvent_test_SUB_MMI_GestureEvent_005 start`);
-      console.info(`${label}:onClickMonitorOn callback`);
-      const handleEvent = (err, pinch) => {
-        pinch.type = 1;
-        pinch.scale = 1;
-        expect(pinch.type == gestureEvent.ActionType.BEGIN).assertTrue();
-        expect(pinch.scale == 1).assertTrue();
-        console.info(`${label}:callbackPinch start`);
-        if (err) {
-          console.info(`${label}:callbackPinch error:${JSON.stringify(err)}`);
-          expect(false).assertTrue();
-          return;
-        }
-        console.info(`${label}:callbackPinch success: ${JSON.stringify(pinch)}`);
-        let ret = Object.keys(pinch);
-        expect(ret.length > 0).assertTure();
-      };
-
-      console.info(`${label}:onClickMonitorOn start`);
-      try {
-        inputMonitor.on("pinch", handleEvent);
-      } catch (error) {
-        expect(true).assertTrue();
-      }
-      console.info(`${label}:onClickMonitorOn end`);
-
-      console.info(`${label}:onClickMonitorOff start`);
-      try {
-        inputMonitor.off("pinch", handleEvent);
-      } catch (error) {
-        expect(false).assertTrue();
-      }
-      console.info(`${label}:Multimodalinput_GestureEvent_test_SUB_MMI_GestureEvent_005 end`);
-    })
-
-    /**
      * @tc.number Multimodalinput_GestureEventTest_006
      * @tc.name Multimodalinput_GestureEvent_test_SUB_MMI_GestureEvent_006
      * @tc.desc GestureEvent interface On gesture pinch
