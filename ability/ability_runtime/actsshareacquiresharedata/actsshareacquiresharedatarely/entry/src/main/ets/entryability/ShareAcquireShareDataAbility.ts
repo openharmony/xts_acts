bf025693f65ad01411e13308d45de53dc6748ec1
jinzhao <jinzhao@kaihong.com>
2025-04-17 14:23:52 +0800

    codecheck
    Signed-off-by: jinzhao <jinzhao@kaihong.com>

--- ability/ability_runtime/actsshareacquiresharedata/actsshareacquiresharedatarely/entry/src/main/ets/entryability/ShareAcquireShareDataAbility.ts
+++ ability/ability_runtime/actsshareacquiresharedata/actsshareacquiresharedatarely/entry/src/main/ets/entryability/ShareAcquireShareDataAbility.ts
@@ -17,15 +17,15 @@ import { hilog } from '@kit.PerformanceAnalysisKit';
 import { window } from '@kit.ArkUI';
 
 export default class ShareAcquireShareDataAbility extends UIAbility {
-  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam): void {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
 
-  onDestroy() {
+  onDestroy(): void {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
-  onWindowStageCreate(windowStage: window.WindowStage) {
+  onWindowStageCreate(windowStage: window.WindowStage): void {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
@@ -38,17 +38,17 @@ export default class ShareAcquireShareDataAbility extends UIAbility {
     });
   }
 
-  onWindowStageDestroy() {
+  onWindowStageDestroy(): void {
     // Main window is destroyed, release UI related resources
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
   }
 
-  onForeground() {
+  onForeground(): void {
     // Ability has brought to foreground
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
   }
 
-  onBackground() {
+  onBackground(): void {
     // Ability has back to background
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
   }
@@ -56,6 +56,6 @@ export default class ShareAcquireShareDataAbility extends UIAbility {
   onShare(wantParam: Record<string, Object>): void {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onShare');
     let TAG = 'SUB_Ability_AbilityRuntime_AppManager_ShareAcquireShareData_3100_Ability';
-    hilog.info(0x0000, TAG, "[%{public}s] Ability onShare", TAG);
+    hilog.info(0x0000, TAG, '[%{public}s] Ability onShare', TAG);
   }
 }
