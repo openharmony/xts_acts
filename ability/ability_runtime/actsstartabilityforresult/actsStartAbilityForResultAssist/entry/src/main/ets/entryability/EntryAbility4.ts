42681ff604a2c0eed7a77e66e1484da3bcd43653
dongwei <dongwei@kaihong.com>
2025-04-17 17:54:18 +0800

    actsstartabilityforresult 测试用例代码提交
    Signed-off-by: dongwei <dongwei@kaihong.com>

---
+++ ability/ability_runtime/actsstartabilityforresult/actsStartAbilityForResultAssist/entry/src/main/ets/entryability/EntryAbility4.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { AbilityConstant, UIAbility, Want } from '@kit.AbilityKit';
+import { hilog } from '@kit.PerformanceAnalysisKit';
+import { window } from '@kit.ArkUI';
+import app, { AppResponse } from '@system.app'
+import { common } from '@kit.AbilityKit';
+import { BusinessError, commonEventManager } from '@kit.BasicServicesKit';
+import { router } from '@kit.ArkUI';
+
+let backSet:number = 0
+
+export default class EntryAbility4 extends UIAbility {
+  onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 onCreate');
+    globalThis.abilityAssist4 = this.context
+  }
+
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 onDestroy');
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 onWindowStageCreate');
+    windowStage.loadContent('pages/Index', (err, data) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return;
+      }
+      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 onWindowStageDestroy');
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 onForeground');
+    let want: Want = {
+      bundleName: 'com.example.mytest1',
+      abilityName: 'startAbilityForResult03'
+    };
+    if(backSet==0){
+      try {
+        globalThis.abilityAssist4.startAbilityForResult(want, (err: BusinessError, result: common.AbilityResult) => {
+          if (err.code) {
+            // 处理业务逻辑错误
+            hilog.info(0x0000, 'testTag', '%{public}s', `startAbilityForResult failed, error.code: ${err.code}, error.message: ${err.message}`);
+            return;
+          }
+          // 执行正常业务
+          hilog.info(0x0000, 'testTag', '%{public}s', 'startAbilityForResult success!');
+        });
+      } catch (err) {
+        // 处理入参错误异常
+        let code = (err as BusinessError).code;
+        let message = (err as BusinessError).message;
+        hilog.info(0x0000, 'testTag', '%{public}s', `startAbilityForResult failed, error.code: ${code}, error.message: ${message}`);
+      }
+    }else if(backSet==1){
+      hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 execute router.back!');
+      try{
+        router.back();
+        hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 router.back success!');
+      } catch (err){
+        let code = (err as BusinessError).code;
+        let message = (err as BusinessError).message;
+        hilog.info(0x0000, 'testTag', '%{public}s', `EntryAbility4 router.back failed, error.code: ${code}, error.message: ${message}`);
+      }
+    }
+    backSet++
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'EntryAbility4 onBackground');
+  }
+}
---
+++ ability/ability_runtime/actsstartabilityforresult/actsstartabilityforresulttest/entry/src/ohosTest/ets/test/startAbilityForResult/ability02.ts
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIAbility from '@ohos.app.ability.UIAbility';
+import hilog from '@ohos.hilog';
+import window from '@ohos.window';
+import { common } from '@kit.AbilityKit';
+import { BusinessError, commonEventManager } from '@kit.BasicServicesKit';
+import { Want } from '@kit.AbilityKit';
+
+let backSet:number = 0
+
+export default class UiAbility02 extends UIAbility {
+  onCreate(want, launchParam) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'UiAbility02 onCreate');
+    globalThis.uiAbilityContext2 = this.context
+    hilog.info(0x0000, 'testTag', '%{public}s', `UiAbility02 JSON.stringify(want) is: ${JSON.stringify(want)}`);
+  }
+
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'UiAbility02 onDestroy');
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'UiAbility02 onWindowStageCreate');
+
+    windowStage.loadContent('testability/pages/Index', (err, data) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return;
+      }
+      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+  }
+
+  onWindowStageDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'UiAbility02 onWindowStageDestroy');
+  }
+
+  onForeground() {
+    let want: Want = {
+      bundleName: 'com.acts.actsstartandterminateassisttest02',
+      abilityName: 'EntryAbility4'
+    };
+    hilog.info(0x0000, 'testTag', '%{public}s', 'UiAbility02 onForeground');
+    hilog.info(0x0000, 'testTag', '%{public}s', `UiAbility02 backSet is: ${backSet}`);
+    if(backSet==0){
+      backSet++;
+      hilog.info(0x0000, 'testTag', '%{public}s', `UiAbility02 backSet.. is: ${backSet}`);
+      try {
+        globalThis.uiAbilityContext2.startAbilityForResult(want, (err: BusinessError, result: common.AbilityResult) => {
+          if (err.code) {
+            // 处理业务逻辑错误
+            hilog.info(0x0000, 'testTag', '%{public}s', `startAbilityForResult failed, error.code: ${err.code}, error.message: ${err.message}`);
+            return;
+          }
+          // 执行正常业务
+          hilog.info(0x0000, 'testTag', '%{public}s', 'startAbilityForResult success UiAbility02!');
+
+        });
+      } catch (err) {
+        // 处理入参错误异常
+        let code = (err as BusinessError).code;
+        let message = (err as BusinessError).message;
+        hilog.info(0x0000, 'testTag', '%{public}s', `startAbilityForResult failed, error.code: ${code}, error.message: ${message}`);
+      }
+    } else if(backSet==1){
+      commonEventManager.publish('ACTS_TEST_DESTROY', function () {
+        hilog.info(0x0000, 'testTag', '%{public}s', 'commonEventManager UiAbility02 publish ACTS_TEST_DESTROY');
+      });
+    }
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'UiAbility02 onBackground');
+  }
+}
