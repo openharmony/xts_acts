a4f78c98a1018fc6d724fb7619f2a48656aa6c40
fuwei <fuwei45@huawei.com>
2025-04-25 06:19:59 +0000

    add new testcase
    Signed-off-by: fuwei <fuwei45@huawei.com>

--- storage/storagenopermissionjstest/entry/src/ohosTest/js/test/nopermission.test.js
+++ storage/storagenopermissionjstest/entry/src/ohosTest/js/test/nopermission.test.js
@@ -146,5 +146,47 @@ export default function File_Nopermission_test() {
         done();
       }
   });
+
+  /**
+   * @tc.number SUB_BASIC_FM_FileAPI_FileShare_NoPermission_0400
+   * @tc.name FileShare_NoPermission_004
+   * @tc.desc Test activatePermission() interfaces
+   * Test normal uri and OperationMode is READ_MODE.
+   * @tc.size MEDIUM
+   * @tc.type Function
+   * @tc.level Level 0
+   * @tc.require
+   */
+  it('FileShare_NoPermission_004', Level.LEVEL0, async function (done) {
+    let fpath = await nextFileName('FileShare_NoPermission_004');
+    expect(prepareFile(fpath, FILE_CONTENT)).assertTrue();
+
+    try {
+    let uriObject = new fileuri.FileUri(fpath);
+    let uri = uriObject.toString();
+    let fd = await fs.open(uri,fs.OpenMode.CREATE);
+    await fs.close(fd);
+    let policyInfo = {
+        uri: uri, 
+        operationMode: fileShare.OperationMode.READ_MODE,
+      };
+      let policies = [policyInfo];
+      fileShare.activatePermission(policies).then(() => {
+        console.info("FileShare_NoPermission_004 successfully");
+        expect(true).assertTrue();
+        done();
+      }).catch((err) => {
+        console.info("FileShare_NoPermission_004 failed with error message: " + err.message + ", error code: " + err.code);
+        expect(err.code == 201 && err.message == "Permission verification failed").assertTrue();
+        done();
+      });
+    } catch (e) {
+      console.info('FileShare_NoPermission_004 has failed for ' + e.message + ", err code: " + e.code);
+      if(e.code == 801){
+        expect(e.message == "The device doesn't support this api").assertTrue();
+      }
+      done();
+    }
+  });
   })
 }
