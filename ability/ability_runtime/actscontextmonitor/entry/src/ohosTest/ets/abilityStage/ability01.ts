15a63120c47d85a9bcd4b04e2eadee18590563d6
dongwei <dongwei@kaihong.com>
2025-05-08 16:41:43 +0800

    断言调试 漏修改部分修改
    Signed-off-by: dongwei <dongwei@kaihong.com>

--- ability/ability_runtime/actscontextmonitor/entry/src/ohosTest/ets/abilityStage/ability01.ts
+++ ability/ability_runtime/actscontextmonitor/entry/src/ohosTest/ets/abilityStage/ability01.ts
@@ -30,7 +30,7 @@ export default class AbilityStage01 extends AbilityStage {
     };
     if (argOne === 1 && argTwo === 2) {
       setTimeout(()=>{
-        commonEventManager.publish('ACTS_TEST_DESTROY', function () {
+        commonEventManager.publish('ACTS_TEST_DESTROY', commonEventData, function () {
           hilog.info(0x0000, 'testTag', '%{public}s', 'abilityStage01 entry publish ACTS_TEST_DESTROY');
         });
       }, 2000);
