9a48459fe4176e47cde853952362b499ee021949
zengjingjing <zengjingjing3@h-partners.com>
2025-05-09 18:43:50 +0800

    优化部分L0设备测试用例报告不完整的问题
    Signed-off-by: zengjingjing <zengjingjing3@h-partners.com>

--- validator_lite/acts_validator/entry/src/main/js/MainAbility/pages/event/longpress/index.js
+++ validator_lite/acts_validator/entry/src/main/js/MainAbility/pages/event/longpress/index.js
@@ -14,11 +14,11 @@
  */
 
 import { commonFunc } from '../../../common/js/event';
-import { saveTxtData } from '../../../common/js/saveData';
 import router from '@system.router';
 
 export default {
   data: {
+    itemIndex: NaN,
     color: '#5265ef',
     flag: false,
     pointX: 0,
@@ -48,10 +48,16 @@ export default {
 
   back() {
     console.info('onclick back ');
-    router.replace({ uri: 'pages/second-compent/index' });
+    router.replace({
+      uri: 'pages/second-compent/index',
+      params: {
+        itemIndex: this.itemIndex
+      }
+    });
   },
 
   changeResult(result) {
-    saveTxtData(this, result);
+    getApp().data.keyList[this.title] = result;
+    this.back();
   },
 };
--- validator_lite/acts_validator/entry/src/main/js/MainAbility/pages/image-animator/04/index.js
+++ validator_lite/acts_validator/entry/src/main/js/MainAbility/pages/image-animator/04/index.js
@@ -14,11 +14,11 @@
  */
 
 import { imageFrames as frames } from '../../../common/js/general';
-import { saveTxtData } from '../../../common/js/saveData';
 import router from '@system.router';
 
 export default {
   data: {
+    itemIndex: NaN,
     frames,
     state: null,
     str: '',
@@ -68,10 +68,16 @@ export default {
 
   back() {
     console.info('onclick back ');
-    router.replace({ uri: 'pages/second-compent/index' });
+    router.replace({
+      uri: 'pages/second-compent/index',
+      params: {
+        itemIndex: this.itemIndex
+      }
+    });
   },
 
   changeResult(result) {
-    saveTxtData(this, result);
+    getApp().data.keyList[this.title] = result;
+    this.back();
   },
 };
