a7954729379539e67122d406466e4a2ba1bcf64f
revin-sun <sunren1@huawei.com>
2025-05-22 19:35:27 +0800

    新增sdkMinorApiVersion和sdkPatchApiVersion两个接口xts
    Signed-off-by: revin-sun <sunren1@huawei.com>

--- startup/startup_standard/deviceinfo/entry/src/ohosTest/js/test/DeviceInfoJsUnit.test.js
+++ startup/startup_standard/deviceinfo/entry/src/ohosTest/js/test/DeviceInfoJsUnit.test.js
@@ -2033,5 +2033,75 @@ export default function DeviceInfoTest() {
             expect(deviceTypes).assertEqual('car');
             console.info('testGetDeviceTypes : end');
         })
+        
+        /**
+         * @tc.number    SUB_STARTUP_JS_DEVCEINFO_080
+         * @tc.name      testGetSdkMinorApiVersion01
+         * @tc.desc      Get the Minor API version number, which return value type is number and return value is greater than or equal to 0.
+         * @tc.size      : MediumTest
+         * @tc.type      : Function
+         * @tc.level     : Level 0
+         */
+        it('device_info_test_080', TestType.FUNCTION | Size.MEDIUMTEST | Level.LEVEL0, async function (done) {
+            console.info('testGetSdkMinorApiVersion01 start');
+            let ret = false;
+            let sdkMinorApiVersionInfo = deviceinfo.sdkMinorApiVersion;
+            console.info('the value of the deviceinfo sdkMinorApiVersion is :' + sdkMinorApiVersionInfo);
+
+            expect(sdkMinorApiVersionInfo).assertInstanceOf('Number');
+            if (sdkMinorApiVersionInfo >= 0) {
+                ret = true;
+                expect(ret).assertTrue();
+                done();
+            } else {
+                console.info('the return value sdkMinorApiVersion is not greater than 0.');
+                expect(false).assertTrue();
+                done();
+            }
+
+            let str = /[\w-\.\(\)]/g;
+            let arr = sdkMinorApiVersionInfo.toString().match(str);
+            let substr = arr.join('');
+            let numstr = parseInt(substr);
+            console.info('the value of the bufferstr is :' + numstr);
+            expect(sdkMinorApiVersionInfo).assertEqual(numstr);
+
+            console.info('testGetSdkMinorApiVersion01 ：end');
+        })
+
+        /**
+         * @tc.number    SUB_STARTUP_JS_DEVCEINFO_081
+         * @tc.name      testGetSdkPatchApiVersion01
+         * @tc.desc      Get the Patch API version number, which return value type is number and return value is greater than or equal to 0.
+         * @tc.size      : MediumTest
+         * @tc.type      : Function
+         * @tc.level     : Level 0
+         */
+        it('device_info_test_081', TestType.FUNCTION | Size.MEDIUMTEST | Level.LEVEL0, async function (done) {
+            console.info('testGetSdkPatchApiVersion01 start');
+            let ret = false;
+            let sdkPatchApiVersionInfo = deviceinfo.sdkPatchApiVersion;
+            console.info('the value of the deviceinfo sdkPatchApiVersion is :' + sdkPatchApiVersionInfo);
+
+            expect(sdkPatchApiVersionInfo).assertInstanceOf('Number');
+            if (sdkPatchApiVersionInfo >= 0) {
+                ret = true;
+                expect(ret).assertTrue();
+                done();
+            } else {
+                console.info('the return value sdkPatchApiVersion is not greater than 0.');
+                expect(false).assertTrue();
+                done();
+            }
+
+            let str = /[\w-\.\(\)]/g;
+            let arr = sdkPatchApiVersionInfo.toString().match(str);
+            let substr = arr.join('');
+            let numstr = parseInt(substr);
+            console.info('the value of the bufferstr is :' + numstr);
+            expect(sdkPatchApiVersionInfo).assertEqual(numstr);
+
+            console.info('testGetSdkPatchApiVersion01 ：end');
+        })
     })
 }
