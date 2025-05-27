0976e61f7b2828360e43d992b4e461fd94683924
hezhiyong15 <hezhiyong15@h-partners.com>
2025-05-27 14:22:20 +0800

    区域解码NDK
    Signed-off-by: hezhiyong15 <hezhiyong15@h-partners.com>

--- multimedia/image/image_ets_standard/imageNdk/entry/src/main/cpp/types/libentry/index.d.ts
+++ multimedia/image/image_ets_standard/imageNdk/entry/src/main/cpp/types/libentry/index.d.ts
@@ -108,3 +108,5 @@ export const GetMimeTypeWithNull: () => number;
 export const GetImagePropertyWithNull: (fd:number) => number;
 export const testCropAndScaleStrategy: (fd:number, desiredWidth:number, desiredHeight:number, regionX:number, regionY:number, 
                                         regionWidth:number, regionHight:number, imageWidth:number, imageHeight:number) => number;
+export const testCropAndScaleStrategyErr: (fd:number, desiredWidth:number, desiredHeight:number, regionX:number, regionY:number, 
+                                           regionWidth:number, regionHight:number, cropAndScaleStrategy:number) => number;
