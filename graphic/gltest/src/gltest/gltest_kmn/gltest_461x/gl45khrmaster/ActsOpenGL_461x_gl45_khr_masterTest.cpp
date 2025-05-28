2d2a75a935205ad9944601c737ef082f8c1eaced
huruitao <huruitao@kaihong.com>
2025-05-28 17:28:19 +0800

    commit files bef ./xts_acts_3/graphic/gltest/src/gltest/gltest_kmn/gltest_460x/gl43khrmaster/BUILD.gn
    Signed-off-by: huruitao <huruitao@kaihong.com>

---
+++ graphic/gltest/src/gltest/gltest_kmn/gltest_461x/gl45khrmaster/ActsOpenGL_461x_gl45_khr_masterTest.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ #include "ActsOpenGL_461x_gl45_khr_masterTest.h"
+ #include <gtest/gtest.h>
+ #include <cstdlib>
+ #include "common.h"
+ 
+ 
+ namespace OHOS {
+     using namespace std;
+     using namespace testing::ext;
+ 
+     // Preset action of the test suite, which is executed before the first test case
+     void ActsOpenGL_461x_gl45_khr_masterTest::SetUpTestCase(void)
+     {
+     }
+     // Test suite cleanup action, which is executed after the last test case
+     void ActsOpenGL_461x_gl45_khr_masterTest::TearDownTestCase(void)
+     {
+     }
+     // Preset action of the test case
+     void ActsOpenGL_461x_gl45_khr_masterTest::SetUp()
+     {
+     }
+     // Cleanup action of the test case
+     void ActsOpenGL_461x_gl45_khr_masterTest::TearDown()
+     {
+     }
+ 
+     HWTEST_F(ActsOpenGL_461x_gl45_khr_masterTest, TestOpenGLglTestCase09, Function | MediumTest | Level2)
+     {
+         printf("------start ActsOpenGL461xgl45khrmasterTest------\n");
+         if (canIUse("SystemCapability.Graphic.Graphic2D.WebGL")) {
+             system("/data/local/tmp/opengl/mustpass/glcts "
+                 "--deqp-caselist-file=/data/local/tmp/opengl/mustpass/gl/khronos_mustpass_noctx/4.6.1.x/"
+                 "gl45-khr-master.txt "
+                 "--deqp-log-filename=ActsOpenGL461xgl45khrmasterTest.qpa");
+         }
+         EXPECT_TRUE(true);
+         printf("------end ActsOpenGL461xgl45khrmasterTest------\n");
+     }
+ }
