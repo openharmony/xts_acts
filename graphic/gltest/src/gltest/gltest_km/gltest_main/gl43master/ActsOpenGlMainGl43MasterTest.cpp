5c157d813eaebb4d68a1d258d3401939ff8034dc
huruitao <huruitao@kaihong.com>
2025-05-29 16:10:54 +0800

    commit files bef ./xts_acts_3/graphic/gltest/src/gltest/gltest_kms/gltest_461x/gl43khrsingle/ActsOpenGl461xGl43KhrSingleTest.h
    Signed-off-by: huruitao <huruitao@kaihong.com>

---
+++ graphic/gltest/src/gltest/gltest_km/gltest_main/gl43master/ActsOpenGlMainGl43MasterTest.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ #include "ActsOpenGlMainGl43MasterTest.h"
+ #include <gtest/gtest.h>
+ #include <cstdlib>
+ #include "common.h"
+ 
+ 
+ namespace OHOS {
+     using namespace std;
+     using namespace testing::ext;
+ 
+     // Preset action of the test suite, which is executed before the first test case
+     void ActsOpenGlMainGl43MasterTest::SetUpTestCase(void)
+     {
+     }
+     // Test suite cleanup action, which is executed after the last test case
+     void ActsOpenGlMainGl43MasterTest::TearDownTestCase(void)
+     {
+     }
+     // Preset action of the test case
+     void ActsOpenGlMainGl43MasterTest::SetUp()
+     {
+     }
+     // Cleanup action of the test case
+     void ActsOpenGlMainGl43MasterTest::TearDown()
+     {
+     }
+ 
+     HWTEST_F(ActsOpenGlMainGl43MasterTest, TestOpenGLglTestCase16, Function | MediumTest | Level2)
+     {
+         printf("------start ActsOpenGlMainGl43MasterTest------\n");
+         if (canIUse("SystemCapability.Graphic.Graphic2D.WebGL")) {
+             system("/data/local/tmp/opengl/mustpass/glcts "
+                 "--deqp-caselist-file=/data/local/tmp/opengl/mustpass/gl/khronos_mustpass/main/"
+                 "gl43-master.txt "
+                 "--deqp-log-filename=ActsOpenGlMainGl43MasterTest.qpa");
+         }
+         EXPECT_TRUE(true);
+         printf("------end ActsOpenGlMainGl43MasterTest------\n");
+     }
+ }
---
+++ graphic/gltest/src/gltest/gltest_kmn/gltest_main/gl40khrmaster/ActsOpenGlMainGl40KhrMasterTest.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ #include "ActsOpenGlMainGl40KhrMasterTest.h"
+ #include <gtest/gtest.h>
+ #include <cstdlib>
+ #include "common.h"
+ 
+ 
+ namespace OHOS {
+     using namespace std;
+     using namespace testing::ext;
+ 
+     // Preset action of the test suite, which is executed before the first test case
+     void ActsOpenGlMainGl40KhrMasterTest::SetUpTestCase(void)
+     {
+     }
+     // Test suite cleanup action, which is executed after the last test case
+     void ActsOpenGlMainGl40KhrMasterTest::TearDownTestCase(void)
+     {
+     }
+     // Preset action of the test case
+     void ActsOpenGlMainGl40KhrMasterTest::SetUp()
+     {
+     }
+     // Cleanup action of the test case
+     void ActsOpenGlMainGl40KhrMasterTest::TearDown()
+     {
+     }
+ 
+     HWTEST_F(ActsOpenGlMainGl40KhrMasterTest, TestOpenGLglTestCase09, Function | MediumTest | Level2)
+     {
+         printf("------start ActsOpenGlMainGl40KhrMasterTest------\n");
+         if (canIUse("SystemCapability.Graphic.Graphic2D.WebGL")) {
+             system("/data/local/tmp/opengl/mustpass/glcts "
+                 "--deqp-caselist-file=/data/local/tmp/opengl/mustpass/gl/khronos_mustpass_noctx/main/"
+                 "gl40-khr-master.txt "
+                 "--deqp-log-filename=ActsOpenGlMainGl40KhrMasterTest.qpa");
+         }
+         EXPECT_TRUE(true);
+         printf("------end ActsOpenGlMainGl40KhrMasterTest------\n");
+     }
+ }
