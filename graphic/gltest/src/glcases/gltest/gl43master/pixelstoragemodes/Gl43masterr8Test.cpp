236cf475da2abdc28db84a340ca5a0c48b202032
wangshi <wangshi@kaihong.com>
2025-05-30 09:18:28 +0800

    commit glcases: pixelstoragemodes
    Signed-off-by: wangshi <wangshi@kaihong.com>

---
+++ graphic/gltest/src/glcases/gltest/gl43master/pixelstoragemodes/Gl43masterr8Test.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <climits>
+#include <gtest/gtest.h>
+#include "../Gl43masterBaseFunc.h"
+#include "../ActsGl43master0001TestSuite.h"
+#include "shrinkdefine.h"
+
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS;
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003574,
+    "KHR-GL43.pixelstoragemodes.teximage2d.r8.0_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003575,
+    "KHR-GL43.pixelstoragemodes.teximage2d.r8.1_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003576,
+    "KHR-GL43.pixelstoragemodes.teximage2d.r8.16_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003577,
+    "KHR-GL43.pixelstoragemodes.teximage2d.r8.1_1");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003578,
+    "KHR-GL43.pixelstoragemodes.teximage2d.r8.16_1");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003579,
+    "KHR-GL43.pixelstoragemodes.teximage2d.r8.16_16");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003850,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.0_0_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003851,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.1_0_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003852,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.16_0_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003853,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.1_1_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003854,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.16_1_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003855,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.16_16_0");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003856,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.1_1_1");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003857,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.16_1_1");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003858,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.16_16_1");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003859,
+    "KHR-GL43.pixelstoragemodes.teximage3d.r8.16_16_4");
