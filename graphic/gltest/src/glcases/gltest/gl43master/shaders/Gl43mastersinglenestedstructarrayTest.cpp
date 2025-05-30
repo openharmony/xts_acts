37a7339586586a5ad977b1797892b65c4f5da804
wangshi <wangshi@kaihong.com>
2025-05-30 09:18:45 +0800

    commit glcases: shaders
    Signed-off-by: wangshi <wangshi@kaihong.com>

---
+++ graphic/gltest/src/glcases/gltest/gl43master/shaders/Gl43mastersinglenestedstructarrayTest.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <climits>
+#include <gtest/gtest.h>
+#include "../Gl43masterBaseFunc.h"
+#include "../ActsGl43master0001TestSuite.h"
+#include "shrinkdefine.h"
+
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS;
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003236,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.per_block_buffer_shared");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003237,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.per_block_buffer_shared_instance_array");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003238,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.per_block_buffer_packed");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003239,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.per_block_buffer_packed_instance_array");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003240,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.per_block_buffer_std140");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003241,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.per_block_buffer_std140_instance_array");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003242,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.single_buffer_shared_instance_array");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003243,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.single_buffer_packed_instance_array");
+
+static SHRINK_HWTEST_SF(ActsGl43master0001TestSuite, TestCase_003244,
+    "KHR-GL43.shaders.uniform_block.single_nested_struct_array.single_buffer_std140_instance_array");
