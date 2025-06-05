33512be23b54479b1c9d09a73b4782824de78642
刘从来 <liuconglai@h-partners.com>
2025-06-05 08:19:17 +0000

    update CustomComponentOndrawBehindTest.cpp.
    Signed-off-by: 刘从来 <liuconglai@h-partners.com>

--- arkui/ace_c_arkui_test_api20/entry/src/main/cpp/customcomponent/CustomComponentOndrawBehindTest.cpp
+++ arkui/ace_c_arkui_test_api20/entry/src/main/cpp/customcomponent/CustomComponentOndrawBehindTest.cpp
@@ -98,7 +98,7 @@ napi_value CustomComponentOnDrawBehindTest::CreateNativeNode(napi_env env, napi_
     nodeAPI->addChild(column, custom);
 
     nodeAPI->registerNodeCustomEventReceiver(&OnEventReceive);
-    nodeAPI->registerNodeCustomEvent(custom, ARKUI_NODE_CUSTOM_EVENT_ON_DRAW, ON_DRAW_EVENT_ID, nullptr);
+    nodeAPI->registerNodeCustomEvent(custom, ARKUI_NODE_CUSTOM_EVENT_ON_DRAW_BEHIND, ON_DRAW_EVENT_ID, nullptr);
 
     std::string id(xComponentID);
     if (OH_NativeXComponent_AttachNativeRootNode(PluginManager::GetInstance()->GetNativeXComponent(id), column) ==
