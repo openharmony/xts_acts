c5a572c4cdf5cdd2983d0ca5b3aa9462d8e48c8e
王倩56 <wangqian342@h-partners.com>
2025-06-09 19:53:03 +0800

    update linkEnhance testcases
    Signed-off-by: 王倩56 <wangqian342@h-partners.com>

--- communication/dsoftbus/linkEnhance/entry/hvigorfile.ts
+++ communication/dsoftbus/linkEnhance/entry/hvigorfile.ts
@@ -13,8 +13,3 @@
  * limitations under the License.
  */
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
-
-export default {
-    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
-}
