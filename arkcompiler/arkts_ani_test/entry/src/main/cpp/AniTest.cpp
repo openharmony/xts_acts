7d6eb0c72eaeb203214ea55a3daded607931e930
chenhliang <18991832943@163.com>
2025-06-14 15:56:06 +0800

    ani test
    Signed-off-by: chenhliang <18991832943@163.com>

--- arkcompiler/arkts_ani_test/entry/src/main/cpp/AniTest.cpp
+++ arkcompiler/arkts_ani_test/entry/src/main/cpp/AniTest.cpp
@@ -12,36 +12,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#include "ani/ani.h"
-#include <array>
-#include <iostream>
-const uint32_t ANI_RES_INVALID_ARGS = 2;
-const uint32_t ANI_RES_INVALID_TYPE = 3;
-const uint32_t ANI_RES_OUT_OF_REF = 9;
-
+#include "Common.h"
+#include "ClassOperations.h"
+#include "VersionInfo.h"
 
 static ani_int add([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_object object, ani_int arg1, ani_int arg2)
 {
     return arg1 + arg2;
-}
-
-static ani_int test_GetVersion([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_object object)
-{
-    uint32_t aniVersion = 0;
-    env->GetVersion(&aniVersion);
-    return (ani_int)aniVersion;
-}
-
-static ani_boolean test_FindClass([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_object object)
-{
-    ani_boolean res = false;
-    ani_class cls = nullptr;
-    auto status = env->FindClass("Lentry/src/main/src/ets/Index/Calc;", &cls);
-    if (status == ANI_OK && cls != nullptr) {
-        res = true;
-    }
-    return res;
 }
 
 static ani_status bindCase(ani_vm *vm, const char *clsName)
@@ -62,6 +39,8 @@ static ani_status bindCase(ani_vm *vm, const char *clsName)
         ani_native_function {"add", "II:I", reinterpret_cast<void *>(add)},
         ani_native_function {"test_GetVersion", ":I", reinterpret_cast<void *>(test_GetVersion)},
         ani_native_function {"test_FindClass", ":Z", reinterpret_cast<void *>(test_FindClass)},
+        ani_native_function {"test_Class_FindStaticMethod", ":Z",
+                             reinterpret_cast<void *>(test_Class_FindStaticMethod)},
     };
     
     if (ANI_OK != env->Class_BindNativeMethods(cls, methods.data(), methods.size())) {
@@ -74,7 +53,7 @@ static ani_status bindCase(ani_vm *vm, const char *clsName)
 ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
 {
     std::cout << "testTag : entering libentry.so's ANI_Constructor" << std::endl;
-    bindCase(vm, "Lentry/src/main/src/ets/Index/Calc;");
+    bindCase(vm, "Lentry/src/main/src/ets/Index/Foo;");
     *result = ANI_VERSION_1;
     return ANI_OK;
 }
