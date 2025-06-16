d64af3beb27a9ab67a503718ec561cd39277e605
jinzhao <jinzhao@kaihong.com>
2025-06-16 15:30:30 +0800

    CodeFix
    Signed-off-by: jinzhao <jinzhao@kaihong.com>

--- ability/ability_runtime/actssetfonttest/entry/src/main/ets/entryability/EntryAbility.ts
+++ ability/ability_runtime/actssetfonttest/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,10 +1,10 @@
-/**
- * Copyright (c) 2025 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,9 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { AbilityConstant, UIAbility, Want } from '@kit.AbilityKit';
+import { AbilityConstant, Configuration, UIAbility, Want } from '@kit.AbilityKit';
 import { hilog } from '@kit.PerformanceAnalysisKit';
 import { window } from '@kit.ArkUI';
+
+const TAG: string = 'testTag-EntryAbility ';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want: Want, launchParam: AbilityConstant.LaunchParam) {
@@ -51,5 +53,9 @@ export default class EntryAbility extends UIAbility {
   onBackground() {
     // Ability has back to background
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+  }
+
+  onConfigurationUpdate(config: Configuration): void {
+    hilog.info(0x0000, TAG, '%{public}s', 'UpdateConfiguration: EntryAbility onConfigurationUpdate');
   }
 }
