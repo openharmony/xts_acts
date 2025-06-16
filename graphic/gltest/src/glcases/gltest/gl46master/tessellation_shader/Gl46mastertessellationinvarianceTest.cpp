4b1717cac9f3442b876fe44b8b988705928fb7e5
shitijun <shitijun@kaihong.com>
2025-06-16 14:34:12 +0800

    commit glcases: shader_draw_parameters_tests
    Signed-off-by: shitijun <shitijun@kaihong.com>

---
+++ graphic/gltest/src/glcases/gltest/gl46master/tessellation_shader/Gl46mastertessellationinvarianceTest.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <climits>
+#include <gtest/gtest.h>
+#include "../Gl46masterBaseFunc.h"
+#include "../ActsGl46master0001TestSuite.h"
+#include "shrinkdefine.h"
+
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS;
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008335,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule1");
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008336,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule2");
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008337,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule3");
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008338,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule4");
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008339,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule5");
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008340,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule6");
+
+static SHRINK_HWTEST_SF(ActsGl46master0001TestSuite, TestCase_008341,
+    "KHR-GL46.tessellation_shader.tessellation_invariance.invariance_rule7");
